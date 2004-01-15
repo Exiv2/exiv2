ffa409cb781e1cd07568704662a81f021892ca54
Andreas Huggel <ahuggel@gmx.net>
2004-01-15 07:52:19 +0000

    Added more tag descriptions and better handling thereof

--- src/exiftest.cc
+++ src/exiftest.cc
@@ -1,5 +1,6 @@
 #include "exif.h"
 #include <iostream>
+#include <iomanip>
 
 int main(int argc, char* const argv[])
 {
@@ -9,5 +10,30 @@ int main(int argc, char* const argv[])
     }
 
     Exif::ExifData exifData;
-    return exifData.read(argv[1]);
+    int rc = exifData.read(argv[1]);
+
+    if (rc == 0) {
+
+        Exif::ExifData::const_iterator beg = exifData.begin();
+        Exif::ExifData::const_iterator end = exifData.end();
+        Exif::ExifData::const_iterator i = beg;
+        for (; i != end; ++i) {
+
+            std::cout << "0x" 
+                      << std::hex << std::setw(4) << std::setfill('0') << std::right
+                      << i->tag_ << " " 
+                      << std::setw(27) << std::setfill(' ') << std::left
+                      << i->tagName() << " "
+                      << std::setw(17) << std::setfill(' ') << std::left
+                      << i->typeName() << " "
+                      << std::dec << std::setw(3) << std::setfill(' ') << std::right
+                      << i->count_ << " " 
+                      << std::dec << std::setw(3) << std::setfill(' ') << std::right
+                      << i->typeSize() * i->count_ << "\n";
+
+        }
+    }
+
+    return rc;
+
 }
