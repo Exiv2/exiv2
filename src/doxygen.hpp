6f9b10db6168853e3105e377c2c05a98cbb3c139
Andreas Huggel <ahuggel@gmx.net>
2004-02-10 02:41:43 +0000

    Pseudo headerfile containing only Dozygen documentation

---
+++ src/doxygen.hpp
@@ -0,0 +1,39 @@
+// ***************************************************************** -*- C++ -*-
+/*
+ */
+/*!
+  @file    doxygen.hpp
+  @brief   Additional documentation, this file contains no source code
+  @version $Name:  $ $Revision: 1.1 $
+  @author  Andreas Huggel (ahu)
+           <a href="mailto:ahuggel@gmx.net">ahuggel@gmx.net</a>
+  @date    07-Feb-04, ahu: created
+ */
+/*!
+  @mainpage Exiv2
+
+  <a href="../tags.xml">List of %Exif tags</a> as defined in Exiv2
+  
+  <BR>
+
+  <P>Copyright (C) 2004 Andreas Huggel <ahuggel@gmx.net></P>
+ 
+  <P>Exiv2 is free software; you can redistribute it and/or modify it under the
+  terms of the <a href="../gpl-license.html">GNU General Public License</a> as
+  published by the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.</P>
+ 
+  <P>Exiv2 is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+  A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.</P>
+ 
+  <P>You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc., 59
+  Temple Place - Suite 330, Boston, MA 02111-1307, USA.</P>
+
+*/
+/*!
+  @example example1.cpp
+  Sample usage of high-level metadata operations.
+*/
