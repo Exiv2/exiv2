bbe84812664e45b1576fce46ba990df2cca64c4e
Andreas Huggel <ahuggel@gmx.net>
2004-03-06 06:36:36 +0000

    Print info that exiv2 does not print (mainly the key)

--- src/exifprint.cpp
+++ src/exifprint.cpp
@@ -23,12 +23,12 @@
 
   File:      exifprint.cpp
   Author(s): Andreas Huggel (ahu) <ahuggel@gmx.net>
-  Version  : $Name:  $ $Revision: 1.9 $
+  Version  : $Name:  $ $Revision: 1.10 $
   History  : 26-Jan-04, ahu: created
  */
 // *****************************************************************************
 #include "rcsid.hpp"
-EXIV2_RCSID("@(#) $Name:  $ $Revision: 1.9 $ $RCSfile: exifprint.cpp,v $")
+EXIV2_RCSID("@(#) $Name:  $ $Revision: 1.10 $ $RCSfile: exifprint.cpp,v $")
 
 // *****************************************************************************
 // included header files
@@ -65,17 +65,10 @@ try {
 
     ExifData::const_iterator end = exifData.end();
     for (ExifData::const_iterator i = exifData.begin(); i != end; ++i) {
-        std::cout << "0x" << std::setw(4) << std::setfill('0') << std::right
-                  << std::hex << i->tag() << " " 
-                  << std::setw(4) << std::setfill(' ') << std::left
-                  << i->ifdName() << " "
-                  << std::setw(27) << std::setfill(' ') << std::left
-                  << i->tagName() << " "
-                  << std::setw(9) << std::setfill(' ') << std::left
-                  << i->typeName() << " "
-                  << std::dec << std::setw(3) 
-                  << std::setfill(' ') << std::right
-                  << i->count() << " "
+        std::cout << std::setw(53) << std::setfill(' ') << std::left
+                  << i->key() << " "
+                  << "0x" << std::setw(4) << std::setfill('0') << std::right
+                  << std::hex << i->tag() << "  " 
                   << std::dec << i->value() 
                   << "\n";
     }
