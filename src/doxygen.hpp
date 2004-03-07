f65e61b5ebc43460b684189661cbf58343a3f7f6
Andreas Huggel <ahuggel@gmx.net>
2004-03-07 16:11:48 +0000

    Updated documentation

--- src/doxygen.hpp
+++ src/doxygen.hpp
@@ -1,10 +1,8 @@
 // ***************************************************************** -*- C++ -*-
-/*
- */
 /*!
   @file    doxygen.hpp
   @brief   Additional documentation, this file contains no source code
-  @version $Name:  $ $Revision: 1.3 $
+  @version $Name:  $ $Revision: 1.4 $
   @author  Andreas Huggel (ahu)
            <a href="mailto:ahuggel@gmx.net">ahuggel@gmx.net</a>
   @date    07-Feb-04, ahu: created
@@ -21,14 +19,17 @@
   - extract the thumbnail image embedded in the %Exif metadata
   - delete the thumbnail or the complete %Exif metadata from an image
 
-  Exiv2 is also a C++ %Exif metadata library, providing high-level access to the
-  %Exif data of an image. Supported functionality includes
-  - read %Exif information from JPEG files
-  - access metadata through keys and standard C++ iterators
-  - add, modify and delete metadata 
-  - write %Exif data to JPEG files
-  - extract %Exif metadata to files, insert from these files
-  - extract and delete %Exif thumbnail (JPEG and TIFF thumbnails)
+  Exiv2 is also a C++ %Exif metadata library that provides
+  - full read and write access to the %Exif metadata of an image through Exiv2
+    keys and standard C++ iterators
+  - extensive MakerNote support: 
+      - MakerNote tags can be accessed just like any other %Exif metadata
+      - a sophisticated write algorithm avoids corrupting the MakerNote:
+        1) does not re-locate the MakerNote if possible, and 
+        2) re-calculates IFD offsets if the MakerNote needs to be moved
+      - new camera make/model specific MakerNotes can be added to the 
+        library in OO-fashion (by subclassing MakerNote or IfdMakerNote)
+  - extract and delete methods for %Exif thumbnails (both, JPEG and TIFF thumbnails)
 
   In addition, the library provides generic lower-level functionality to access
   IFD (Image File Directory) data structures.
