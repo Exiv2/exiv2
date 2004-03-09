87cc071ef93758262df88608c158368bfa3d3580
ahuggel <ahuggel@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2004-03-09 08:20:36 +0000

    Added simple unit tests and expected output

---
+++ trunk/src/makernote-test.cpp
@@ -0,0 +1,48 @@
+#include "makernote.hpp"
+
+#include <iostream>
+#include <string>
+#include <utility>
+
+void testMatch(const std::string& reg, const std::string& key);
+
+int main()
+{
+    testMatch("Canon", "Canon");
+    testMatch("Canon*", "Canon");
+    testMatch("Canon*", "Canon Corp.");
+    testMatch("*foo*bar*", "foobar");
+    testMatch("*foo*bar*", "barfoofoobarbar");
+    testMatch("foo*bar", "foo");
+    testMatch("foo*bar", "bar");
+    testMatch("foo*bar", "foobar");
+    testMatch("foo*bar", "fooYAHOObar");
+    testMatch("foo*bar", "Thefoobar");
+    testMatch("foo*bar", "foobarTrick");
+    testMatch("foo*bar", "ThefoobarTrick");
+    testMatch("foo*bar", "ThefooYAHOObarTrick");
+
+    testMatch("*", "anything");
+    testMatch("**", "anything times two");
+
+    testMatch("*bar", "bar");
+    testMatch("b*bar", "bar");
+    testMatch("b*bar", "bbar");
+    testMatch("*foobar", "bar");
+    testMatch("*bar", "foobar");
+
+    return 0;
+}
+
+void testMatch(const std::string& reg, const std::string& key)
+{
+    std::pair<bool, int> rc = Exif::MakerNoteFactory::match(reg, key);
+
+    if (rc.first) {
+        std::cout << "Key '" << key << "' matches '" << reg << "' in " 
+                  << rc.second << " characters.\n";
+    }
+    else {
+        std::cout << "Key '" << key << "' does not match '" << reg << "'.\n";
+    }
+}
