53eb17ca5ed8a5c17b777230158ce6b163434d9f
Andreas Huggel <ahuggel@gmx.net>
2004-05-08 10:41:28 +0000

    Updated according to the webpage

--- src/doxygen.hpp
+++ src/doxygen.hpp
@@ -2,56 +2,63 @@
 /*!
   @file    doxygen.hpp
   @brief   Additional documentation, this file contains no source code
-  @version $Name:  $ $Revision: 1.8 $
+  @version $Name:  $ $Revision: 1.9 $
   @author  Andreas Huggel (ahu)
            <a href="mailto:ahuggel@gmx.net">ahuggel@gmx.net</a>
   @date    07-Feb-04, ahu: created
  */
 /*!
-  @mainpage %Exif metadata manipulation library and tools v0.3
+  @mainpage Exif metadata manipulation library and tools v0.3
 
-  Exiv2 is a command line program to 
-  - print the %Exif metadata of JPEG images (as summary info, interpreted values, 
-    or the plain data for each tag)
-  - adjust the %Exif timestamp (that's how it all started...)
-  - rename %Exif image files according to the %Exif timestamp
-  - extract %Exif metadata from image files and insert it again
-  - extract the thumbnail image embedded in the %Exif metadata
-  - delete the thumbnail or the complete %Exif metadata from an image
+  @section overview Exiv2 Overview
+ 
+  Exiv2 is a C++ library that provides
+  - full read and write access to the Exif metadata of an image through %Exiv2 
+    keys and standard C++ iterators 
+  - extensive MakerNote support:
+      - MakerNote tags can be accessed just like any other Exif metadata
+      - a sophisticated write algorithm avoids corrupting the MakerNote: 
+         <BR>&nbsp;&nbsp;1) the MakerNote is not re-located if possible at all, and 
+         <BR>&nbsp;&nbsp;2) MakerNote IFD offsets are re-calculated if the MakerNote
+                            needs to be moved (for known IFD MakerNotes)
+      - new camera make/model specific MakerNotes can be added to the library
+        with minimum effort in OO-fashion (by subclassing MakerNote or IfdMakerNote)
 
-  Exiv2 is also a C++ library that provides
-  - full read and write access to the %Exif metadata of an image through Exiv2
-    keys and standard C++ iterators
-  - extensive MakerNote support: 
-      - MakerNote tags can be accessed just like any other %Exif metadata
-      - a sophisticated write algorithm avoids corrupting the MakerNote:
-        1) the MakerNote is not re-located if possible at all, and 
-        2) MakerNote IFD offsets are re-calculated if the MakerNote needs to be 
-           moved (for known MakerNotes in IFD format)
-      - new camera make/model specific MakerNotes can be added to the 
-        library with minimum effort in OO-fashion (by subclassing MakerNote
-        or IfdMakerNote)
-  - extract and delete methods for %Exif thumbnails (both, JPEG and TIFF thumbnails)
+  - extract and delete methods for Exif thumbnails (both, JPEG and TIFF thumbnails)
+  - extensive Doxygen documentation
+  - generic lower-level classes to access IFD (Image File Directory) data structures
 
-  In addition, the library provides generic lower-level classes to access IFD
-  (Image File Directory) data structures.
+  %Exiv2 is also a command line program to
+  - print the Exif metadata of JPEG images (as summary info, interpreted values, or
+    the plain data for each tag)
+  - adjust the Exif timestamp (that's how it all started...)
+  - rename Exif image files according to the Exif timestamp
+  - extract Exif metadata from image files and insert it again
+  - extract the thumbnail image embedded in the Exif metadata
+  - delete the thumbnail or the complete Exif metadata from an image
 
-  Here is a (sortable) <A HREF="../tags.xml">list of %Exif tags</A> as defined
-  in Exiv2.
+  @section metadata Supported Exif and MakerNote tags
 
+  - <A HREF="../tags.xml">Standard Exif tags</A>
+  - <A HREF="../tags-canon.xml">Canon MakerNote tags</A>
+  - <A HREF="../tags-fujifilm.xml">Fujifilm MakerNote tags</A>
+  - <A HREF="../tags-sigma.xml">Sigma/Foveon MakerNote tags</A>
 
-  <A HREF="../makernote.html">MakerNote Formats and Specifications</A>
+  @section makernote MakerNote Formats and Specifications
 
+  A summary of <A HREF="../makernote.html">MakerNote structures</A> with links to 
+  publicly available specifications.
 
+  @section license License
 
   <P>Copyright (C) 2004 Andreas Huggel <ahuggel@gmx.net></P>
- 
-  <P>Exiv2 is free software; you can redistribute it and/or modify it under the
+
+  <P>%Exiv2 is free software; you can redistribute it and/or modify it under the
   terms of the <a href="../gpl-license.html">GNU General Public License</a> as
   published by the Free Software Foundation; either version 2 of the License, or
   (at your option) any later version.</P>
  
-  <P>Exiv2 is distributed in the hope that it will be useful, but WITHOUT ANY
+  <P>%Exiv2 is distributed in the hope that it will be useful, but WITHOUT ANY
   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
   A PARTICULAR PURPOSE.  See the GNU General Public License for more
   details.</P>
@@ -67,5 +74,5 @@
  */
 /*!
   @example exifprint.cpp
-  Sample program to print %Exif data from an image.
+  Sample program to print Exif data from an image.
  */
