0e40ee4d3ab345ca86d97a4314be9a30ebc69a5a
Andreas Huggel <ahuggel@gmx.net>
2004-07-03 07:01:32 +0000

    Minor change

--- src/write-test.sh
+++ src/write-test.sh
@@ -30,14 +30,24 @@ runTestCase()
 # **********************************************************************
 # main
 
-runTestCase  1 ../test/img_1771.jpg
-runTestCase  2 ../test/img_1771.jpg
-runTestCase  3 ../test/kodak-dc210.jpg
-runTestCase  4 ../test/img_1771.jpg
-runTestCase  5 ../test/img_1771.jpg
-runTestCase  6 ../test/kodak-dc210.jpg
-runTestCase  7 ../test/dscf0176.jpg
-runTestCase  8 ../test/sigma-d10-IMG10086.jpg
-runTestCase  9 ../test/nikon-coolpix-990-DSCN1180.jpg
-runTestCase 10 ../test/nikon-e950.jpg
-runTestCase 11 ../test/nikon-d70-dsc_0063.jpg
+images="img_1771.jpg \
+        kodak-dc210.jpg \
+        dscf0176.jpg \
+        sigma-d10-IMG10086.jpg \
+        nikon-coolpix-990-DSCN1180.jpg \
+        nikon-d70-dsc_0063.jpg \
+        nikon-e950.jpg"
+
+for i in $images; do cp -f ../test/images/$i ./; done
+
+runTestCase  1 ./img_1771.jpg
+runTestCase  2 ./img_1771.jpg
+runTestCase  3 ./kodak-dc210.jpg
+runTestCase  4 ./img_1771.jpg
+runTestCase  5 ./img_1771.jpg
+runTestCase  6 ./kodak-dc210.jpg
+runTestCase  7 ./dscf0176.jpg
+runTestCase  8 ./sigma-d10-IMG10086.jpg
+runTestCase  9 ./nikon-coolpix-990-DSCN1180.jpg
+runTestCase 10 ./nikon-e950.jpg
+runTestCase 11 ./nikon-d70-dsc_0063.jpg
