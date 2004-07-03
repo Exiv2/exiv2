ba2770426fd19ee9a198c4c08533f314c2eeb0be
Andreas Huggel <ahuggel@gmx.net>
2004-07-03 06:46:23 +0000

    Bugfix: added missing break

--- src/exiv2.cpp
+++ src/exiv2.cpp
@@ -22,13 +22,13 @@
   Abstract:  Command line program to display and manipulate image %Exif data
 
   File:      exiv2.cpp
-  Version:   $Name:  $ $Revision: 1.10 $
+  Version:   $Name:  $ $Revision: 1.11 $
   Author(s): Andreas Huggel (ahu) <ahuggel@gmx.net>
   History:   10-Dec-03, ahu: created
  */
 // *****************************************************************************
 #include "rcsid.hpp"
-EXIV2_RCSID("@(#) $Name:  $ $Revision: 1.10 $ $RCSfile: exiv2.cpp,v $")
+EXIV2_RCSID("@(#) $Name:  $ $Revision: 1.11 $ $RCSfile: exiv2.cpp,v $")
 
 // *****************************************************************************
 // included header files
@@ -266,6 +266,7 @@ int Params::option(int opt, const std::string& optarg, int optopt)
                 rc = 1;
                 break;
             }
+            break;
         case Action::extract:
             std::cerr << progname() 
                       << ": Ignoring surplus option -e" << optarg << "\n";
