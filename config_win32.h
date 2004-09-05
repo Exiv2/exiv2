b9275b8d88715b82c5851425e2e08615a00dfac5
Andreas Huggel <ahuggel@gmx.net>
2004-09-05 15:14:12 +0000

    Configuration for win32

---
+++ config_win32.h
@@ -0,0 +1,32 @@
+/* config_win32.h */
+
+/* Todo: The PACKAGE_* defines should be generated */
+
+#ifdef _MSC_VER
+
+/* Define to 1 if you have the <process.h> header file. */
+#define HAVE_PROCESS_H 1
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "ahuggel@gmx.net"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Exiv2"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Exiv2 0.5"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "exiv2"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.5"
+
+/* Define to `int' if <sys/types.h> does not define. */
+#define pid_t int
+
+/* File path seperator */
+#define SEPERATOR_STR "\\"
+#define SEPERATOR_CHR '\\'
+
+#endif /* _MSC_VER */
