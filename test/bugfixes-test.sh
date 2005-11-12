fc93a620ee84eb9e336f7a783fb80933fccfcb49
Andreas Huggel <ahuggel@gmx.net>
2005-11-12 17:13:49 +0000

    Improved bugfix regression test script

--- test/bugfixes-test.sh
+++ test/bugfixes-test.sh
@@ -9,18 +9,39 @@ diff -q $diffargs $tmpfile $tmpfile 2>/dev/null
 if [ $? -ne 0 ] ; then
     diffargs=""
 fi
+
+prep_file()
+{
+    gf_num=$1
+    gf_filename=exiv2-bug$gf_num.jpg
+    echo '------>' Bug $gf_num '<-------' >&2
+    cp -f ../data/$gf_filename .
+    echo $gf_filename
+}
+
 (
 LD_LIBRARY_PATH=../../src:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH
 binpath="../../src"
 cd ./tmp
-# bug 440
-cp -f ../data/exiv2-bug440.jpg .
-$binpath/exiv2 -pi exiv2-bug440.jpg
-# bug 441
-cp -f ../data/exiv2-bug441.jpg .
-$binpath/exiv2 -pi exiv2-bug441.jpg
-) > $results
+
+num=426
+filename=`prep_file $num`
+$binpath/exiv2 -pi $filename
+
+num=440
+filename=`prep_file $num`
+$binpath/exiv2 -pi $filename
+
+num=443
+filename=`prep_file $num`
+$binpath/exiv2 -pt $filename
+
+num=444
+filename=`prep_file $num`
+$binpath/exiv2 -pi $filename
+
+) > $results 2>&1
 
 diff -q $diffargs $results $good
 rc=$?
