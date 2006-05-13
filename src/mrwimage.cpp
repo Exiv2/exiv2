431ee85816196e71dfb189fb611e00d8f49b0768
Andreas Huggel <ahuggel@gmx.net>
2006-05-13 15:43:55 +0000

    Added support for MRW images (read-only), first try

---
+++ src/mrwimage.cpp
@@ -0,0 +1,222 @@
+// ***************************************************************** -*- C++ -*-
+/*
+ * Copyright (C) 2006 Andreas Huggel <ahuggel@gmx.net>
+ *
+ * This program is part of the Exiv2 distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, 5th Floor, Boston, MA 02110-1301 USA.
+ */
+/*
+  File:      mrwimage.cpp
+  Version:   $Rev$
+  Author(s): Andreas Huggel (ahu) <ahuggel@gmx.net>
+  History:   13-May-06, ahu: created
+  Credits:   See header file
+ */
+// *****************************************************************************
+#include "rcsid.hpp"
+EXIV2_RCSID("@(#) $Id$");
+
+// *****************************************************************************
+// included header files
+#ifdef _MSC_VER
+# include "exv_msvc.h"
+#else
+# include "exv_conf.h"
+#endif
+
+#include "mrwimage.hpp"
+#include "tiffparser.hpp"
+#include "image.hpp"
+#include "basicio.hpp"
+#include "error.hpp"
+#include "futils.hpp"
+
+// + standard includes
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <cassert>
+
+// *****************************************************************************
+// class member definitions
+namespace Exiv2 {
+
+    MrwImage::MrwImage(BasicIo::AutoPtr io, bool create)
+        : Image(mdExif), io_(io)
+    {
+        if (create) {
+            IoCloser closer(*io_);
+            io_->open();
+        }
+    } // MrwImage::MrwImage
+
+    bool MrwImage::good() const
+    {
+        if (io_->open() != 0) return false;
+        IoCloser closer(*io_);
+        return isThisType(*io_, false);
+    }
+
+    void MrwImage::clearMetadata()
+    {
+        clearExifData();
+    }
+
+    void MrwImage::setMetadata(const Image& image)
+    {
+        setExifData(image.exifData());
+    }
+
+    void MrwImage::clearExifData()
+    {
+        exifData_.clear();
+    }
+
+    void MrwImage::setExifData(const ExifData& exifData)
+    {
+        exifData_ = exifData;
+    }
+
+    void MrwImage::clearIptcData()
+    {
+        // not supported
+    }
+
+    void MrwImage::setIptcData(const IptcData& /*iptcData*/)
+    {
+        // not supported
+    }
+
+    void MrwImage::clearComment()
+    {
+        // not supported
+    }
+
+    void MrwImage::setComment(const std::string& comment)
+    {
+        // not supported
+    }
+
+    void MrwImage::readMetadata()
+    {
+#ifdef DEBUG
+        std::cerr << "Reading MRW file " << io_->path() << "\n";
+#endif
+        if (io_->open() != 0) {
+            throw Error(9, io_->path(), strError());
+        }
+        IoCloser closer(*io_);
+        // Ensure that this is the correct image type
+        if (!isThisType(*io_, false)) {
+            if (io_->error() || io_->eof()) throw Error(14);
+            throw Error(3, "MRW");
+        }
+        clearMetadata();
+
+        // Read the image into a memory buffer
+        uint32_t const len = 16;
+        byte tmp[len];
+        io_->read(tmp, len);
+        if (io_->error() || io_->eof()) throw Error(14);
+
+        io_->seek(getLong(tmp + 12, bigEndian), BasicIo::cur);
+        if (io_->error() || io_->eof()) throw Error(14);
+        
+        io_->read(tmp, 8);
+        if (io_->error() || io_->eof()) throw Error(14);
+
+        DataBuf buf(getULong(tmp + 4, bigEndian));
+        io_->read(buf.pData_, buf.size_);
+        if (io_->error() || io_->eof()) throw Error(14);
+
+        TiffParser::decode(this, buf.pData_, buf.size_, TiffCreator::create);
+    } // MrwImage::readMetadata
+
+    void MrwImage::writeMetadata()
+    {
+/*
+
+       Todo: implement me!
+
+#ifdef DEBUG
+        std::cerr << "Writing MRW file " << io_->path() << "\n";
+#endif
+        // Read existing image
+        DataBuf buf;
+        if (io_->open() == 0) {
+            IoCloser closer(*io_);
+            // Ensure that this is the correct image type
+            if (isThisType(*io_, false)) {
+                // Read the image into a memory buffer
+                buf.alloc(io_->size());
+                io_->read(buf.pData_, buf.size_);
+                if (io_->error() || io_->eof()) {
+                    buf.reset();
+                }
+            }
+        }
+
+        // Parse image, starting with a MRW header component
+        TiffHeade2::AutoPtr head(new TiffHeade2);
+        if (buf.size_ != 0) {
+            head->read(buf.pData_, buf.size_);
+        }
+
+        Blob blob;
+        TiffParser::encode(blob, head.get(), this);
+
+        // Write new buffer to file
+        BasicIo::AutoPtr tempIo(io_->temporary()); // may throw
+        assert (tempIo.get() != 0);
+        tempIo->write(&blob[0], static_cast<long>(blob.size()));
+        io_->close();
+        io_->transfer(*tempIo); // may throw
+*/
+    } // MrwImage::writeMetadata
+
+    bool MrwImage::isThisType(BasicIo& iIo, bool advance) const
+    {
+        return isMrwType(iIo, advance);
+    }
+
+    // *************************************************************************
+    // free functions
+
+    Image::AutoPtr newMrwInstance(BasicIo::AutoPtr io, bool create)
+    {
+        Image::AutoPtr image(new MrwImage(io, create));
+        if (!image->good()) {
+            image.reset();
+        }
+        return image;
+    }
+
+    bool isMrwType(BasicIo& iIo, bool advance)
+    {
+        const int32_t len = 4;
+        byte buf[len];
+        iIo.read(buf, len);
+        if (iIo.error() || iIo.eof()) {
+            return false;
+        }
+        int rc = memcmp(buf, "\0MRM", 4);
+        if (!advance || rc != 0) {
+            iIo.seek(-len, BasicIo::cur);
+        }
+        return rc == 0;
+    }
+
+}                                       // namespace Exiv2
---
+++ src/mrwimage.hpp
@@ -0,0 +1,190 @@
+// ***************************************************************** -*- C++ -*-
+/*
+ * Copyright (C) 2006 Andreas Huggel <ahuggel@gmx.net>
+ *
+ * This program is part of the Exiv2 distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, 5th Floor, Boston, MA 02110-1301 USA.
+ */
+/*!
+  @file    mrwimage.hpp
+  @brief   Minolta RAW image, implemented using the following references:
+           <a href="http://www.dalibor.cz/minolta/raw_file_format.htm">Minolta Raw file format</a> by Dalibor Jelinek
+  @version $Rev$
+  @author  Andreas Huggel (ahu)
+           <a href="mailto:ahuggel@gmx.net">ahuggel@gmx.net</a>
+  @date    13-May-06, ahu: created
+ */
+#ifndef MRWIMAGE_HPP_
+#define MRWIMAGE_HPP_
+
+// *****************************************************************************
+// included header files
+#include "exif.hpp"
+#include "iptc.hpp"
+#include "image.hpp"
+
+// + standard includes
+#include <string>
+
+// *****************************************************************************
+// namespace extensions
+namespace Exiv2 {
+
+// *****************************************************************************
+// class definitions
+
+    // Add MRW to the supported image formats
+    namespace ImageType {
+        const int mrw = 5;          //!< MRW image type (see class MrwImage)
+    }
+
+    /*!
+      @brief Class to access raw MRW images. Only Exif metadata is currently
+          supported.
+     */
+    class MrwImage : public Image {
+        friend bool isMrwType(BasicIo& iIo, bool advance);
+
+        //! @name NOT Implemented
+        //@{
+        //! Copy constructor
+        MrwImage(const MrwImage& rhs);
+        //! Assignment operator
+        MrwImage& operator=(const MrwImage& rhs);
+        //@}
+
+    public:
+        //! @name Creators
+        //@{
+        /*!
+          @brief Constructor that can either open an existing MRW image or create
+              a new image from scratch. If a new image is to be created, any
+              existing data is overwritten. Since the constructor can not return
+              a result, callers should check the good() method after object
+              construction to determine success or failure.
+          @param io An auto-pointer that owns a BasicIo instance used for
+              reading and writing image metadata. \b Important: The constructor
+              takes ownership of the passed in BasicIo instance through the
+              auto-pointer. Callers should not continue to use the BasicIo
+              instance after it is passed to this method.  Use the Image::io()
+              method to get a temporary reference.
+          @param create Specifies if an existing image should be read (false)
+              or if a new file should be created (true).
+         */
+        MrwImage(BasicIo::AutoPtr io, bool create);
+        //! Destructor
+        ~MrwImage() {}
+        //@}
+
+        //! @name Manipulators
+        //@{
+        void            readMetadata();
+        /*!
+          @brief Todo: Write metadata back to the image. This method is not
+                 yet implemented.
+         */
+        void            writeMetadata();
+        void            setExifData(const ExifData& exifData);
+        void            clearExifData();
+        /*!
+          @brief Not supported. MRW format does not contain IPTC metadata.
+                 Calling this function will do nothing.
+         */
+        void            setIptcData(const IptcData& iptcData);
+        /*!
+          @brief Not supported. MRW format does not contain IPTC metadata.
+                 Calling this function will do nothing.
+         */
+        void            clearIptcData();
+        /*!
+          @brief Not supported. MRW format does not contain a comment.
+                 Calling this function will do nothing.
+         */
+        void            setComment(const std::string& comment);
+        /*!
+          @brief Not supported. MRW format does not contain a comment.
+                 Calling this function will do nothing.
+         */
+        void            clearComment();
+        void            setMetadata(const Image& image);
+        void            clearMetadata();
+        ExifData&       exifData()       { return exifData_; }
+        IptcData&       iptcData()       { return iptcData_; }
+        //@}
+
+        //! @name Accessors
+        //@{
+        bool            good()     const;
+        const ExifData& exifData() const { return exifData_; }
+        const IptcData& iptcData() const { return iptcData_; }
+        std::string     comment()  const { return comment_; }
+        BasicIo&        io()       const { return *io_; }
+        //@}
+
+    private:
+        //! @name Accessors
+        //@{
+        /*!
+          @brief Determine if the content of the BasicIo instance is a MRW image.
+
+          The advance flag determines if the read position in the stream is
+          moved (see below). This applies only if the type matches and the
+          function returns true. If the type does not match, the stream
+          position is not changed. However, if reading from the stream fails,
+          the stream position is undefined. Consult the stream state to obtain
+          more information in this case.
+
+          @param iIo BasicIo instance to read from.
+          @param advance Flag indicating whether the position of the io
+              should be advanced by the number of characters read to
+              analyse the data (true) or left at its original
+              position (false). This applies only if the type matches.
+          @return  true  if the data matches the type of this class;<BR>
+                   false if the data does not match
+         */
+        bool isThisType(BasicIo& iIo, bool advance) const;
+        /*!
+          @brief Todo: Write MRW header. Not implemented yet.
+         */
+        int writeHeader(BasicIo& oIo) const;
+        //@}
+
+        // DATA
+        BasicIo::AutoPtr  io_;                  //!< Image data io pointer
+        ExifData          exifData_;            //!< Exif data container
+        IptcData          iptcData_;            //!< IPTC data container
+        std::string       comment_;             //!< User comment
+
+    }; // class MrwImage
+
+// *****************************************************************************
+// template, inline and free functions
+
+    // These could be static private functions on Image subclasses but then
+    // ImageFactory needs to be made a friend.
+    /*!
+      @brief Create a new MrwImage instance and return an auto-pointer to it.
+             Caller owns the returned object and the auto-pointer ensures that
+             it will be deleted.
+     */
+    Image::AutoPtr newMrwInstance(BasicIo::AutoPtr io, bool create);
+
+    //! Check if the file iIo is a MRW image.
+    bool isMrwType(BasicIo& iIo, bool advance);
+
+}                                       // namespace Exiv2
+
+#endif                                  // #ifndef MRWIMAGE_HPP_
