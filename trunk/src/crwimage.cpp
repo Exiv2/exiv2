fb3d6794e3fe2d0aea6f0bd93500baf9fd883049
ahuggel <ahuggel@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2006-07-02 15:45:30 +0000

    Minor change

--- trunk/src/crwimage.cpp
+++ trunk/src/crwimage.cpp
@@ -1138,8 +1138,7 @@ namespace Exiv2 {
 
     void CrwMap::encode(CiffHeader* pHead, const Image& image)
     {
-        for (const CrwMapping* cmi = crwMapping_;
-             cmi->ifdId_ != ifdIdNotSet; ++cmi) {
+        for (const CrwMapping* cmi = crwMapping_; cmi->ifdId_ != ifdIdNotSet; ++cmi) {
             if (cmi->fromExif_ != 0) {
                 cmi->fromExif_(image, cmi, pHead);
             }
