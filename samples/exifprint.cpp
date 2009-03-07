917080fc37211c6bdecdfca1eec5834b22b5f964
Andreas Huggel <ahuggel@gmx.net>
2009-03-07 03:23:38 +0000

    Handle unknown type name (reported by Marijn Kampf).

--- samples/exifprint.cpp
+++ samples/exifprint.cpp
@@ -28,12 +28,13 @@ try {
     }
     Exiv2::ExifData::const_iterator end = exifData.end();
     for (Exiv2::ExifData::const_iterator i = exifData.begin(); i != end; ++i) {
+        const char* tn = i->typeName();
         std::cout << std::setw(44) << std::setfill(' ') << std::left
                   << i->key() << " "
                   << "0x" << std::setw(4) << std::setfill('0') << std::right
                   << std::hex << i->tag() << " "
                   << std::setw(9) << std::setfill(' ') << std::left
-                  << i->typeName() << " "
+                  << (tn ? tn : "Unknown") << " "
                   << std::dec << std::setw(3)
                   << std::setfill(' ') << std::right
                   << i->count() << "  "
