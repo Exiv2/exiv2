c62cd8f0a4f13a24506ab2402737b1629bc694aa
cgilles <cgilles@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2010-04-20 09:38:52 +0000

    add Package url rule

--- trunk/config/exiv2.pc.cmake
+++ trunk/config/exiv2.pc.cmake
@@ -6,5 +6,6 @@ includedir=${prefix}/include
 Name: exiv2
 Description: Exif and IPTC metadata library and tools
 Version: @PACKAGE_VERSION@
+URL: ${PACKAGE_URL}
 Libs: -L${libdir} -lexiv2 @EXV_LIB_STATIC@
 Cflags: -I${includedir}
