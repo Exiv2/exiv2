3d172ef6f8d7a4c420c0a7ee4d3a8bc3ea41ab96
Andreas Huggel <ahuggel@gmx.net>
2010-10-15 07:44:48 +0000

    #736: Fix for Make starting with a dash.

--- src/makernote.cpp
+++ src/makernote.cpp
@@ -86,6 +86,7 @@ namespace Exiv2 {
     bool TiffMnRegistry::operator==(const std::string& key) const
     {
         std::string make(make_);
+        if (key.size() > 0 && key[0] == '-') return false;
         return make == key.substr(0, make.length());
     }
 
