ad222b323dda6ff9520c451d5866bdd035e35788
Robin Mills <robin@clanmills.com>
2010-12-12 16:18:26 +0000

    msvc: remove all the sample applications (to reduce project effort.  I'll restore them if requested.  msvc build environment is sufficient.

--- msvc64/editem.bat
+++ msvc64/editem.bat
@@ -1,30 +1,33 @@
-ce addmoddel\addmoddel.vcproj
-ce convert-test\convert-test.vcproj
-ce exifcomment\exifcomment.vcproj
-ce exifdata-test\exifdata-test.vcproj
-ce exifprint\exifprint.vcproj
-ce exiv2\exiv2.vcproj
-ce exiv2lib\exiv2lib.vcproj
-ce exivsimple\exivsimple.vcproj
-ce expat\expat.vcproj
-ce iotest\iotest.vcproj
-ce iptceasy\iptceasy.vcproj
-ce iptcprint\iptcprint.vcproj
-ce iptctest\iptctest.vcproj
-ce largeiptc-test\largeiptc-test.vcproj
-ce metacopy\metacopy.vcproj
-ce mmap-test\mmap-test.vcproj
-ce prevtest\prevtest.vcproj
-ce stringto-test\stringto-test.vcproj
-ce taglist\taglist.vcproj
-ce tiff-test\tiff-test.vcproj
-ce utiltest\utiltest.vcproj
-ce write-test\write-test.vcproj
-ce write2-test\write2-test.vcproj
-ce xmpparse\xmpparse.vcproj
-ce xmpparser-test\xmpparser-test.vcproj
-ce xmpsample\xmpsample.vcproj
-ce xmpsdk\xmpsdk.vcproj
+@echo off
+rem edit the project files
+rem
+rem ce addmoddel\addmoddel.vcproj
+rem ce convert-test\convert-test.vcproj
+rem ce exifcomment\exifcomment.vcproj
+rem ce exifdata-test\exifdata-test.vcproj
+rem ce exifprint\exifprint.vcproj
+    ce exiv2\exiv2.vcproj
+    ce exiv2lib\exiv2lib.vcproj
+rem ce exivsimple\exivsimple.vcproj
+    ce expat\expat.vcproj
+rem ce iotest\iotest.vcproj
+rem ce iptceasy\iptceasy.vcproj
+rem ce iptcprint\iptcprint.vcproj
+rem ce iptctest\iptctest.vcproj
+rem ce largeiptc-test\largeiptc-test.vcproj
+rem ce metacopy\metacopy.vcproj
+rem ce mmap-test\mmap-test.vcproj
+rem ce prevtest\prevtest.vcproj
+rem ce stringto-test\stringto-test.vcproj
+rem ce taglist\taglist.vcproj
+rem ce tiff-test\tiff-test.vcproj
+rem ce utiltest\utiltest.vcproj
+rem ce write-test\write-test.vcproj
+rem ce write2-test\write2-test.vcproj
+rem ce xmpparse\xmpparse.vcproj
+rem ce xmpparser-test\xmpparser-test.vcproj
+rem ce xmpsample\xmpsample.vcproj
+    ce xmpsdk\xmpsdk.vcproj
 ce zlib\zlib.vcproj
 ce exiv2.sln
 ce expat\expat.sln
