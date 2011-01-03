5ead8bdf11bdcd25ad18f3daafdec2dd7693552a
Robin Mills <robin@clanmills.com>
2011-01-03 02:10:02 +0000

    msvc64: adding tools/depends directory and code.

---
+++ msvc64/tools/depends/depends.cpp
@@ -0,0 +1,12 @@
+// Depends2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main(int argc, char argv[])
+{
+	printf("hello world\n") ;
+	return 0;
+}
+
