afeb7cdcf800ddb8dd048c70851ed303bf507a1b
vog <vog@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2011-06-17 13:52:45 +0000

    #689: provide support for EPS files
    EPS is added to the front of the registry to ensure that very small
    EPS files will be detected.
    
    This implementation refuses to deal with "read-only XMP" as well
    as "%%Include..." DSC comments, because it is unclear how to handle
    those properly. If one of these special cases occur, a warning will
    be emitted which encourages the user to provide a real-world EPS
    file to us.

---
+++ trunk/test/eps-test.sh
@@ -0,0 +1,38 @@
+#! /bin/sh
+# Test driver for EPS files
+
+exiv2="$VALGRIND ../../src/exiv2"
+
+diffargs="--strip-trailing-cr"
+tmpfile=tmp/ttt
+touch $tmpfile
+diff -q $diffargs $tmpfile $tmpfile 2>/dev/null
+if [ $? -ne 0 ] ; then
+    diffargs=""
+fi
+
+(
+    cd data/eps
+    for epsfile in *.eps; do
+        cp "$epsfile" ../../tmp/
+        (
+            cd ../../tmp
+
+            echo "Command: exiv2 -pp $epsfile"
+            $exiv2 -pp "$epsfile"
+            echo "Exit code: $?"
+            echo
+
+            echo "Command: exiv2 -pa $epsfile"
+            $exiv2 -pa "$epsfile"
+            echo "Exit code: $?"
+            echo
+        )
+    done
+) > tmp/eps-test.out 2>&1
+
+if diff -q $diffargs data/eps/eps-test.out tmp/eps-test.out ; then
+    echo "All testcases passed."
+else
+    diff -u $diffargs data/eps/eps-test.out tmp/eps-test.out
+fi
