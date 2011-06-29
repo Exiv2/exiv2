bc7d533aa22dffc110c500ab11192c1e81dc4280
vog <vog@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2011-06-29 17:40:35 +0000

    Fixed EPS test suite: use GNU SED to ensure that SED won't add a line ending at EOF which would make the test comparisons fail

--- trunk/test/eps-test.sh
+++ trunk/test/eps-test.sh
@@ -12,6 +12,7 @@ else
     bin="$VALGRIND $EXIV2_BINDIR"
     samples="$VALGRIND $EXIV2_BINDIR"
 fi
+gsed=`gsed --help >/dev/null 2>&1 && echo g`sed
 exiv2version="`$bin/exiv2 -V | sed -n '1 s,^exiv2 \([^ ]*\).*,\1,p'`"
 if [ -z "$exiv2version" ]; then
     echo "Error: Unable to determine Exiv2 version"
@@ -73,7 +74,7 @@ done
             continue
         fi
 
-        sed "s,@Exiv2Version@,$exiv2version," < "../data/eps/$image.eps.newxmp" > "$image.eps.newxmp"
+        $gsed "s,@Exiv2Version@,$exiv2version," < "../data/eps/$image.eps.newxmp" > "$image.eps.newxmp"
 
         if ! diff -q "$image.eps.newxmp" "$image.eps" ; then
             continue
