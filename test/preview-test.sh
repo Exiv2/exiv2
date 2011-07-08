5eb9aefc67ab58f79d500f22ee4f26b1b57c2f7b
vog <vog@notjusthosting.com>
2011-07-08 05:05:00 +0000

    Improved XMP embedding/replace mechanism for EPS files, achieved more flexibility with less code, added and adjusted test cases

--- test/preview-test.sh
+++ test/preview-test.sh
@@ -44,6 +44,7 @@ images="eps/eps-flat_inkscape-epsi.eps \
         eps/eps-flat_photoshop-cs5-binary_exiv2-bigxmp.eps \
         eps/eps-flat_photoshop-cs5-binary_exiv2.eps \
         eps/eps-flat_photoshop-cs5-binary_exiv2_missing-begin-xml-packet.eps \
+        eps/eps-flat_photoshop-cs5-binary_no-adocontainsxmp.eps \
         eps/eps-flat_photoshop-e9-win-doseps.eps \
         eps/eps-flat_photoshop-e9-win.eps \
         eps/eps-flat_photoshop-e9-win_exiv2.eps \
