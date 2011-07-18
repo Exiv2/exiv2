80f3b46c0a557ed91e24416be97418fa7068174a
vog <vog@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2011-07-18 15:24:35 +0000

    Added helper function decodeHex() that is needed to decode some types of EPS previews

--- trunk/src/preview.cpp
+++ trunk/src/preview.cpp
@@ -68,6 +68,11 @@ namespace {
     }
 
     /*!
+      @brief Decode a Hex string.
+     */
+    DataBuf decodeHex(const byte *src, long srcSize);
+
+    /*!
       @brief Decode a Base64 string.
      */
     DataBuf decodeBase64(const std::string &src);
@@ -863,6 +868,40 @@ namespace {
     bool LoaderXmpJpeg::readDimensions()
     {
         return valid();
+    }
+
+    DataBuf decodeHex(const byte *src, long srcSize)
+    {
+        // create decoding table
+        byte invalid = 16;
+        byte decodeHexTable[256];
+        for (long i = 0; i < 256; i++) decodeHexTable[i] = invalid;
+        for (byte i = 0; i < 10; i++) decodeHexTable[static_cast<byte>('0') + i] = i;
+        for (byte i = 0; i < 6; i++) decodeHexTable[static_cast<byte>('A') + i] = i + 10;
+        for (byte i = 0; i < 6; i++) decodeHexTable[static_cast<byte>('a') + i] = i + 10;
+
+        // calculate dest size
+        long validSrcSize = 0;
+        for (long srcPos = 0; srcPos < srcSize; srcPos++) {
+            if (decodeHexTable[src[srcPos]] != invalid) validSrcSize++;
+        }
+        const long destSize = validSrcSize / 2;
+
+        // allocate dest buffer
+        DataBuf dest(destSize);
+
+        // decode
+        for (long srcPos = 0, destPos = 0; destPos < destSize; destPos++) {
+            byte buffer = 0;
+            for (int bufferPos = 1; bufferPos >= 0 && srcPos < srcSize; srcPos++) {
+                byte srcValue = decodeHexTable[src[srcPos]];
+                if (srcValue == invalid) continue;
+                buffer |= srcValue << (bufferPos * 4);
+                bufferPos--;
+            }
+            dest.pData_[destPos] = buffer;
+        }
+        return dest;
     }
 
     static const char encodeBase64Table[64 + 1] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
