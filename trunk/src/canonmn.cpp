f051b4bb7e3c580971bd40bc472ae78f8e9f9fd5
ahuggel <ahuggel@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2011-09-04 08:10:32 +0000

    Round the Exif.CanonSi.MeasuredEV properly.

--- trunk/src/canonmn.cpp
+++ trunk/src/canonmn.cpp
@@ -20,14 +20,14 @@
  */
 /*
   File:      canonmn.cpp
-  Version:   $Rev: 2478 $
+  Version:   $Rev$
   Author(s): Andreas Huggel (ahu) <ahuggel@gmx.net>
              David Cannings (dc) <david@edeca.net>
              Andi Clemens (ac) <andi.clemens@gmx.net>
  */
 // *****************************************************************************
 #include "rcsid_int.hpp"
-EXIV2_RCSID("@(#) $Id: canonmn.cpp 2478 2011-06-17 03:08:42Z ahuggel $")
+EXIV2_RCSID("@(#) $Id$")
 
 // *****************************************************************************
 // included header files
@@ -1358,8 +1358,8 @@ namespace Exiv2 {
             // see also printSi0x0017
             std::ostringstream oss;
             oss.copyfmt(os);
-            os << std::fixed << std::setprecision(2)
-               << value.toLong() / 32.0 + 5.0;
+            int res = static_cast<int>(100.0 * (value.toLong() / 32.0 + 5.0) + 0.5);
+            os << std::fixed << std::setprecision(2) << res / 100.0;
             os.copyfmt(oss);
         }
         return os;
