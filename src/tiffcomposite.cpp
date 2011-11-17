1c32314c107b9b1d07f833081cbd070a771c75d3
Andreas Huggel <ahuggel@gmx.net>
2011-11-17 04:30:48 +0000

    #797: TiffBinaryArray::doCount(): Replaced assertion with a check for type size which sets it to 1 for unknown types. (Clint Rogers)

--- src/tiffcomposite.cpp
+++ src/tiffcomposite.cpp
@@ -1046,7 +1046,17 @@ namespace Exiv2 {
 
         TypeId typeId = toTypeId(tiffType(), tag(), group());
         long typeSize = TypeInfo::typeSize(typeId);
-        assert(typeSize != 0);
+        if (0 == typeSize) {
+#ifndef SUPPRESS_WARNINGS
+            EXV_WARNING << "Directory " << groupName(group())
+                        << ", entry 0x" << std::setw(4)
+                        << std::setfill('0') << std::hex << tag()
+                        << " has unknown Exif (TIFF) type " << std::dec << tiffType()
+                        << "; setting type size 1.\n";
+#endif
+            typeSize = 1;
+        }
+
         return static_cast<uint32_t>(static_cast<double>(size()) / typeSize + 0.5);
     }
 
