cb60ef8b19e9a84a7d7d1105b9857986155fd2f3
ahuggel <ahuggel@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2012-01-19 17:50:16 +0000

    #799: Added command file for tests.

---
+++ trunk/test/data/bug799.cmd
@@ -0,0 +1,14 @@
+set Xmp.MP.RegionInfo/MPRI:Regions ''
+set Xmp.MP.RegionInfo/MPRI:Regions[1]/MPReg:Rectangle 0.11, 0.22, 0.33, 0.44
+set Xmp.MP.RegionInfo/MPRI:Regions[1]/MPReg:PersonDisplayName Baby Gnu
+set Xmp.mwg-rs.Regions/mwg-rs:AppliedToDimensions/stDim:w 1600
+set Xmp.mwg-rs.Regions/mwg-rs:AppliedToDimensions/stDim:h 800
+set Xmp.mwg-rs.Regions/mwg-rs:AppliedToDimensions/stDim:unit pixel
+set Xmp.mwg-rs.Regions/mwg-rs:RegionList ''
+set Xmp.mwg-rs.Regions/mwg-rs:RegionList[1]/mwg-rs:Name Baby Gnu
+set Xmp.mwg-rs.Regions/mwg-rs:RegionList[1]/mwg-rs:Type Face
+set Xmp.mwg-rs.Regions/mwg-rs:RegionList[1]/mwg-rs:Area/stArea:x 0.275312
+set Xmp.mwg-rs.Regions/mwg-rs:RegionList[1]/mwg-rs:Area/stArea:y 0.3775
+set Xmp.mwg-rs.Regions/mwg-rs:RegionList[1]/mwg-rs:Area/stArea:w 0.164375
+set Xmp.mwg-rs.Regions/mwg-rs:RegionList[1]/mwg-rs:Area/stArea:h 0.28125
+set Xmp.mwg-rs.Regions/mwg-rs:RegionList[1]/mwg-rs:Area/stArea:unit normalized
