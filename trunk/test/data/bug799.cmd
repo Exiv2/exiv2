9df8d72f3e4a7540056d881cd02f227a4f479a4c
ahuggel <ahuggel@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2012-01-20 17:02:04 +0000

    Use the new method to set a tag without any value in a test.

--- trunk/test/data/bug799.cmd
+++ trunk/test/data/bug799.cmd
@@ -1,10 +1,10 @@
-set Xmp.MP.RegionInfo/MPRI:Regions ''
+set Xmp.MP.RegionInfo/MPRI:Regions
 set Xmp.MP.RegionInfo/MPRI:Regions[1]/MPReg:Rectangle 0.11, 0.22, 0.33, 0.44
 set Xmp.MP.RegionInfo/MPRI:Regions[1]/MPReg:PersonDisplayName Baby Gnu
 set Xmp.mwg-rs.Regions/mwg-rs:AppliedToDimensions/stDim:w 1600
 set Xmp.mwg-rs.Regions/mwg-rs:AppliedToDimensions/stDim:h 800
 set Xmp.mwg-rs.Regions/mwg-rs:AppliedToDimensions/stDim:unit pixel
-set Xmp.mwg-rs.Regions/mwg-rs:RegionList ''
+set Xmp.mwg-rs.Regions/mwg-rs:RegionList
 set Xmp.mwg-rs.Regions/mwg-rs:RegionList[1]/mwg-rs:Name Baby Gnu
 set Xmp.mwg-rs.Regions/mwg-rs:RegionList[1]/mwg-rs:Type Face
 set Xmp.mwg-rs.Regions/mwg-rs:RegionList[1]/mwg-rs:Area/stArea:x 0.275312
