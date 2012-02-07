a16b2c005283411a7f707d2bc6341eae16907c3a
HumanDynamo <caulier.gilles@gmail.com>
2012-02-07 09:15:33 +0000

    another KIPI xmp tag, this time used with Yandex Fotki web service to identify item uploaded

--- src/properties.cpp
+++ src/properties.cpp
@@ -188,7 +188,8 @@ namespace Exiv2 {
     extern const XmpPropertyInfo xmpKipiInfo[] = {
         { "EnfuseInputFiles",       N_("Enfuse Input Files"),        "Text",     xmpText, xmpExternal, N_("The list of files processed with Enfuse program through ExpoBlending tool.") },
         { "EnfuseSettings",         N_("Enfuse Settings"),           "Text",     xmpText, xmpExternal, N_("The list of Enfuse settings used to blend image stack with ExpoBlending tool.") },
-        { "picasawebGPhotoId",      N_("PicasaWeb Item ID"),         "Text",     xmpText, xmpExternal, N_("Item ID from PicasaWeb service.") },
+        { "picasawebGPhotoId",      N_("PicasaWeb Item ID"),         "Text",     xmpText, xmpExternal, N_("Item ID from PicasaWeb web service.") },
+        { "yandexGPhotoId",         N_("Yandex Fotki Item ID"),      "Text",     xmpText, xmpExternal, N_("Item ID from Yandex Fotki web service.") },
         // End of list marker
         { 0, 0, 0, invalidTypeId, xmpInternal, 0 }
     };
