7860641d59d29665956cb61118c0031376eaef1a
robinwmills <robinwmills@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2012-04-24 04:39:01 +0000

    simplified mingfixup.sh

--- trunk/test/mingfixup.sh
+++ trunk/test/mingfixup.sh
@@ -4,14 +4,9 @@
 # for MING, ensure that we have .exe files in src and samples
 os=$(uname)
 if [ "${os:0:4}" == "MING" ]; then
-	for dir in ../src/.libs ../samples/.libs; do
-		for file in $dir/*.dll $dir/*.exe ; do
-			leaf=$(basename $file)
-			if [ ! -e $dir/../$leaf ]; then
-				cp $file $dir/.. 2>/dev/null >/dev/null
-			fi
-		done
-	done
+	cp ../src/.libs/*.exe     ../src/
+	cp ../src/.libs/*.dll     ../src/
+	cp ../samples/.libs/*.exe ../samples/
 fi
 
 # That's all Folks!
