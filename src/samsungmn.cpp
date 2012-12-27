b0202f1e4de98936d5e3975844249acd304f0e33
Robin Mills <robin@clanmills.com>
2012-12-27 17:53:24 +0000

    Issue: #834.  Thanks Jaroslav and Pascal for the discovery/patches/solution.

--- src/samsungmn.cpp
+++ src/samsungmn.cpp
@@ -50,14 +50,17 @@ namespace Exiv2 {
 
     //! LensType, tag 0xa003
     extern const TagDetails samsung2LensType[] = {
-        { 0, N_("Built-in")                              },
-        { 1, N_("Samsung 30mm F2 Pancake")               },
-        { 2, N_("Samsung Zoom 18-55mm F3.5-5.6 OIS")     },
-        { 3, N_("Samsung Zoom 50-200mm F4-5.6 ED OIS")   },
-        { 4, N_("Samsung 20-50mm F3.5-5.6 Compact Zoom") },
-        { 5, N_("Samsung 20mm F2.8 Pancake")             },
-        { 7, N_("Samsung 60mm F2.8 Macro ED OIS SSA")    },
-        { 8, N_("Samsung 16mm F2.4 Ultra Wide Pancake")  }
+        {  0, N_("Built-in")                              },
+        {  1, N_("Samsung NX 30mm F2 Pancake")            },
+        {  2, N_("Samsung NX 18-55mm F3.5-5.6 OIS")       },
+        {  3, N_("Samsung NX 50-200mm F4-5.6 ED OIS")     },
+        {  4, N_("Samsung NX 20-50mm F3.5-5.6 ED")        },
+        {  6, N_("Samsung NX 18-200mm F3.5-6.3 ED OIS")   },
+        {  5, N_("Samsung NX 20mm F2.8 Pancake")          },
+        {  7, N_("Samsung NX 60mm F2.8 Macro ED OIS SSA") },
+        {  8, N_("Samsung NX 16mm F2.4 Pancake")          },
+        {  9, N_("Samsung NX 85mm F1.4 ED SSA")           },
+        { 12, N_("Samsung NX 12-24mm F4-5.6 ED")          }
     };
 
     //! ColorSpace, tag 0xa011
