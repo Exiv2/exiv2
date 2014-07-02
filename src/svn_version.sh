290baeea7249a654fa8ef9d023533b769ecbfa06
Robin Mills <robin@clanmills.com>
2014-07-02 22:18:29 +0000

    svn_version: strengthened to handle errors from svn (for example when building from the distribution tar bundle)

--- src/svn_version.sh
+++ src/svn_version.sh
@@ -2,7 +2,8 @@
 
 ## 
 # update svn_version.h when the revision has changed
-svn_version=$(svn info .. | grep ^Revision | cut -f 2 -d' ')
+svn_version=$(svn info .. 2>/dev/null | grep ^Revision | cut -f 2 -d' ')
+if [ -z "$svn_version" ]; then svn_version=0 ; fi
 if [ -e svn_version.h ]; then
 	old=$(cut -f 3 -d' ' svn_version.h)
 	if [ "$old" != "$svn_version" ]; then
