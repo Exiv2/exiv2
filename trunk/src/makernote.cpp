d26ce5dff022872104367a995b5ae17d33d39d58
nkbj <nkbj@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2014-09-21 11:07:10 +0000

    Issue #919: Handle Pentax makernotes in samsung-rebranded cameras. Thanks goes to Michael Karcher for the patch.

--- trunk/src/makernote.cpp
+++ trunk/src/makernote.cpp
@@ -956,13 +956,23 @@ namespace Exiv2 {
     TiffComponent* newSamsungMn(uint16_t    tag,
                                 IfdId       group,
                                 IfdId       mnGroup,
-                                const byte* /*pData*/,
+                                const byte* pData,
                                 uint32_t    size,
                                 ByteOrder   /*byteOrder*/)
     {
-        // Require at least an IFD with 1 entry
-        if (size < 18) return 0;
-        return newSamsungMn2(tag, group, mnGroup);
+        if (   size > 4
+            && std::string(reinterpret_cast<const char*>(pData), 4) == std::string("AOC\0", 4)) {
+            // Samsung branded Pentax camera:
+            // Require at least the header and an IFD with 1 entry
+            if (size < PentaxMnHeader::sizeOfSignature() + 18) return 0;
+            return newPentaxMn2(tag, group, pentaxId);
+        }
+        else {
+            // Genuine Samsung camera:
+            // Require at least an IFD with 1 entry
+            if (size < 18) return 0;
+            return newSamsungMn2(tag, group, mnGroup);
+        }
     }
 
     TiffComponent* newSamsungMn2(uint16_t tag,
