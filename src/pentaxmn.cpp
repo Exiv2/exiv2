bc2f4174201227b9fd266b6c1ede82a1dede8dee
nkbj <nkbj1970@hotmail.com>
2014-11-05 17:25:04 +0000

    Update lens lists to match ExifTool version 9.75.

--- src/pentaxmn.cpp
+++ src/pentaxmn.cpp
@@ -905,6 +905,7 @@ namespace Exiv2 {
         { 0x081e, "Sigma 17-70mm F2.8-4 DC Macro HSM Contemporary" },
         { 0x081f, "Sigma 18-35mm F1.8 DC HSM" },
         { 0x0820, "Sigma 30mm F1.4 DC HSM | A" },
+        { 0x08c8, "HD PENTAX-DA 16-85mm F3.5-5.6 ED DC WR" },
         { 0x08d1, "HD PENTAX-DA 20-40mm F2.8-4 ED Limited DC WR" },
         { 0x08d2, "smc PENTAX-DA 18-270mm F3.5-6.3 ED SDM" },
         { 0x08d3, "HD PENTAX-DA 560mm F5.6 ED AW" },
