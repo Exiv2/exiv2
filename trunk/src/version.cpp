c9e24945948a308286877de4cf79f74bda8e5179
robinwmills <robinwmills@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2014-12-03 19:49:34 +0000

    #1007.  Thank You, Nicolas for reporting this and identifying the fix.

--- trunk/src/version.cpp
+++ trunk/src/version.cpp
@@ -116,7 +116,6 @@ typedef string_v::iterator  string_i;
 #elif defined(__linux__)
 # include <unistd.h>
 // http://syprog.blogspot.com/2011/12/listing-loaded-shared-objects-in-linux.html
-# include "link.h"
 # include <dlfcn.h>
   struct something
   {
