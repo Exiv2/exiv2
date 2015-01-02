f2a247fb625bc2b0575ce016b53d20aa853cc3b0
Robin Mills <robin@clanmills.com>
2015-01-02 09:05:37 +0000

    #1012.  Thank you, Thomas, for reporting this a providing the patch.

--- include/exiv2/http.hpp
+++ include/exiv2/http.hpp
@@ -1,11 +1,9 @@
-#ifndef HTTP_H_
-#define HTTP_H_
+#ifndef HTTP_HPP_
+#define HTTP_HPP_
 
 #include <string>
 #include <map>
 #include <algorithm>
-
-#include <exiv2/exiv2.hpp>
 
 namespace Exiv2 {
     typedef std::map<std::string,std::string> dict_t;
@@ -15,5 +13,3 @@ namespace Exiv2 {
 
 }
 #endif
-
-
