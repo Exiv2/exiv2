c4d3471b7bd96cd024984bf2203acf26b399b848
robinwmills <robinwmills@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2015-01-02 09:49:16 +0000

    #1012. r3508 broke the build on macosx and linux. Fixed cosmetic issues in samples/httptest.cpp and <exiv2dir>/Makefile.

--- trunk/include/exiv2/http.hpp
+++ trunk/include/exiv2/http.hpp
@@ -5,6 +5,8 @@
 #include <map>
 #include <algorithm>
 
+#include <exiv2/exiv2.hpp>
+
 namespace Exiv2 {
     typedef std::map<std::string,std::string> dict_t;
     typedef dict_t::iterator                  dict_i;
@@ -12,4 +14,9 @@ namespace Exiv2 {
     EXIV2API int http(dict_t& request,dict_t& response,std::string& errors);
 
 }
+
+#if EXV_USE_CURL
+#include <curl/curl.h>
+#endif
+
 #endif
--- trunk/samples/httptest.cpp
+++ trunk/samples/httptest.cpp
@@ -9,11 +9,6 @@
 #include <stdlib.h>
 using namespace std;
 
-#if     defined(_MSC_VER)
-#pragma comment(lib, "ws2_32.lib")
-#pragma comment(lib, "wldap32.lib")
-#endif
-
 static int testSyntax(const char* arg)
 {
 	if ( !arg ) {
