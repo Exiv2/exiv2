9f3e11c829492af9d0fdeccdceb90fb8c4da80f0
robinwmills <robinwmills@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2015-03-11 10:55:05 +0000

    #1039.  Thank You, Torsten for raising this matter.  Thank You, Phil for your help with this.

--- trunk/src/types.cpp
+++ trunk/src/types.cpp
@@ -49,6 +49,7 @@ EXIV2_RCSID("@(#) $Id$")
 #include <cstdlib>
 #include <cassert>
 #include <cstring>
+#include <cmath>
 
 // *****************************************************************************
 namespace {
@@ -186,12 +187,21 @@ namespace Exiv2 {
 
     std::istream& operator>>(std::istream& is, Rational& r)
     {
-        int32_t nominator;
-        int32_t denominator;
-        char c('\0');
-        is >> nominator >> c >> denominator;
-        if (c != '/') is.setstate(std::ios::failbit);
-        if (is) r = std::make_pair(nominator, denominator);
+        // http://dev.exiv2.org/boards/3/topics/1912?r=1915
+        if ( std::tolower(is.peek()) == 'f' ) {
+            char  F;
+            float f;
+            is >> F >> f ;
+            f  = 2.0f * std::log(f) / std::log(2.0f) ;
+            r  = Exiv2::floatToRationalCast(f);
+        } else {
+            int32_t nominator;
+            int32_t denominator;
+            char c('\0');
+            is >> nominator >> c >> denominator;
+            if (c != '/') is.setstate(std::ios::failbit);
+            if (is) r = std::make_pair(nominator, denominator);
+        }
         return is;
     }
 
@@ -202,12 +212,21 @@ namespace Exiv2 {
 
     std::istream& operator>>(std::istream& is, URational& r)
     {
-        uint32_t nominator;
-        uint32_t denominator;
-        char c('\0');
-        is >> nominator >> c >> denominator;
-        if (c != '/') is.setstate(std::ios::failbit);
-        if (is) r = std::make_pair(nominator, denominator);
+        // http://dev.exiv2.org/boards/3/topics/1912?r=1915
+        if ( std::tolower(is.peek()) == 'f' ) {
+            char  F;
+            float f;
+            is >> F >> f ;
+            f  = 2.0f * std::log(f) / std::log(2.0f) ;
+            r  = Exiv2::floatToRationalCast(f);
+        } else {
+            uint32_t nominator;
+            uint32_t denominator;
+            char c('\0');
+            is >> nominator >> c >> denominator;
+            if (c != '/') is.setstate(std::ios::failbit);
+            if (is) r = std::make_pair(nominator, denominator);
+        }
         return is;
     }
 
