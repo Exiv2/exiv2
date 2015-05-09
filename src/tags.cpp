dcead6da9c04e8ee739b69480df98c8250a86641
asp <alan.pater@gmail.com>
2015-05-09 23:49:36 +0000

    New translations from Launchpad. Updates to es.

--- src/tags.cpp
+++ src/tags.cpp
@@ -619,7 +619,7 @@ namespace Exiv2 {
                 N_("The number of inks. Usually equal to SamplesPerPixel, unless there are extra samples."),
                 ifd0Id, imgStruct, unsignedShort, 1, printValue), // TIFF tag
         TagInfo(0x0150, "DotRange", N_("Dot Range"),
-                N_("The component values that correspond to a 0% dot and 100% dot."),
+                N_("The component values that correspond to a 0\% dot and 100\% dot."),
                 ifd0Id, imgStruct, unsignedByte, -1, printValue), // TIFF tag
         TagInfo(0x0151, "TargetPrinter", N_("Target Printer"),
                 N_("A description of the printing environment for which this separation is intended."),
