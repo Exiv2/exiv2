67aef6117a2eccec0f5b8dcef7cfd3fd6f930185
Andreas Huggel <ahuggel@gmx.net>
2015-06-07 16:29:06 +0000

    Only compile video source code if it is enabled.

--- src/matroskavideo.cpp
+++ src/matroskavideo.cpp
@@ -33,12 +33,7 @@ EXIV2_RCSID("@(#) $Id$")
 // included header files
 #include "config.h"
 
-#ifndef    EXV_ENABLE_VIDEO
-namespace  Exiv2 {
-extern int matroskavideo_extern;
-int        matroskavideo_extern=1;
-}
-#else
+#ifdef EXV_ENABLE_VIDEO
 #include "matroskavideo.hpp"
 #include "futils.hpp"
 #include "basicio.hpp"
