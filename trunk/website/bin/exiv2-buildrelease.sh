e31de7524be245e9cf5ab827755d2e43458b665f
ahuggel <ahuggel@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2015-06-21 14:15:48 +0000

    Updated build scripts.

--- trunk/website/bin/exiv2-buildrelease.sh
+++ trunk/website/bin/exiv2-buildrelease.sh
@@ -20,58 +20,75 @@ if [ -e exiv2-$rel ] ; then
     echo File exiv2-$rel already exists, exiting...
     exit 1
 fi
-
+echo
+echo ==========================================================================
 echo Exporting sources, this may take a while...
-
+echo
 path=tags/$rel
 if [ $rel = trunk ] ; then
     path=trunk
 fi
 svn export svn://dev.exiv2.org/svn/$path
 mv $rel exiv2-$rel
-
-# Prepare the source code
+echo
+echo ==========================================================================
+echo Preparing the source code
+echo
 cd exiv2-$rel
 make config
 ./configure --disable-shared
-make -j3
+make -j4
 sudo make install
-make samples
+make -j4 samples
 make doc
 cd test
 make
 cd .. 
 rm -f ABOUT-NLS
 rm -f .gitignore
-rm -rf test/
 rm -rf kdevelop/
+sudo make uninstall
 make distclean
+rm -rf test/
 rm -f Makefile
 rm -f bootstrap.linux
 rm -f msvc64\\runner.txt
 # 01-Dec-2013, ahu: Include CMake files in the distribution
 #find . -type f -name '*[Cc][Mm][Aa][Kk][Ee]*' | xargs rm -f
 rm -rf xmpsdk/src/.libs
-
-# Create source and doc packages
+rm -f config.log
+rm -rf website
+rm -f jenkins_build.bat jenkins_build.sh
+rm -f fixxml.sh
+echo
+echo ==========================================================================
+echo Creating source and doc packages
+echo
 cd ..
 tar zcvf exiv2-$rel-doc.tar.gz exiv2-$rel/doc/index.html exiv2-$rel/doc/html exiv2-$rel/doc/include
 rm -rf exiv2-$rel/doc/html
 tar zcvf exiv2-$rel.tar.gz exiv2-$rel
-
-# Test the tarball: unpack, build and run tests
+echo
+echo ==========================================================================
+echo Testing the tarball: unpack, build and run tests
+echo
 rm -rf exiv2-$rel
 tar zxvf exiv2-$rel.tar.gz
 cd exiv2-$rel
 ./configure
-make -j5
-make install
-make samples
+make -j4
+sudo make install
+make -j4 samples
 echo Exporting tests, this may take a while...
 svn export svn://dev.exiv2.org/svn/tags/$rel/test
 du -sk test/
 cd test
 make
 cd ../..
+echo
+echo ==========================================================================
+echo Error-summary
+echo
+grep 'Error ' exiv2-buildrelease-$rel.out | grep -v -e'BasicError ' -e'Error 1 (ignored)'
 
 ) 2>&1 | tee exiv2-buildrelease-$rel.out 2>&1
