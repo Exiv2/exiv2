9e9b1c8d772fa73dc2795a8dc7026285225ebdda
asp <asp@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2015-08-05 17:39:12 +0000

    #1093 ExifEX for XMP namespace and properties

--- trunk/src/xmp.cpp
+++ trunk/src/xmp.cpp
@@ -419,6 +419,7 @@ namespace Exiv2 {
             SXMPMeta::RegisterNamespace("http://www.metadataworkinggroup.com/schemas/regions/", "mwg-rs");
             SXMPMeta::RegisterNamespace("http://www.metadataworkinggroup.com/schemas/keywords/", "mwg-kw");
             SXMPMeta::RegisterNamespace("http://ns.adobe.com/xmp/sType/Area#", "stArea");
+	    SXMPMeta::RegisterNamespace("http://cipa.jp/exif/1.0/", "exifEX");
 
 #else
             initialized_ = true;
