0c5ec2c4115eb65c31541d4f1d0caaec3835c901
robinwmills <robinwmills@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2015-11-17 19:15:54 +0000

    #1041.  Lots of changes.  Added a command-line parser to build.cmd.  Build.cmd obtains source from the Exiv2 Team Repository.

--- trunk/contrib/cmake/msvc/build.cmd
+++ trunk/contrib/cmake/msvc/build.cmd
@@ -1,39 +1,112 @@
 @echo off
-SETLOCAL
+setlocal enableextensions
 
-REM ---------------------------------------------------
-REM -- Created by danielkaneider
-REM ---------------------------------------------------
+set "BUILD_DIR=%CD%"
 
-echo calling setenv
+:GETOPTS
+if /I "%1" == "--help" (
+   call:Help
+   exit /b
+)
+if /I "%1" == "--webready"        set "_WEBREADY_=1"
+if /I "%1" == "--config"          set "_CONFIG_=%2"& shift
+if /I "%1" == "--temp"            set "_TEMP_=%2"& shift
+if /I "%1" == "--generator"       set "_GENERATOR_=%2"& shift
+if /I "%1" == "--exiv2"           set "_EXIV2_=%2"& shift
+if /I "%1" == "--verbose"         set "_VERBOSE_=1"
+if /I "%1" == "--dryrun"          set "_DRYRUN_=1"
+if /I "%1" == "--rebuild"         set "_REBUILD_=1"
+if /I "%1" == "--silent"          set "_SILENT_=1"
+if /I "%1" == "--silent"          set "_QUIET_=1"
+if /I "%1" == "--quiet"           set "_QUIET_=1"
+
+shift
+if not (%1) EQU () goto GETOPTS
+
+rem  ----
+call:echo calling setenv
 call setenv.cmd
 IF ERRORLEVEL 1 (
 	echo "*** setenv.cmd has failed ***"
 	GOTO error_end
 )
 
-IF EXIST ..\msvc (
-	echo.
-	echo.ERROR: This file should NOT be executed within the exiv2 source directory,
-	echo.       but in a new empty folder!
-	echo.
-	goto error_end
-)
+rem if NOT DEFINED _DRYRUN_ if DEFINED _REBUILD_ (
+rem 	for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)  	
+rem )
 
-echo testing compiler
-cl > NUL
-IF ERRORLEVEL 1 (
-	echo "*** ensure cl is not on path.  Run vcvars32.bat or vcvarsall.bat ***"
+rem  ----
+call:echo checking that %_EXIV2_% exists
+if NOT EXIST %_EXIV2_% (
+	echo "_EXIV2_ = %_EXIV2_% does not exist ***"
+	exit /b 1
+)
+pushd %_EXIV2_%
+set _EXIV2_=%CD%
+popd
+call:echo _EXIV2_ = %_EXIV2_%
+
+rem  ----
+call:echo testing VSINSTALLDIR "%VSINSTALLDIR%"
+IF NOT DEFINED VSINSTALLDIR (
+	echo "VSINSTALLDIR not set.  Run vcvars32.bat or vcvarsall.bat ***"
 	GOTO error_end
 )
-
-echo testing VSINSTALLDIR
 IF NOT EXIST "%VSINSTALLDIR%" (
-	echo "VSINSTALLDIR %VSINSTALLDIR% does not exist.  Ruv vcvars32.bat or vcvarsall.bat ***"
+	echo "VSINSTALLDIR %VSINSTALLDIR% does not exist.  Run vcvars32.bat or vcvarsall.bat ***"
 	GOTO error_end
 )
 
-echo testing architecture
+rem http://stackoverflow.com/questions/9252980/how-to-split-the-filename-from-a-full-path-in-batch
+for %%A in ("%VSINSTALLDIR%") do (
+    set "VSSTUDIO=%%~nA"
+)
+call:echo VSSTUDIO = "%VSSTUDIO%"
+
+rem  ----
+call:echo setting CMake command options
+IF "%VSSTUDIO%" EQU "Microsoft Visual Studio 14" (
+	rem Visual Studio 2015
+	set VS_SHORT=vc14
+	set VS_CMAKE=Visual Studio 14
+	set VS_PROG_FILES=Microsoft Visual Studio 14.0
+	set VS_OPENSSL=vs2015
+) ELSE IF "%VSSTUDIO%" EQU "Microsoft Visual Studio 12" (
+	rem Visual Studio 2013
+	set VS_SHORT=vc12
+	set VS_CMAKE=Visual Studio 12
+	set VS_PROG_FILES=Microsoft Visual Studio 12.0
+	set VS_OPENSSL=vs2013
+) ELSE IF "%VSSTUDIO%" EQU "Microsoft Visual Studio 11" (
+	rem Visual Studio 2012
+	set VS_SHORT=vc11
+	set VS_CMAKE=Visual Studio 11
+	set VS_PROG_FILES=Microsoft Visual Studio 11.0
+	set VS_OPENSSL=vs2012
+) ELSE IF "%VSSTUDIO%" EQU "Microsoft Visual Studio 10" (
+	rem Visual Studio 2010
+	set VS_SHORT=vc10
+	set VS_CMAKE=Visual Studio 10
+	set VS_PROG_FILES=Microsoft Visual Studio 10.0
+	set VS_OPENSSL=vs2010
+) ELSE IF "%VSSTUDIO%" EQU "Microsoft Visual Studio 9" (
+	rem Visual Studio 2008
+	set VS_SHORT=vc9
+	set VS_CMAKE=Visual Studio 9 2008
+	set VS_PROG_FILES=Microsoft Visual Studio 9.0
+	set VS_OPENSSL=vs2008
+) ELSE IF "%VSSTUDIO%" EQU "Microsoft Visual Studio 8" (
+	rem Visual Studio 2005
+	set VS_SHORT=vc8
+	set VS_CMAKE=Visual Studio 8 2005
+	set VS_PROG_FILES=Microsoft Visual Studio 8.0
+	set VS_OPENSSL=vs2005
+) ELSE (
+    echo "*** Unsupported version of Visual Studio in '%VSINSTALLDIR%' ***"
+	GOTO error_end
+)
+
+call:echo testing architecture
 if "%PROCESSOR_ARCHITECTURE%" EQU "AMD64" ( 
 	set Platform=x64
 	set RawPlatform=x64
@@ -44,38 +117,53 @@ if "%PROCESSOR_ARCHITECTURE%" EQU "AMD64" (
 	set CpuPlatform=ia32
 )
 
-echo testing svn is on path
+IF %Platform% EQU x64 (
+	set VS_CMAKE=%VS_CMAKE% Win64
+)
+call:echo Platform = %Platform% (%RawPlatform%)
+
+rem  ----
+call:echo testing out of source build
+dir/s exiv2.cpp >NUL 2>NUL
+IF NOT ERRORLEVEL 1 (
+	echo "*** error: do not execute this script within the exiv2 source directory ***"
+	goto error_end
+)
+
+rem  ----
+call:echo testing compiler
+cl > NUL 2>NUL
+IF ERRORLEVEL 1 (
+	echo "*** ensure cl is on path.  Run vcvars32.bat or vcvarsall.bat ***"
+	GOTO error_end
+)
+if NOT DEFINED _SILENT_ cl>/NUL
+
+rem  ----
+call:echo testing svn is on path
 svn --version > NUL
 IF ERRORLEVEL 1 (
 	echo "*** please ensure SVN.exe is on the PATH ***"
 	GOTO error_end
 )
 
-echo testing cmake is on path
+rem  ----
+call:echo testing cmake is on path
 cmake --version > NUL
 IF ERRORLEVEL 1 (
 	echo "*** please ensure cmake.exe is on the PATH ***"
 	GOTO error_end
 )
 
-echo testing curl is on path
-curl --version > NUL
-IF ERRORLEVEL 1 (
-	echo "*** please ensure cmake.exe is on the PATH ***"
-	GOTO error_end
-)
-
-echo testing the cygwin_dir exists
+rem  ----
+call:echo testing the cygwin_dir exists
 if NOT EXIST %CYGWIN_DIR% (
 	echo "*** CYGWIN_DIR %CYGWIN_DIR% does not exist ***"
 	GOTO error_end
 )
 
-IF %Platform% EQU x64 (
-	set VS_CMAKE=%VS_CMAKE% Win64
-)
-
-echo testing cygwin tools are available
+rem  ----
+call:echo testing cygwin tools are available
 SET 7Z_PATH=%CYGWIN_DIR%\lib\p7zip\7z.exe
 IF NOT EXIST %CYGWIN_DIR%\lib\p7zip\7z.exe GOTO cygwin_error
 IF NOT EXIST %CYGWIN_DIR%\bin\cp.exe       GOTO cygwin_error
@@ -85,7 +173,7 @@ IF NOT EXIST %CYGWIN_DIR%\bin\svn.exe      GOTO cygwin_error
 IF NOT EXIST %CYGWIN_DIR%\bin\tar.exe      GOTO cygwin_error
 IF NOT EXIST %CYGWIN_DIR%\bin\unzip.exe    GOTO cygwin_error
 IF NOT EXIST %CYGWIN_DIR%\bin\wget.exe     GOTO cygwin_error
-SET PATH=%PATH%;%CYGWIN_DIR%\bin
+rem SET PATH=%PATH%;%CYGWIN_DIR%\bin
 GOTO cygwin_ok
 
 :cygwin_error
@@ -102,52 +190,42 @@ echo is required
 GOTO error_end
 
 :cygwin_ok
+pushd %CYGWIN_DIR%
+set CYGWIN_DIR=%CD%
+popd
+set %PATH=%PATH%;%CYGWIN_DIR%\bin"
 
+rem  ----
+call:echo testing temporary directory
+IF NOT EXIST %_TEMP_% mkdir %_TEMP_%
+pushd "%_TEMP_%"
+set    "_TEMP_=%CD%"
+popd
+call:echo _TEMP_ = %_TEMP_% 
 
-echo setting configuration and creating working directories
-IF NOT DEFINED Configuration (
-	set Configuration=Release
-)
+rem ----
+call:echo testing INSTALL
+SET _INSTALL_=dist
+IF NOT EXIST %_INSTALL_% mkdir %_INSTALL_%
+pushd %_INSTALL_%
+set   "_INSTALL_=%CD%"
+popd
+call:echo   _INSTALL_ = %_INSTALL_%
 
+if NOT DEFINED _GENERATOR_       set "_GENERATOR_=%VS_CMAKE%"
+if /I "%_GENERATOR_%" == "NMake" set "_GENERATOR_=NMake Makefiles"
+
+call:echo setting cmake options
+IF     DEFINED _WEBREADY_ set  T=ON
+IF NOT DEFINED _WEBREADY_ set  T=OFF
+                          set "_WEBREADY_=-DEXIV2_ENABLE_WEBREADY=%T%"
+rem  ----
 echo.
-echo.--- %VS_CMAKE% ---
-echo.Configuration = %Configuration%
-echo.Platform = %Platform% (%RawPlatform%)
-echo.
+echo config = %_CONFIG_% webready = %_WEBREADY_% _EXIV2_= %_EXIV2_%"
 
-IF NOT EXIST %TEMP_DIR% (
-	mkdir %TEMP_DIR%
-)
-
-IF NOT EXIST vcDlls (
-	mkdir vcDlls
-	robocopy "%vcinstalldir%redist\%RawPlatform%" vcDlls /MIR >nul
-)
-
-IF NOT EXIST vcDlls\selected (
-	mkdir vcDlls\selected
-	%CYGWIN_DIR%\bin\cp.exe vcDlls/**/vcomp* vcDlls/selected
-	%CYGWIN_DIR%\bin\cp.exe vcDlls/**/msv*   vcDlls/selected
-)
-
-echo setting INSTALL_DIR
-SET INSTALL_DIR=dist
-IF NOT EXIST %INSTALL_DIR% (
-	mkdir %INSTALL_DIR%
-	pushd %INSTALL_DIR%
-	set "INSTALL_DIR=%CD%"
-	popd
-)
-echo INSTALL_DIR = %INSTALL_DIR%
-
+IF DEFINED _DRYRUN_ exit /b 1
 
 rem ---- location of library code ---------
-rem  https://github.com/madler/zlib/commits
-SET ZLIB_COMMIT_LONG=50893291621658f355bc5b4d450a8d06a563053d
-
-rem https://github.com/pol51/OpenSSL-CMake
-SET SSL_COMMIT_LONG=595a9e4384e1280659080f7c7029e66544b772a8
-
 rem https://github.com/bagder/curl
 SET CURL_COMMIT_LONG=dd39a671019d713bd077be9eed511c2dc6013598
 
@@ -155,90 +233,19 @@ rem http://www.npcglib.org/~stathis/blog/precompiled-openssl/
 SET OPENSSL_VERSION=openssl-1.0.2d
 
 echo ---------- building ZLIB ------------------
-cd "%BUILD_DIR%"
+call:buildLib zlib-1.2.8
 
-SET ZLIB_COMMIT=%ZLIB_COMMIT_LONG:~0,7%
-IF NOT EXIST %TEMP_DIR%\zlib-%ZLIB_COMMIT%.zip (
-	%CYGWIN_DIR%\bin\wget.exe -O %TEMP_DIR%/zlib-%ZLIB_COMMIT%.zip --no-check-certificate http://github.com/madler/zlib/zipball/%ZLIB_COMMIT_LONG%
-)
-
-IF NOT EXIST zlib-%ZLIB_COMMIT% (
-	%CYGWIN_DIR%\bin\unzip.exe -q %TEMP_DIR%/zlib-%ZLIB_COMMIT%.zip
-	%CYGWIN_DIR%\bin\mv.exe madler-zlib-* zlib-%ZLIB_COMMIT%
-)
-
-IF NOT EXIST zlib-%ZLIB_COMMIT%.build (
-    mkdir zlib-%ZLIB_COMMIT%.build
-    
-    pushd zlib-%ZLIB_COMMIT%.build
-	cmake -G "%VS_CMAKE%" -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% ..\zlib-%ZLIB_COMMIT%
-	IF errorlevel 1 (
-		echo "*** errors in ZLIB build [1] ***"
-	    popd
-		goto error_end
-	)
-
-	cmake --build . --config %Configuration%
-	IF errorlevel 1 (
-		echo "*** errors in ZLIB build [2] ***"
-	    popd
-		goto error_end
-	)
-
-	cmake --build . --config %Configuration% --target install
-	IF errorlevel 1 (
-		echo "*** errors in ZLIB build [3] ***"
-	    popd
-		goto error_end
-	)
-    
-    popd
-)
-
-echo ---------- building EXPAT ------------------
-cd "%BUILD_DIR%"
-
-IF NOT EXIST %TEMP_DIR%\expat-2.1.0.tar (
-	if NOT EXIST %TEMP_DIR%\expat-2.1.0.tar.gz (
-		%CYGWIN_DIR%\bin\wget.exe -O %TEMP_DIR%/expat-2.1.0.tar.gz http://sourceforge.net/projects/expat/files/expat/2.1.0/expat-2.1.0.tar.gz/download
-	)
-	%CYGWIN_DIR%\bin\gzip.exe -d %TEMP_DIR%/expat-2.1.0.tar.gz
-)
-IF NOT EXIST expat-2.1.0 (
-	%CYGWIN_DIR%\bin\tar.exe -xf %TEMP_DIR%/expat-2.1.0.tar   
-)
-
-IF NOT EXIST expat-2.1.0.build (
-    mkdir expat-2.1.0.build
-)    
-pushd expat-2.1.0.build
-
-	cmake -G "%VS_CMAKE%" -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% ..\expat-2.1.0
-	IF errorlevel 1 (
-		echo "*** errors in expat build [1] ***"
-	    popd
-		goto error_end
-	)
-
-	cmake --build . --config %Configuration%
-	IF errorlevel 1 (
-		echo "*** WARNING errors in expat build [2] ***"
-	)
-
-	cmake --build . --config %Configuration% --target install
-	IF errorlevel 1 (
-		echo "*** WARNING errors in expat build [3] ***"
-	)
-popd
-
-
+echo ---------- building EXPAT -----------------
+set "TARGET=--target expat"
+call:buildLib expat-2.1.0
+set  TARGET=
 
 rem SET OPENSSL_LONG=%OPENSSL_VERSION%-%VS_OPENSSL%
-rem IF NOT EXIST %TEMP_DIR%\%OPENSSL_LONG%.7z (
-rem 	%CYGWIN_DIR%\bin\wget.exe -O %TEMP_DIR%/%OPENSSL_LONG%.7z --no-check-certificate http://www.npcglib.org/~stathis/downloads/%OPENSSL_LONG%.7z
+rem IF NOT EXIST %_TEMP_%\%OPENSSL_LONG%.7z (
+rem 	%CYGWIN_DIR%\bin\wget.exe -O %_TEMP_%/%OPENSSL_LONG%.7z --no-check-certificate http://www.npcglib.org/~stathis/downloads/%OPENSSL_LONG%.7z
 rem )
 rem IF NOT EXIST %OPENSSL_LONG% (
-rem 	%CYGWIN_DIR%\lib\p7zip\7z.exe x %TEMP_DIR%/%OPENSSL_LONG%.7z
+rem 	%CYGWIN_DIR%\lib\p7zip\7z.exe x %_TEMP_%/%OPENSSL_LONG%.7z
 rem 	IF %Platform% EQU x64 (
 rem 		ren %OPENSSL_LONG%\lib xxlib
 rem 		ren %OPENSSL_LONG%\lib64 lib
@@ -246,17 +253,17 @@ rem 		ren %OPENSSL_LONG%\bin xxbin
 rem 		ren %OPENSSL_LONG%\bin64 bin
 rem 	)
 rem )
-rem robocopy %OPENSSL_LONG%\bin %INSTALL_DIR%\bin libeay32MD.dll /MIR /NJS >nul
-rem robocopy %OPENSSL_LONG%\bin %INSTALL_DIR%\bin ssleay32MD.dll /MIR /NJS >nul
+rem robocopy %OPENSSL_LONG%\bin %_INSTALL_%\bin libeay32MD.dll /MIR /NJS >nul
+rem robocopy %OPENSSL_LONG%\bin %_INSTALL_%\bin ssleay32MD.dll /MIR /NJS >nul
 rem 
 rem 
 rem SET CURL_COMMIT=%CURL_COMMIT_LONG:~0,7%
-rem IF NOT EXIST %TEMP_DIR%\curl-%CURL_COMMIT%.zip (
-rem 	%CYGWIN_DIR%\bin\wget.exe -O %TEMP_DIR%/curl-%CURL_COMMIT%.zip --no-check-certificate http://github.com/bagder/curl/zipball/%CURL_COMMIT_LONG%
+rem IF NOT EXIST %_TEMP_%\curl-%CURL_COMMIT%.zip (
+rem 	%CYGWIN_DIR%\bin\wget.exe -O %_TEMP_%/curl-%CURL_COMMIT%.zip --no-check-certificate http://github.com/bagder/curl/zipball/%CURL_COMMIT_LONG%
 rem )
 rem 
 rem IF NOT EXIST curl-%CURL_COMMIT% (
-rem 	%CYGWIN_DIR%\bin\unzip.exe -q %TEMP_DIR%/curl-%CURL_COMMIT%.zip
+rem 	%CYGWIN_DIR%\bin\unzip.exe -q %_TEMP_%/curl-%CURL_COMMIT%.zip
 rem 	%CYGWIN_DIR%\bin\mv.exe bagder-curl-* curl-%CURL_COMMIT%
 rem )
 rem 
@@ -264,54 +271,54 @@ rem IF NOT EXIST curl-%CURL_COMMIT%.build (
 rem     mkdir curl-%CURL_COMMIT%.build
 rem     
 rem     pushd curl-%CURL_COMMIT%.build
-rem 	cmake -G "%VS_CMAKE%" -DCMAKE_INSTALL_PREFIX=..\%INSTALL_DIR% -DCMAKE_PREFIX_PATH=..\%OPENSSL_LONG% -DBUILD_CURL_TESTS=OFF -DCMAKE_USE_OPENSSL=ON -DCMAKE_USE_LIBSSH2=OFF ..\curl-%CURL_COMMIT%
+rem 	cmake -G "%_GENERATOR_%" -DCMAKE_INSTALL_PREFIX=..\%_INSTALL_% -DCMAKE_PREFIX_PATH=..\%OPENSSL_LONG% -DBUILD_CURL_TESTS=OFF -DCMAKE_USE_OPENSSL=ON -DCMAKE_USE_LIBSSH2=OFF ..\curl-%CURL_COMMIT%
 rem 	IF errorlevel 1 goto error_end
-rem 	cmake --build . --config %Configuration%
+rem 	cmake --build . --config %_CONFIG_%
 rem 	IF errorlevel 1 goto error_end
-rem 	cmake --build . --config %Configuration% --target install
+rem 	cmake --build . --config %_CONFIG_% --target install
 rem 	IF errorlevel 1 goto error_end
 rem     
 rem     popd
 rem )
 rem 
 rem SET SSH_VERSION=0.7.2
-rem IF NOT EXIST %TEMP_DIR%\libssh-%SSH_VERSION%.zip (
-rem 	%CYGWIN_DIR%\bin\wget.exe -O %TEMP_DIR%/libssh-%SSH_VERSION%.zip --no-check-certificate https://git.libssh.org/projects/libssh.git/snapshot/libssh-%SSH_VERSION%.zip
+rem IF NOT EXIST %_TEMP_%\libssh-%SSH_VERSION%.zip (
+rem 	%CYGWIN_DIR%\bin\wget.exe -O %_TEMP_%/libssh-%SSH_VERSION%.zip --no-check-certificate https://git.libssh.org/projects/libssh.git/snapshot/libssh-%SSH_VERSION%.zip
 rem )
 rem IF NOT EXIST libssh-%SSH_VERSION% (
-rem 	%CYGWIN_DIR%\bin\unzip.exe -q %TEMP_DIR%/libssh-%SSH_VERSION%.zip
+rem 	%CYGWIN_DIR%\bin\unzip.exe -q %_TEMP_%/libssh-%SSH_VERSION%.zip
 rem )
 rem IF NOT EXIST libssh-%SSH_VERSION%.build (
 rem     mkdir libssh-%SSH_VERSION%.build
 rem     
 rem     pushd libssh-%SSH_VERSION%.build
 rem 	
-rem cmake -G "%VS_CMAKE%" -DWITH_GSSAPI=OFF -DWITH_ZLIB=ON -DWITH_SFTP=ON -DWITH_SERVER=OFF -DWITH_EXAMPLES=OFF -DWITH_NACL=OFF -DCMAKE_PREFIX_PATH=..\%OPENSSL_LONG% -DWITH_PCAP=OFF -DCMAKE_INSTALL_PREFIX=..\%INSTALL_DIR%  ..\libssh-%SSH_VERSION%
+rem cmake -G "%_GENERATOR_%" -DWITH_GSSAPI=OFF -DWITH_ZLIB=ON -DWITH_SFTP=ON -DWITH_SERVER=OFF -DWITH_EXAMPLES=OFF -DWITH_NACL=OFF -DCMAKE_PREFIX_PATH=..\%OPENSSL_LONG% -DWITH_PCAP=OFF -DCMAKE_INSTALL_PREFIX=..\%_INSTALL_%  ..\libssh-%SSH_VERSION%
 rem 
 rem 	IF errorlevel 1 goto error_end
-rem 	cmake --build . --config %Configuration%
+rem 	cmake --build . --config %_CONFIG_%
 rem 	IF errorlevel 1 goto error_end
-rem 	cmake --build . --config %Configuration% --target install
+rem 	cmake --build . --config %_CONFIG_% --target install
 rem 	IF errorlevel 1 goto error_end
 rem     
 rem     popd
 rem )
 
 
-rem IF NOT EXIST %EXIV_DIR% (
-rem     %CYGWIN_DIR%\bin\svn.exe co svn://dev.exiv2.org/svn/trunk %EXIV_DIR%
-rem ) ELSE (
-rem     %CYGWIN_DIR%\bin\svn.exe update %EXIV_DIR%
-rem )
+echo ---------- building EXIV2 ------------------
 
-echo. ---------- building EXIV2 ------------------
-cd "%BUILD_DIR%"
+set EXIV_BUILD="%_TEMP_%\exiv2"
 
-IF NOT EXIST exiv2.build (
-    mkdir exiv2.build
-)
-pushd exiv2.build
-	cmake -G "%VS_CMAKE%" -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% -DCMAKE_PROGRAM_PATH=%SVN_DIR% -DEXIV2_ENABLE_WEBREADY=OFF -DEXIV2_ENABLE_BUILD_SAMPLES=ON -DEXIV2_ENABLE_CURL=OFF -DEXIV2_ENABLE_SSH=OFF -DEXIV2_ENABLE_NLS=OFF -DEXIV2_ENABLE_WIN_UNICODE=OFF -DEXIV2_ENABLE_SHARED=ON %EXIV_DIR%
+if defined _REBUILD_ "%EXIV_BUILD%"
+IF NOT EXIST         "%EXIV_BUILD%"   mkdir "%EXIV_BUILD%"
+pushd "%EXIV_BUILD%"
+	call:run cmake -G "%_GENERATOR_%" ^
+	         "-DCMAKE_INSTALL_PREFIX=%_INSTALL_%"      "-DCMAKE_PROGRAM_PATH=%SVN_DIR%" ^
+	         "%_WEBREADY_%"                             -DEXIV2_ENABLE_BUILD_SAMPLES=ON ^
+	          -DEXIV2_ENABLE_CURL=OFF                   -DEXIV2_ENABLE_SSH=OFF          -DEXIV2_ENABLE_NLS=OFF ^
+	          -DEXIV2_ENABLE_WIN_UNICODE=OFF            -DEXIV2_ENABLE_SHARED=ON ^
+	         "-DCMAKE_LIBRARY_PATH=%INSTALL_PATH%\lib" "-DCMAKE_INCLUDE_PATH=%INSTALL_PATH%\include" ^
+	         "%_EXIV2_%"
 
 	IF errorlevel 1 (
 		echo "*** errors in EXIV2 build [1] ***"
@@ -319,25 +326,101 @@ pushd exiv2.build
 		goto error_end
 	)
 
-	cmake --build . --config %Configuration%
+	call:run cmake --build . --config %_CONFIG_%
 	IF errorlevel 1 (
 		echo "*** errors in EXIV2 build [2] ***"
 	    popd
 		goto error_end
 	)
 
-	cmake --build . --config %Configuration% --target install
+	call:run cmake --build . --config %_CONFIG_% --target install
 	IF errorlevel 1 (
 		echo "*** errors in EXIV2 build [3] ***"
 	    popd
 		goto error_end
 	)
+	copy/y "samples\%_CONFIG_%\"*.exe "%_INSTALL_%\bin"
 popd
-goto end
-    
 
+rem -----------------------------------------
+rem Exit
 :error_end
+endlocal
 REM pause
+exit /b 1
 
 :end
 endlocal
+exit /b 0
+
+rem -----------------------------------------
+rem Functions
+:help
+echo Options: --help ^| --webready ^| --dryrun ^| --verbose ^| --rebuild ^| --silent ^| --verbose
+echo.         --exiv2 directory ^| --cygwin directory ^| --temp directory ^| --config name ^| --generator generator
+exit /b 0
+
+:echo
+if NOT DEFINED _SILENT_ echo %*%
+exit /b 0
+
+:run
+if defined _VERBOSE_ (
+	echo.
+	echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	echo CD = %CD%
+	echo %*%
+)	
+if     defined _SILENT_ %*% >nul 2>nul
+if NOT defined _SILENT_ %*%
+exit /b %ERRORLEVEL%
+
+
+
+rem -------------------------------------------------------------
+:buildLib
+cd "%BUILD_DIR%"
+
+set LIB=%1%
+set LIB_T=%LIB%.tar
+set LIB_Z=%LIB%.tar.Z
+set LIB_B="%_TEMP_%\%LIB%"
+
+if defined _REBUILD_ del "%LIB_Z%"
+IF NOT EXIST             "%LIB_Z%" (
+	call:run svn export svn://dev.exiv2.org/svn/team/libraries/%LIB_Z%
+)
+
+if defined _REBUILD_ rmdir/s/q  "%LIB%"
+IF NOT EXIST                    "%LIB%" "%CYGWIN_DIR%\bin\tar.exe" xzf "%LIB_Z%"
+
+if defined _REBUILD_  rmdir/s/q "%LIB_B%"
+IF NOT EXIST %LIB_B%  mkdir     "%LIB_B%"
+
+    pushd "%LIB_B%"
+
+    call:run cmake -G "%_GENERATOR_%" -DCMAKE_INSTALL_PREFIX=%_INSTALL_% ..\..\%LIB%
+	IF errorlevel 1 (
+		echo "*** errors in %LIB% build [1] ***"
+	    popd
+		exit /b 1
+	)
+
+	call:run cmake --build . --config %_CONFIG_% %TARGET%
+	IF errorlevel 1 (
+		echo "*** warning: errors in %LIB% build [2] ***"
+	    popd
+		exit /b 0
+	)
+
+	call:run cmake --build . --config %_CONFIG_% --target install
+	IF errorlevel 1 (
+		echo "*** warning: errors in %LIB% build [3] ***"
+	    popd
+		exit /b 0
+	)
+    
+    popd
+
+exit /b 0
+
--- trunk/contrib/cmake/msvc/setenv.cmd
+++ trunk/contrib/cmake/msvc/setenv.cmd
@@ -1,84 +1,9 @@
 @echo off
 
-rem ---------------------------------------------------
-rem -- Originallly created by danielkaneider
-rem ---------------------------------------------------
-
-
-rem Options: -- begin --
-
-rem enable another configuration (defaulting to Release)
-rem SET Configuration=Debug
-SET CYGWIN_DIR=c:\cygwin64
-SET TEMP_DIR=temp
-SET EXIV_DIR=..\trunk
-SET BUILD_DIR=%CD%
-
-rem Options: --  end  --
-
-
-rem  -------------------------------------
-echo checking that EXIV_DIR exists
-if NOT EXIST %EXIV_DIR% (
-	echo "EXIV_DIR = %EXIV_DIR% does not exist ***"
-	exit /b 1
-)
-pushd %EXIV_DIR%
-set EXIV_DIR=%CD%
-popd
-echo EXIV_DIR = %EXIV_DIR%
-
-
-rem  -------------------------------------
-echo testing VSINSTALLDIR "%VSINSTALLDIR%"
-rem http://stackoverflow.com/questions/9252980/how-to-split-the-filename-from-a-full-path-in-batch
-for %%A in ("%VSINSTALLDIR%") do (
-    set "VSSTUDIO=%%~nA"
-)
-echo VSSTUDIO = "%VSSTUDIO%"
-
-rem  -------------------------------------
-echo setting CMake command options
-IF "%VSSTUDIO%" EQU "Microsoft Visual Studio 14" (
-	rem Visual Studio 2015
-	set VS_SHORT=vc14
-	set VS_CMAKE=Visual Studio 14
-	set VS_PROG_FILES=Microsoft Visual Studio 14.0
-	set VS_OPENSSL=vs2015
-) ELSE IF "%VSSTUDIO%" EQU "Microsoft Visual Studio 12" (
-	rem Visual Studio 2013
-	set VS_SHORT=vc12
-	set VS_CMAKE=Visual Studio 12
-	set VS_PROG_FILES=Microsoft Visual Studio 12.0
-	set VS_OPENSSL=vs2013
-) ELSE IF "%VSSTUDIO%" EQU "Microsoft Visual Studio 11" (
-	rem Visual Studio 2012
-	set VS_SHORT=vc11
-	set VS_CMAKE=Visual Studio 11
-	set VS_PROG_FILES=Microsoft Visual Studio 11.0
-	set VS_OPENSSL=vs2012
-) ELSE IF "%VSSTUDIO%" EQU "Microsoft Visual Studio 10" (
-	rem Visual Studio 2010
-	set VS_SHORT=vc10
-	set VS_CMAKE=Visual Studio 10
-	set VS_PROG_FILES=Microsoft Visual Studio 10.0
-	set VS_OPENSSL=vs2010
-) ELSE IF "%VSSTUDIO%" EQU "Microsoft Visual Studio 9" (
-	rem Visual Studio 2008
-	set VS_SHORT=vc9
-	set VS_CMAKE=Visual Studio 9 2008
-	set VS_PROG_FILES=Microsoft Visual Studio 9.0
-	set VS_OPENSSL=vs2008
-) ELSE IF "%VSSTUDIO%" EQU "Microsoft Visual Studio 8" (
-	rem Visual Studio 2005
-	set VS_SHORT=vc8
-	set VS_CMAKE=Visual Studio 8 2005
-	set VS_PROG_FILES=Microsoft Visual Studio 8.0
-	set VS_OPENSSL=vs2005
-) ELSE (
-    echo "*** Unsupported version of Visual Studio in '%VSINSTALLDIR%' ***"
-    exit /b 1
-)
+if not defined Configuration SET _CONFIG_=Release
+IF NOT DEFINED CYGWIN_DIR    SET CYGWIN_DIR=c:\cygwin64
+if NOT DEFINED _TEMP_        SET _TEMP_=temp
+if NOT DEFINED _EXIV2_       SET _EXIV2_=..\trunk
 
 rem That's all Folks!
 rem
