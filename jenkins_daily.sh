da86b085ae07e274748c0d9f030ac8d66f2ea43b
Robin Mills <robin@clanmills.com>
2015-12-08 13:41:58 +0000

    #1041.  Updating jenkins_daily.sh.  I'll leave this to run for a few days asking him to build everything and publish the artefacts.

--- jenkins_daily.sh
+++ jenkins_daily.sh
@@ -2,20 +2,43 @@
 
 ##
 # jenkins_daily.sh
-#
-#  environment variables (all optional)
-#    JENKINS   : URL of jenkins server. Default http://exiv2.dyndns.org:8080
+# At the moment, this performs a CMake/msvc build
+##
+
+##
+#  environment
+#  JENKINS   : URL of jenkins server. Default http://exiv2.dyndns.org:8080
 ##
 if [ -z "$JENKINS" ]; then export JENKINS=http://exiv2.dyndns.org:8080; fi
 
-export EXIV2=$(cygpath -aw .)
-rm -rf $PWD/../build
-mkdir  $PWD/../build
-PATH=$PATH:/cygdrive/c/Windows/System32:/cygdrive/c/gnu/exiv2/trunk/contrib/
-cmd.exe /c "cd $(cygpath -aw ./../build) && vcvars 2015 64 && set && cmakeBuild --rebuild --test"
-result=$?
 
-set -v
+##
+# determine location of the build and source directories
+exiv2=$(cygpath -aw .)
+build=$(cygpath -aw c:\\temp\\build)
+ dist=$(cygpath -au  c:\\temp\\build\\dist)
+
+##
+# create a clean directory for the out-of-source build
+rm    -rf $build
+mkdir -p  $build
+
+##
+# get the windows cmd to perform the build
+# set the path to find svn/7z/cmake and cmd.exe
+(
+  	PATH="c:\\gnu\\exiv2\\trunk\\contrib\\cmake\\msvc;c:\\Program files\\csvn\\bin;c:\\program files\\7-zip;c:\\program files (x86)\\cmake\\bin;$PATH:/cygdrive/c/Windows/System32;"
+  	result=0
+  	/cygdrive/c/Windows/System32/cmd.exe /c "cd $build && vcvars 2015 64 && cmakeBuild --rebuild --exiv2=$exiv2"
+  	result=$?
+)
+
+##
+# test the build
+pushd test
+	./testMSVC.sh "$dist"
+popd
+
 # That's all Folks!
 ##
-exit $result
+exit "$result"
