a0a4713830364d096eb1ca56b7d24dd0143ad776
Robin Mills <robin@clanmills.com>
2015-12-15 22:22:32 +0000

    #1109 and #1041 Adding cmake_daily.sh and testDailyAll.sh

---
+++ contrib/buildserver/testDailyAll.sh
@@ -0,0 +1,4 @@
+ssh rmills@rmillsmm                           '~/gnu/exiv2/buildserver/contrib/buildserver/test_daily.sh'
+ssh rmills@rmillsmm-kubuntu                   '~/gnu/exiv2/buildserver/contrib/buildserver/test_daily.sh'
+ssh rmills@rmillsmm-w7                        '~/gnu/exiv2/buildserver/contrib/buildserver/test_daily.sh'
+ssh rmills@rmillsmm-w7 'export PLATFORM=msvc ; ~/gnu/exiv2/buildserver/contrib/buildserver/test_daily.sh'
