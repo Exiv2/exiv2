64db9012ef92f6989e032b2fd586b7c7200b79c7
Robin Mills <robin@clanmills.com>
2015-12-16 10:52:20 +0000

    #1109 and #1041 Documentation update

---
+++ contrib/buildserver/cmakeDailyAll.sh
@@ -0,0 +1,4 @@
+ssh rmills@rmillsmm                           'cd ~/gnu/exiv2/buildserver;contrib/buildserver/cmake_daily.sh'
+ssh rmills@rmillsmm-kubuntu                   'cd ~/gnu/exiv2/buildserver;contrib/buildserver/cmake_daily.sh'
+ssh rmills@rmillsmm-w7                        'cd ~/gnu/exiv2/buildserver;contrib/buildserver/cmake_daily.sh'
+ssh rmills@rmillsmm-w7 'export PLATFORM=msvc ; cd ~/gnu/exiv2/buildserver;contrib/buildserver/cmake_daily.sh'
