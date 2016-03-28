a1634ec21a5696e3503648e8fb1918259bcc67a0
robinwmills <robinwmills@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2016-03-28 21:50:07 +0000

    #1041 Debugging msvc/test_daily.sh

--- trunk/contrib/buildserver/test_daily.sh
+++ trunk/contrib/buildserver/test_daily.sh
@@ -93,7 +93,8 @@ case $PLATFORM in
                 echo ''
                 a=''
                 if [ $arch == x64 ]; then a=64 ; fi
-		        cmd /c "vcvars.bat $vs $a && cl /EHsc -I$vs\$arch\dll\Release\include /MD samples\exifprint.cpp /link $vs\$arch\dll\Release\lib\exiv2.lib"
+		        echo cmd /c "vcvars.bat $vs $a && cl /EHsc -I$vs\$arch\dll\Release\include /MD samples\exifprint.cpp /link $vs\$arch\dll\Release\lib\exiv2.lib"
+		             cmd /c "vcvars.bat $vs $a && cl /EHsc -I$vs\$arch\dll\Release\include /MD samples\exifprint.cpp /link $vs\$arch\dll\Release\lib\exiv2.lib"
                 ls -alt exifprint.exe
                 echo ''
               )
