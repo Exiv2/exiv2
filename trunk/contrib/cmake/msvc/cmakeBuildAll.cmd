a5979b19ab107c03e35dfc7a8ec6c255145249f3
robinwmills <robinwmills@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2016-03-29 12:03:15 +0000

    #1041 Debugging msvc/test_daily.sh

--- trunk/contrib/cmake/msvc/cmakeBuildAll.cmd
+++ trunk/contrib/cmake/msvc/cmakeBuildAll.cmd
@@ -1,1 +1,1 @@
-for %%a in (64 32) do for %%v in (2008 2005) do cmd /c "dir && set && call ..\contrib\cmake\msvc\vcvars.bat %%v %%a && cmakeBuild %* 2>&1" 
+for %%a in (32) do for %%v in (2010 2008 2005) do cmd /c "vcvars %%v %%a && cmakeBuild %* 2>&1" 
