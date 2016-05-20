f9d0a92129ca65995d49e1d4a307c9d353892722
robinwmills <robinwmills@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2016-05-20 15:23:20 +0000

    Code polishing contrib/buildRevisions/buildRevisions.sh

--- trunk/contrib/buildRevisions/buildRevisions.sh
+++ trunk/contrib/buildRevisions/buildRevisions.sh
@@ -5,38 +5,35 @@
 # is appropriate for your revision investigation
 
 ##
-# test a sequence of builds
-# to list a list of revisions, use for "r in 3800 3820 3877; do"
-for r in $(seq 3800 100 3899); do
+# test a sequence of build revisions
+# to use a list of revisions, use for "r in 3800 3820 3877; do"
+for r in $(seq 3800 50 3900); do
 	echo ------------- $r begin --------------
 	##
 	# get the code
 	R="../../../$r"
 	if [ -e $R ]; then rm -rf $R ; fi
-	svn export svn://dev.exiv2.org/svn/trunk --revision $r $R | wc
+	svn checkout svn://dev.exiv2.org/svn/trunk --revision $r $R | wc
 	pushd $R
 		##
 		# build and install it
 		echo $PWD
-		make config 2>/dev/null | wc
-		./configure             | wc
-		make -j  2>&1           | wc
-		sudo make install       | wc
-		make -j samples         | wc
+		make config 2>/dev/null  | wc
+		./configure              | wc
+		make -j  2>&1            | wc
+		sudo make install        | wc
+		make -j samples 2>&1     | wc
 		echo build = $(exiv2 -vV | grep -e svn -e dll) $(ls -alt $(which exiv2))
 
 		##
 		# test the build
 		T=tst.tiff
-		curl --silent -O http://dev.exiv2.org/attachments/download/1004/$T
-		exiv2 -v -M"set Exif.Image.Software revision $r" $T
-		exiv2 -pa --grep Software $T
-
+		curl  --silent        -O http://dev.exiv2.org/attachments/download/1004/$T
+		exiv2 --verbose       -M"set Exif.Image.Software revision $r"           $T
+		exiv2 --grep Software -pa                                               $T
 	popd > /dev/null
-	echo ------------- $r end --------------
-	echo ''
+	echo ------------- $r end -------------- $'\n'
 done
 
 # That's all Folks!
 ##
-
