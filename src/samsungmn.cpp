41f14f2b7b1394c8a5e6c12d2dc5e9b24a7dd97f
nkbj <nkbj1970@hotmail.com>
2016-05-28 01:38:37 +0000

    Fix typo as reported in ExifTool version 10.18.

--- src/samsungmn.cpp
+++ src/samsungmn.cpp
@@ -63,7 +63,7 @@ namespace Exiv2 {
         { 10, "Samsung NX 45mm F1.8"                          },
         { 11, "Samsung NX 45mm F1.8 2D/3D"                    },
         { 12, "Samsung NX 12-24mm F4-5.6 ED"                  },
-        { 13, "Saumsun NX 16-50mm F2-2.8 S ED OIS"            },
+        { 13, "Samsung NX 16-50mm F2-2.8 S ED OIS"            },
         { 14, "Samsung NX 10mm F3.5 Fisheye"                  },
         { 15, "Samsung NX 16-50mm F3.5-5.6 Power Zoom ED OIS" },
         { 20, "Samsung NX 50-150mm F2.8 S ED OIS"             },
