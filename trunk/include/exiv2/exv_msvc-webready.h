bb25d0b41dfefb29e65e77e5ef8a8581042bcd04
robinwmills <robinwmills@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2016-06-23 18:30:29 +0000

    #1041  Reorganized and simplified config header files.

--- trunk/include/exiv2/exv_msvc-webready.h
+++ trunk/include/exiv2/exv_msvc-webready.h
@@ -12,38 +12,6 @@
 #ifndef _EXV_MSVC_H_
 #define _EXV_MSVC_H_
 
-#define _MSC_VER_2015 1900
-#define _MSC_VER_2013 1800
-#define _MSC_VER_2012 1700
-#define _MSC_VER_2010 1600
-#define _MSC_VER_2008 1500
-#define _MSC_VER_2005 1400
-#define _MSC_VER_2003 1300
-
-#if    _MSC_VER >= _MSC_VER_2015
-#define MSDEV_2015    1
-#elif  _MSC_VER >= _MSC_VER_2013
-#define MSDEV_2013    1
-#elif  _MSC_VER >= _MSC_VER_2012
-#define MSDEV_2012    1
-#elif  _MSC_VER >= _MSC_VER_2010
-#define MSDEV_2010    1
-#elif  _MSC_VER >= _MSC_VER_2008
-#define MSDEV_2008    1
-#elif  _MSC_VER >= _MSC_VER_2005
-#define MSDEV_2005    1
-#else
-#define MSDEV_2003    1
-#endif
-
-#if _MSC_VER >= _MSC_VER_2010
-#define EXV_HAVE_STDINT_H 1
-#endif
-
-#if _MSC_VER_ == _MSC_VER_2012
-#define HAVE_NTOHLL 1
-#endif
-
 /* Define to 1 if you have the <regex.h> header file. */
 // #define EXV_HAVE_REGEX 1
 
@@ -123,13 +91,6 @@ typedef int pid_t;
 #define CURL_STATICLIB
 #endif
 
-#pragma comment(lib, "ws2_32.lib")
-#pragma comment(lib, "wldap32.lib")
-
-/* File path separator */
-#define EXV_SEPARATOR_STR "\\"
-#define EXV_SEPARATOR_CHR '\\'
-
 /* Windows unicode path support */
 // #define EXV_UNICODE_PATH
 
@@ -139,46 +100,6 @@ typedef int pid_t;
 /* Define to 1 if you have the `munmap' function. */
 /* #undef EXV_HAVE_MUNMAP */
 
-/* Shared library support */
-#ifdef  EXV_HAVE_DLL
-#define EXV_IMPORT __declspec(dllimport)
-#define EXV_EXPORT __declspec(dllexport)
-#define EXV_DLLLOCAL
-#define EXV_DLLPUBLIC
-#else
-#define EXV_IMPORT
-#define EXV_EXPORT
-#define EXV_DLLLOCAL
-#define EXV_DLLPUBLIC
-#define EXIV2API
-#endif
-
-/* Define EXIV2API for DLL builds */
-#ifdef   EXV_HAVE_DLL
-#  ifdef EXV_BUILDING_LIB
-#    define EXIV2API EXV_EXPORT
-#  else
-#    define EXIV2API EXV_IMPORT
-#  endif /* ! EXV_BUILDING_LIB */
-#else
-#  define EXIV2API
-#endif /* ! EXV_HAVE_DLL */
-
-
-/*
-  Disable warning 4251.  This is warning from std templates about exporting interfaces
-*/
-#ifdef  EXV_HAVE_DLL
-#pragma warning( disable : 4251 )
-#endif
-/*
-  Visual Studio C++ 2005 (8.0)
-  Disable warnings about 'deprecated' standard functions
-  See, eg. http://www.informit.com/guides/content.asp?g=cplusplus&seqNum=259
-*/
-#if _MSC_VER >= 1400
-# pragma warning(disable : 4996)
-#endif
 
 #endif /* _EXV_MSVC_H_ */
 #endif /* _MSC_VER     */
