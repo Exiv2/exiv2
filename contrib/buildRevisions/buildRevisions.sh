c6953207c393807ca70601b38d904a083a0d35ce
Robin Mills <robin@clanmills.com>
2016-09-20 19:39:28 +0000

    #1011 make teste is broken between r3450 and r3470 (webready integration for v0.25)

--- contrib/buildRevisions/buildRevisions.sh
+++ contrib/buildRevisions/buildRevisions.sh
@@ -4,36 +4,42 @@
 # You're expected to modify this script to perform whatever test
 # is appropriate for your revision investigation
 
-##
-# test a sequence of build revisions
-# to use a list of revisions, use for "r in 3800 3820 3877; do"
-for r in $(seq 3800 50 3900); do
+buildRevision()
+{
+	r="$1"
 	echo ------------- $r begin --------------
 	##
 	# get the code
 	R="../../../$r"
-	if [ -e $R ]; then rm -rf $R ; fi
-	svn checkout svn://dev.exiv2.org/svn/trunk --revision $r $R | wc
+	if [ ! -e $R ]; then
+		svn checkout svn://dev.exiv2.org/svn/trunk --revision $r $R | wc
+	fi
 	pushd $R
 		##
 		# build and install it
 		echo $PWD
 		make config 2>/dev/null  | wc
 		./configure              | wc
-		make -j  2>&1            | wc
+		make -j             2>&1 | wc
 		sudo make install        | wc
-		make -j samples 2>&1     | wc
-		echo build = $(exiv2 -vV | grep -e svn -e dll) $(ls -alt $(which exiv2))
+		make -j  samples    2>&1 | wc
+		echo build = $(exiv2 -vV | grep -e svn -e dll -e svn) $(ls -alt $(which exiv2))
 
 		##
 		# test the build
-		T=tst.tiff
-		curl  --silent        -O http://dev.exiv2.org/attachments/download/1004/$T
-		exiv2 --verbose       -M"set Exif.Image.Software revision $r"           $T
-		exiv2 --grep Software -pa                                               $T
+		make teste
 	popd > /dev/null
 	echo ------------- $r end -------------- $'\n'
-done
+}
+
+##
+# test a sequence of build revisions
+# to use a list of revisions, use for "r in 3800 3820 3877; do"
+if [ "$#"  == "0" ]; then
+    for r in $(seq 3000 500 4000); do buildRevision $r ; done;
+else
+    for r in "$@"                ; do buildRevision $r ; done;
+fi
 
 # That's all Folks!
 ##
