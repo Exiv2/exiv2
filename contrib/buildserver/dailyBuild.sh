f7c2e1fa02e88fb217845277ee87ed613f7608f1
Robin Mills <robin@clanmills.com>
2016-09-25 13:21:25 +0000

    #1230 Buildserver maintenance.

--- contrib/buildserver/dailyBuild.sh
+++ contrib/buildserver/dailyBuild.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 ##
-# This script is called by Jenkins to perform dailyBuild.
-# The script should be run in the directory ~/gnu/exiv2/buildserver
+# This script is called by Jenkins to perform the dailyBuild.
 #
 # The script operates in 3 stages:
 # 1 executes dailyCMake.sh to perform the build and test on the build nodes
 # 2 executes dailyTest.sh to test that the build bundles are good
 # 3 rebuilds all the links in the userContent/builds for "Category" access to the builds
+
+cd /Users/rmills/gnu/exiv2/buildserver
 
 ssh rmills@rmillsmm         'cd ~/gnu/exiv2/buildserver ; /usr/local/bin/svn update . ; rm -rf build ;                               contrib/buildserver/dailyCMake.sh'
 ssh rmills@rmillsmm-kubuntu 'cd ~/gnu/exiv2/buildserver ; /usr/local/bin/svn update . ; rm -rf build ;                               contrib/buildserver/dailyCMake.sh'
@@ -20,6 +21,10 @@ ssh rmills@rmillsmm-w7      'cd ~/gnu/exiv2/buildserver ; /usr/local/bin/svn upd
 date=$(date '+%Y-%m-%d+%H-%M-%S')
 svn=$(/usr/local/bin/svn info . | grep '^Last Changed Rev' | cut -f 2 -d':' | tr -d ' ')
 output="/mmHD/Users/Shared/Jenkins/Home/userContent/builds/Daily/test-svn-${svn}-date-${date}.txt" 
+echo --------------------------------------
+echo test log = $output
+echo --------------------------------------
+
 ssh rmills@rmillsmm          'cd ~/gnu/exiv2/buildserver ;                               contrib/buildserver/dailyTest.sh' | tr -d $'\r' | tee -a $output
 ssh rmills@rmillsmm-kubuntu  'cd ~/gnu/exiv2/buildserver ;                               contrib/buildserver/dailyTest.sh' | tr -d $'\r' | tee -a $output
 ssh rmills@rmillsmm-w7       'cd ~/gnu/exiv2/buildserver ;                               contrib/buildserver/dailyTest.sh' | tr -d $'\r' | tee -a $output
