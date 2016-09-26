bd16f3441e5c4a0c496678cbad082f1e7b504a36
Robin Mills <robin@clanmills.com>
2016-09-26 22:53:42 +0000

    #1233 Thank You to Anton for reporting this and providing a patch.  I've updated the test suite.

---
+++ test/geotag-test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Test driver for geotag
+
+source ./functions.source
+
+(   cd "$testdir"
+
+    printf "geotag" >&3
+
+    jpg=FurnaceCreekInn.jpg
+    gpx=FurnaceCreekInn.gpx
+    copyTestFiles $jpg $gpx
+
+    echo --- show GPSInfo tags ---
+    runTest                      exiv2 -pa --grep GPSInfo $jpg
+    tags=$(runTest               exiv2 -Pk --grep GPSInfo $jpg)
+    echo --- deleting the GPSInfo tags
+    for tag in $tags; do runTest exiv2 -M"del $tag" $jpg; done
+    runTest                      exiv2 -pa --grep GPS     $jpg
+    echo --- run geotag ---
+    runTest                      geotag -tz -8:00   $jpg $gpx
+    echo --- show GPSInfo tags ---
+    runTest                      exiv2 -pa --grep GPSInfo $jpg
+
+) 3>&1 > $results 2>&1
+
+printf "\n"
+
+# ----------------------------------------------------------------------
+# Evaluate results
+cat $results | tr -d $'\r' > $results-stripped
+mv                           $results-stripped $results
+reportTest                                     $results $good
+
+# That's all Folks!
+##
