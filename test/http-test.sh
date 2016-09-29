dbd7e7de1091c9ba8b99a054ae8d5e1ab168e934
Robin Mills <robin@clanmills.com>
2016-09-29 19:38:12 +0000

    #1023  Fix http-test.sh to detect support for https.  This isn't working very well.  I think the jenkins/https support is suspect.

--- test/http-test.sh
+++ test/http-test.sh
@@ -2,31 +2,35 @@
 # Test driver for http
 
 source ./functions.source
+cd "$testdir"
 
+server=exiv2.dyndns.org
+dir=userContent/testfiles/webready
+hosts="http://$server:8080"
 
-(   cd "$testdir"
+if [ $(runTest exiv2 -vVg curlproto | grep -q https ; echo $?) == "0" ]; then
+    hosts="https://$server:8443"
+else
+    echo "https is not a supported protocol"
+fi
 
-    server=exiv2.dyndns.org
-    dir=userContent/testfiles/webready
-    for host in http://$server:8080 https://$server:8443
-    do
-    	printf "$host " >&3
-		for filename in Reagan.jpg ReaganLargePng.png ReaganLargeTiff.tiff ReaganLargeJpg.jpg
-		do
-			echo $host/$dir/$filename
-			runTest exiv2 -pa --grep Software $host/$dir/$filename
-		done
-	done
+(   (   for host in $hosts ; do
+            printf "$host " >&3
+            for filename in Reagan.jpg ReaganLargePng.png ReaganLargeTiff.tiff ReaganLargeJpg.jpg; do
+                echo $filename
+                runTest exiv2 -pa --grep Software $host/$dir/$filename
+            done
+        done
+    ) 2>&1 > $results
 
-) 3>&1 2>&1 > $results
-
+    # ----------------------------------------------------------------------
+    # Evaluate results
+    cat $results | tr -d $'\r' > $results-stripped
+    mv                           $results-stripped $results
+    reportTest                                     $results $good
+) 3>&1
 printf "\n"
 
-# ----------------------------------------------------------------------
-# Evaluate results
-cat $results | tr -d $'\r' > $results-stripped
-mv                           $results-stripped $results
-reportTest                                     $results $good
 
 # That's all Folks!
 ##
