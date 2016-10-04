16202b772a9a99f232e4bef211c587f2c4857e4a
robinwmills <robinwmills@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2016-10-04 12:39:36 +0000

    #1230 Adding --verbose to print action to dump more information (to debug something on the buildserver)

--- trunk/test/webp-test.sh
+++ trunk/test/webp-test.sh
@@ -7,77 +7,78 @@ source ./functions.source
 
     num=1199                    # WebPImage
     printf "WebP " >&3
+#    verbose=--verbose
     filename=exiv2-bug$num.webp # http://dev.exiv2.org/attachments/download/1033/Stonehenge-with-icc.webp
     icc_name=exiv2-bug$num.icc
     exv_name=exiv2-bug$num.exv
     xmp_name=exiv2-bug$num.xmp
 
     copyTestFile                        $filename
-    runTest exiv2 -pS                   $filename
-    runTest exiv2 -pR                   $filename
-    runTest exiv2 -pX                   $filename | xmllint --format -
+    runTest exiv2 $verbose -pS          $filename
+    runTest exiv2 $verbose -pR          $filename
+    runTest exiv2 $verbose -pX          $filename | xmllint --format -
     printf "delete " >&3
     # test deleting metadata
     for option in -dC -de -dx -dCe -dCx -dCxe; do
       copyTestFile                      $filename
-      runTest exiv2 -pS                 $filename
+      runTest exiv2 $verbose -pS        $filename
       runTest exiv2 $option             $filename
-      runTest exiv2 -pS                 $filename
+      runTest exiv2 $verbose -pS        $filename
     done
 
     printf "insert " >&3
     printf "ICC "    >&3
     copyTestFile                        $filename
-    exiv2 -pS                           $filename
+    exiv2 $verbose -pS                  $filename
     copyTestFile                        Reagan.tiff
-    exiv2 -pC                           Reagan.tiff > $icc_name
-    exiv2 -iC                           $filename
+    exiv2 $verbose -pC                  Reagan.tiff > $icc_name
+    exiv2 $verbose -iC                  $filename
     exiv2 -pS                           $filename
 
     printf "XMP " >&3
     # copy the XMP from the test file
     copyTestFile                        $filename
-    exiv2 -pX                           $filename   > $xmp_name;
-    exiv2 -ea --force                   $filename
+    exiv2 $verbose -pX                  $filename   > $xmp_name;
+    exiv2 $verbose -ea --force          $filename
 
     copyTestFile                        $filename
-    exiv2 -pS                           $filename
-    exiv2 -iXX                          $filename
-    exiv2 -pS                           $filename
-    exiv2 -ix                           $filename
+    exiv2 $verbose -pS                  $filename
+    exiv2 $verbose -iXX                 $filename
+    exiv2 $verbose -pS                  $filename
+    exiv2 $verbose -ix                  $filename
 
     # copy the XMP from Reagan.tiff to test file
     copyTestFile                        Reagan.tiff
-    exiv2 -pX                           Reagan.tiff > $xmp_name;
-    exiv2 -ea --force                   Reagan.tiff
+    exiv2 $verbose -pX                  Reagan.tiff > $xmp_name;
+    exiv2 $verbose -ea --force          Reagan.tiff
     mv                                  Reagan.exv    $exv_name
 
     copyTestFile                        $filename
-    exiv2 -pS                           $filename
-    exiv2 -iXX                          $filename
-    exiv2 -pS                           $filename
-    exiv2 -ix                           $filename
+    exiv2 $verbose -pS                  $filename
+    exiv2 $verbose -iXX                 $filename
+    exiv2 $verbose -pS                  $filename
+    exiv2 $verbose -ix                  $filename
 
     # copy the XMP from exiv2-bug922.jpg to test file
     copyTestFile                        exiv2-bug922.jpg
-    exiv2 -pX                           exiv2-bug922.jpg > $xmp_name
-    exiv2 -ea --force                   exiv2-bug922.jpg 2>/dev/null
+    exiv2 $verbose -pX                  exiv2-bug922.jpg > $xmp_name
+    exiv2 $verbose -ea --force          exiv2-bug922.jpg 2>/dev/null
     mv                                  exiv2-bug922.exv   $exv_name
 
     copyTestFile                        $filename
-    exiv2 -pS                           $filename
-    exiv2 -ix                           $filename
-    exiv2 -pS                           $filename
-    exiv2 -iXX                          $filename
-    exiv2 -pS                           $filename
+    exiv2 $verbose -pS                  $filename
+    exiv2 $verbose -ix                  $filename
+    exiv2 $verbose -pS                  $filename
+    exiv2 $verbose -iXX                 $filename
+    exiv2 $verbose -pS                  $filename
 
     printf "EXIF " >&3
     copyTestFile                        exiv2-bug922.jpg $filename
-    exiv2 --force -ea                   $filename
+    exiv2 $verbose --force -ea          $filename
     copyTestFile                        $filename
-    exiv2 -pS                           $filename
-    exiv2 -ie                           $filename
-    exiv2 -pS                           $filename
+    exiv2 $verbose -pS                  $filename
+    exiv2 $verbose -ie                  $filename
+    exiv2 $verbose -pS                  $filename
 
 ) 3>&1 > $results 2>&1
 
