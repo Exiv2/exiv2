4f18c7c76e72ed25f733118421068c53d01a8041
robinwmills <robinwmills@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2016-10-13 12:05:39 +0000

    #1243.  Correction to r4627

--- trunk/src/image.cpp
+++ trunk/src/image.cpp
@@ -651,7 +651,7 @@ namespace Exiv2 {
 
     std::string binaryToString(DataBuf& buf, size_t size, size_t start /*=0*/)
     {
-        if ( size > buf.size_ ) size = buf.size_;
+        if ( size > (size_t) buf.size_ ) size = (size_t) buf.size_;
         return binaryToString(buf.pData_,size,start);
     }
 
