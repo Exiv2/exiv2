c2f5b03a68fc95d5babd2e0e9010a334fa8f8622
robinwmills <robinwmills@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2016-10-18 16:07:22 +0000

    #1240 Strengthened test/http-test.

--- trunk/test/http-test.sh
+++ trunk/test/http-test.sh
@@ -1,35 +1,45 @@
 #!/bin/bash
-# Test driver for http
+# Test driver for http/https
 
 source ./functions.source
-cd "$testdir"
+pushd "$testdir" 2>/dev/null
+
+recursive=$1
 
 server=exiv2.dyndns.org
 dir=userContent/testfiles/webready
-hosts="http://$server:8080"
+host="http://$server:8080"
 
-if [ $(runTest exiv2 -vVg curlproto | grep -q https ; echo $?) == "0" ]; then
-    hosts="https://$server:8443"
-else
-    echo "https is not a supported protocol"
+if [ ! -z "$recursive" ]; then
+	if [ $(runTest exiv2 -vVg curlproto | grep -q https ; echo $?) == "0" ]; then
+    	host="https://$server:8443"
+	else
+    	echo "*** https is not a supported protocol - test skipped ***"
+    	exit 0
+	fi
 fi
 
-(   (   for host in $hosts ; do
-            printf "$host " >&3
-            for filename in Reagan.jpg ReaganLargePng.png ReaganLargeTiff.tiff ReaganLargeJpg.jpg; do
-                echo $filename
-                runTest exiv2 -pa --grep Software $host/$dir/$filename
-            done
-        done
-    ) 2>&1 > $results
+echo 'server: ' $host
 
-    # ----------------------------------------------------------------------
-    # Evaluate results
-    cat $results | tr -d $'\r' > $results-stripped
-    mv                           $results-stripped $results
-    reportTest                                     $results $good
-) 3>&1
-printf "\n"
+(
+    for filename in Reagan.jpg ReaganLargePng.png ReaganLargeTiff.tiff ReaganLargeJpg.jpg Reagan.tiff DSC_0002.NEF ; do
+        echo $filename
+        runTest exiv2 -pa --grep Software $host/$dir/$filename
+    done
+) 2>&1 > $results
+
+# ----------------------------------------------------------------------
+# Evaluate results
+cat $results | tr -d $'\r' > $results-stripped
+mv                           $results-stripped $results
+reportTest                                     $results $good
+
+popd 2>/dev/null
+
+if [ -z "$recursive" ]; then
+	$0 1
+fi
+
 
 
 # That's all Folks!
