f3342c891267b0a6507f9a2b721e453b551001fd
Robin Mills <robin@clanmills.com>
2016-10-19 19:18:07 +0000

    #1080 Added the file BLANK.JPG to test suite as exiv2-bug1080.jpg

--- test/bugfixes-test.sh
+++ test/bugfixes-test.sh
@@ -424,6 +424,13 @@ source ./functions.source
     copyTestFile              $filename
     runTest exiv2 -pa -g zone $filename
 
+    num=1080
+    filename=exiv2-bug$num.jpg
+    printf "$num " >&3
+    echo '------>' Bug $num '<-------' >&2
+    copyTestFile              $filename
+    runTest exiv2 -pa         $filename
+
     num=1074
     filename=exiv2-bug$num.png
     printf "$num " >&3
