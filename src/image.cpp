021bf5c23732150f6fd00def4a1e6421aebecd4c
Robin Mills <robin@clanmills.com>
2016-10-21 17:44:19 +0000

    #1247  Thank You Hanno for reporting this and providing a patch.

--- src/image.cpp
+++ src/image.cpp
@@ -614,6 +614,7 @@ namespace Exiv2 {
     void Image::setIccProfile(Exiv2::DataBuf& iccProfile,bool bTestValid)
     {
         if ( bTestValid ) {
+            if ( iccProfile.pData_ && ( iccProfile.size_ < sizeof(long)) ) throw Error(53);
             long size = iccProfile.pData_ ? getULong(iccProfile.pData_, bigEndian): -1;
             if ( size!= iccProfile.size_ ) throw Error(53);
         }
