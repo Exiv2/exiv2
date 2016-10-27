372d657f8c25a7b9e00a8a25b8c1bc598f5462fe
robinwmills <robinwmills@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2016-10-27 08:59:30 +0000

    Fixing msvc/copylibs.bat

--- trunk/msvc/copylibs.bat
+++ trunk/msvc/copylibs.bat
@@ -5,11 +5,11 @@ rem
 setlocal
 set EXIV2LIBS=c:\exiv2libs
 
-xcopy/yesihq  %EXIV2LIBS%\expat-2.1.0     ..\..\expat
-xcopy/yesihq  %EXIV2LIBS%\zlib-1.2.7      ..\..\zlib
-xcopy/yesihq  %EXIV2LIBS%\openssl-1.0.1j  ..\..\openssl 
-xcopy/yesihq  %EXIV2LIBS%\libssh-0.5.5    ..\..\libssh
-xcopy/yesihq  %EXIV2LIBS%\curl-7.39.0     ..\..\curl
+xcopy/yesihq  %EXIV2LIBS%\expat   ..\..\expat
+xcopy/yesihq  %EXIV2LIBS%\zlib    ..\..\zlib
+xcopy/yesihq  %EXIV2LIBS%\openssl ..\..\openssl 
+xcopy/yesihq  %EXIV2LIBS%\libssh  ..\..\libssh
+xcopy/yesihq  %EXIV2LIBS%\curl    ..\..\curl
 
 
 rem That's all Folks!
