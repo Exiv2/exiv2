25a84facef7df31c7f0aac178c7ab1085456de38
robinwmills <robinwmills@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2017-03-15 17:40:32 +0000

    #1279 Preparing the release.

--- trunk/website/bin/download.sh
+++ trunk/website/bin/download.sh
@@ -13,9 +13,9 @@ rm -rf $basedir/var/$buttons
 for P in trunk macosx linux cygwin mingw msvc; do
 	p=exiv2-$version-$P.tar.gz
 	size=$(ls -la html/builds/$p | cut -d' ' -f 5)
-	date=$(stat -c "%w" html/builds/$p | cut -d' ' -f 1)
+	date=$(stat -c "%y" html/builds/$p | cut -d' ' -f 1)
 	md5=$(md5sum html/builds/$p  | cut -d' ' -f 1)
-	
+
 	echo "<tr>  \
             <td>Exiv2 v$version $P</td> \
             <td><a href=\"builds/${p}\">${p}</a></td> \
@@ -23,13 +23,13 @@ for P in trunk macosx linux cygwin mingw msvc; do
             <td class=\"text-nowrap\">$date</td> \
             <td>$md5</td> \
           </tr>"  >> $basedir/var/$table
-          
+
 	platform=$P
-	config="64 bit shared libraries" 
-	if [ "$platform" == macosx ]; then platform="MacOSX"       ; fi          
-	if [ "$platform" == cygwin ]; then platform="Cygwin"       ; fi          
-	if [ "$platform" == mingw  ]; then platform="MinGW"        ; config="32 bit shared libraries"                     ; fi          
-	if [ "$platform" == msvc   ]; then platform="Visual Studio"; config="32 & 64 bit DLLs for MSVC 2005/8/10/12/13/15"; fi          
+	config="64 bit shared libraries"
+	if [ "$platform" == macosx ]; then platform="MacOSX"       ; fi
+	if [ "$platform" == cygwin ]; then platform="Cygwin"       ; fi
+	if [ "$platform" == mingw  ]; then platform="MinGW"        ; config="32 bit shared libraries"                     ; fi
+	if [ "$platform" == msvc   ]; then platform="Visual Studio"; config="32 & 64 bit DLLs for MSVC 2005/8/10/12/13/15"; fi
 	if [ "$platform" == linux  ]; then platform="Linux"        ; fi
 	if [ "$platform" != trunk  ]; then
   	  echo "<tr><td>$platform<h3></td><td>$config</td> \
