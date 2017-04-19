675ff11a2d220fe15367d60e74d0c47a1b8d1c05
robinwmills <robinwmills@b7c8b350-86e7-0310-a4b4-de8f6a8f16a3>
2017-04-19 15:36:00 +0000

    #1175.  Thanks to LaserSoft for reporting this and providing a patch.

--- trunk/src/basicio.cpp
+++ trunk/src/basicio.cpp
@@ -237,15 +237,25 @@ namespace Exiv2 {
     {
         int ret = 0;
 #ifdef EXV_UNICODE_PATH
-        if (wpMode_ == wpUnicode) {
-            struct _stat st;
-            ret = ::_wstat(wpath_.c_str(), &st);
+#ifdef _WIN64
+            struct _stat64 st;
+            ret = ::_wstati64(wpath_.c_str(), &st);
+
             if (0 == ret) {
                 buf.st_size = st.st_size;
                 buf.st_mode = st.st_mode;
                 buf.st_nlink = st.st_nlink;
             }
-        }
+#else
+            struct _stat st;
+            ret = ::_wstat(wpath_.c_str(), &st);
+
+            if (0 == ret) {
+                buf.st_size = st.st_size;
+                buf.st_mode = st.st_mode;
+                buf.st_nlink = st.st_nlink;
+            }
+#endif
         else
 #endif
         {
