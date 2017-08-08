89ab475c66f05a9d0e91d90ad032d66bf60c9039
clanmills <robin@clanmills.com>
2017-08-08 12:16:35 +0100

    Change uname code in buildXMPsdk.sh

--- xmpsdk/buildXMPsdk.sh
+++ xmpsdk/buildXMPsdk.sh
@@ -4,19 +4,19 @@
 # buildXMPsdk.sh
 # TODO:
 #     1) Command-line parser for options such as:
-#	     version of xmpsdk (2014, 2016)
-#	     build options     (64/32 static/dynamic debug/release)
+#        version of xmpsdk (2014, 2016)
+#        build options     (64/32 static/dynamic debug/release)
 #        help
 #     2) Cygwin support (in progress)
 #     3) Write buildXMPsdk.cmd (for MSVC)
 ##
 
-uname=$(uname -o)
+uname=$(uname -a | cut -d' ' -f 1 | cut -d_ -f 1)
 case "$uname" in
-	Darwin|GNU/Linux|Cygwin) ;;
-	*)  echo "*** unsupported platform $uname ***"
-	    exit 1
-	;;
+    Darwin|Linux|CYGWIN) ;;
+    *)  echo "*** unsupported platform $uname ***"
+        exit 1
+    ;;
 esac
 
 ##
@@ -24,8 +24,7 @@ esac
 if [ ! -e Adobe ]; then (
     mkdir Adobe
     cd    Adobe
-    curl -O http://download.macromedia.com/pub/developer/xmp/sdk/XMP-Toolkit-SDK-CC201607.zip
-    # cp ~/important/XMP-Toolkit-SDK-CC201607.zip .
+    curl  -O http://download.macromedia.com/pub/developer/xmp/sdk/XMP-Toolkit-SDK-CC201607.zip
     unzip XMP-Toolkit-SDK-CC201607.zip
 ) fi
 
@@ -38,10 +37,11 @@ if [ ! -e Adobe ]; then (
 ##
 # generate Makefile and build
 (   cd Adobe/XMP-Toolkit-SDK-CC201607/build
+    result=1  # assume the build will fail
     ##
     # Tweak the code (depends on platform)
     case "$uname" in
-        GNU/Linux)
+        Linux)
             # modify ProductConfig.cmake (don't link libssp.a)
             f=ProductConfig.cmake
             if [ -e $f.orig ]; then mv $f.orig $f ; fi ; cp $f $f.orig
@@ -51,30 +51,38 @@ if [ ! -e Adobe ]; then (
             for f in XMPFiles XMPCore; do
                 cp ../$f/resource/linux/* ../$f/resource
             done
+
+            cmake . -G "Unix Makefiles"              \
+                    -DXMP_ENABLE_SECURE_SETTINGS=OFF \
+                    -DXMP_BUILD_STATIC=1             \
+                    -DCMAKE_CL_64=ON                 \
+                    -DCMAKE_BUILD_TYPE=Release
+            make
+            result=$?
         ;;
 
-        Cygwin)
-    	    f=../source/Host_IO-POSIX.cpp
+        CYGWIN)
+            f=../source/Host_IO-POSIX.cpp
             if [ -e $f.orig ]; then mv $f.orig $f ; fi ; cp $f $f.orig
 
             sed -E -e $'s?// Writeable?// Writeable~#include <windows.h>~#ifndef PATH_MAX~#define PATH_MAX 512~#endif?' $f.orig | tr "~" "\n" > $f
         ;;
+
+        Darwin)
+            cmake . -G "Unix Makefiles"              \
+                    -DXMP_ENABLE_SECURE_SETTINGS=OFF \
+                    -DXMP_BUILD_STATIC=1             \
+                    -DCMAKE_CL_64=ON                 \
+                    -DCMAKE_BUILD_TYPE=Release
+            make
+            result=$?
+        ;;
     esac
-
-    cmake . -G "Unix Makefiles"              \
-            -DXMP_ENABLE_SECURE_SETTINGS=OFF \
-            -DXMP_BUILD_STATIC=1             \
-            -DCMAKE_CL_64=ON                 \
-            -DCMAKE_BUILD_TYPE=Release
-    make
-
-	echo '3' | ./GenerateXMPToolkitSDK_mac.sh
-	find . -name XMPToolkitSDK64.xcodeproj -execdir xcodebuild {} \;
 )
 
 ##
-# copy headers and built libraries
-(
+# copy headers and build libraries
+if [ "$result" == "0" ]; then (
     rm -rf include
     cp -R  Adobe/XMP-Toolkit-SDK-CC201607/public/include include
 
@@ -85,31 +93,20 @@ if [ ! -e Adobe ]; then (
 
     # move the library/archives into xmpsdk
     case "$uname" in
-      Cygwin99|GNU/Linux)
+      Linux)
           find Adobe/XMP-Toolkit-SDK-CC201607/public -name "*.ar" -exec cp {} . ';'
-          ./ren-lnx.sh
+          mv staticXMPCore.ar  libXMPCore.a
+          mv staticXMPFiles.ar libXMPFiles.a
       ;;
 
       Darwin)
-        find Adobe/XMP-Toolkit-SDK-CC201607/public -name "*.a" -exec cp {} . ';'
-        ./ren-mac.sh
+          find Adobe/XMP-Toolkit-SDK-CC201607/public -name "*.a" -exec cp {} . ';'
+          mv libXMPCoreStatic.a  libXMPCore.a
+          mv libXMPFilesStatic.a libXMPFiles.a
       ;;
     esac
     ls -alt *.a
-
-	if [ 1 == 2 ]; then
-    # combine libraries into libxmpsdk.a
-        mkdir  foo
-        mv *.a foo
-        cd     foo
-        for i in *.a; do ar -x $i ; rm -rf $i ; done
-        ar -cq libxmpsdk.a *.o
-        mv     libxmpsdk.a  ..
-        cd ..
-        rm -rf foo
-        ls -alt *.a
-    fi
-)
+) ; fi
 
 # That's all Folks!
 ##
