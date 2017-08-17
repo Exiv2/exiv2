45807ca03df84afbc3781582fd927fdfba48f1f9
Luis Diaz Mas <piponazo@gmail.com>
2017-08-17 22:59:34 +0200

    Do not include SSH and CURL include dirs globally. Do it properly for the exiv2lib target

--- config/findDependencies.cmake
+++ config/findDependencies.cmake
@@ -13,12 +13,10 @@ if( EXIV2_ENABLE_WEBREADY )
     set( ENABLE_WEBREADY 1 )
     if( EXIV2_ENABLE_CURL )
         find_package( CURL REQUIRED)
-        include_directories( ${CURL_INCLUDE_DIR} )
     endif()
 
     if( EXIV2_ENABLE_SSH )
         find_package( SSH REQUIRED)
-        include_directories( ${SSH_INCLUDE_DIR} )
     endif( EXIV2_ENABLE_SSH )
 endif( EXIV2_ENABLE_WEBREADY )
 
