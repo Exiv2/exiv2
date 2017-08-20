37c1291671785d81cd2b9f893db0847f968a7db6
Luis Diaz Mas <piponazo@gmail.com>
2017-08-20 17:01:02 +0200

    Travis support for OSX

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,5 +4,9 @@ set -x
 
 if [[ "$(uname -s)" == 'Linux' ]]; then
     sudo apt-get install cmake zlib1g-dev libssh-dev libcurl4-openssl-dev gettext libexpat1-dev
+else
+    brew update
+    brew install expat gettext libssh
+    # By default it already has cmake 3.6.2
 fi
 
