3aa77e0b870b02703f993276e0a1360936cb051f
Luis Díaz Más <piponazo@gmail.com>
2017-08-21 23:02:11 +0200

    Remove msvc macros and run that code always for the MSVC configuration

--- config/compilerFlags.cmake
+++ config/compilerFlags.cmake
@@ -17,63 +17,51 @@ if ( MINGW OR UNIX ) # MINGW, Linux, APPLE, CYGWIN
     endif()
 endif ()
 
-
-##
-# msvc tuning macros
-macro(msvc_runtime_set_static_ignores bDynamic bDynamicRuntime)
-  if(MSVC)
-    # don't link msvcrt for .exe which use shared libraries (use default libcmt)
-	if ( ${bDynamic} STREQUAL "OFF" AND ${bDynamicRuntime} STREQUAL "OFF")
-		set(CMAKE_EXE_LINKER_FLAGS_DEBUG          "/NODEFAULTLIB:MSVCRTD")
-		set(CMAKE_EXE_LINKER_FLAGS_RELEASE        "/NODEFAULTLIB:MSVCRT")
-		set(CMAKE_EXE_LINKER_FLAGS_MINSIZEREL     "/NODEFAULTLIB:MSVCRT")
-		set(CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO "/NODEFAULTLIB:MSVCRT")
-	endif()
-  endif()
-endmacro()
-
 # http://stackoverflow.com/questions/10113017/setting-the-msvc-runtime-in-cmake
-macro(msvc_runtime_configure bDynamic bDynamicRuntime)
-  if(MSVC)
+if(MSVC)
     set(variables
       CMAKE_C_FLAGS_DEBUG
-      CMAKE_C_FLAGS_MINSIZEREL
-      CMAKE_C_FLAGS_RELEASE
-      CMAKE_C_FLAGS_RELWITHDEBINFO
-      CMAKE_CXX_FLAGS_DEBUG
-      CMAKE_CXX_FLAGS_MINSIZEREL
+      CMAKE_C_FLAGS_MINSIZEREL
+      CMAKE_C_FLAGS_RELEASE
+      CMAKE_C_FLAGS_RELWITHDEBINFO
+      CMAKE_CXX_FLAGS_DEBUG
+      CMAKE_CXX_FLAGS_MINSIZEREL
       CMAKE_CXX_FLAGS_RELEASE
       CMAKE_CXX_FLAGS_RELWITHDEBINFO
     )
-
-    if( ${bDynamic} STREQUAL "ON" OR ${bDynamicRuntime} STREQUAL "ON")
-      message(STATUS  "MSVC -> forcing use of dynamically-linked runtime." )
-      foreach(variable ${variables})
-        if(${variable} MATCHES "/MT")
-          string(REGEX REPLACE "/MT" "/MD" ${variable} "${${variable}}")
-        endif()
-      endforeach()
+    if ( ${EXIV2_ENABLE_SHARED} OR ${EXIV2_ENABLE_DYNAMIC_RUNTIME})
+        message(STATUS  "MSVC -> forcing use of dynamically-linked runtime." )
+        foreach(variable ${variables})
+            if(${variable} MATCHES "/MT")
+                string(REGEX REPLACE "/MT" "/MD" ${variable} "${${variable}}")
+            endif()
+        endforeach()
     else()
-      message(STATUS "MSVC -> forcing use of statically-linked runtime." )
-      foreach(variable ${variables})
-        if(${variable} MATCHES "/MD")
-          string(REGEX REPLACE "/MD" "/MT" ${variable} "${${variable}}")
-        endif()
-        set(${variable} "${${variable}} /DXML_STATIC /D_LIB")
-      endforeach()
+        message(STATUS "MSVC -> forcing use of statically-linked runtime." )
+        foreach(variable ${variables})
+            if(${variable} MATCHES "/MD")
+                string(REGEX REPLACE "/MD" "/MT" ${variable} "${${variable}}")
+            endif()
+            set(${variable} "${${variable}} /DXML_STATIC /D_LIB")
+        endforeach()
     endif()
-    
+
     # remove /Ob2 and /Ob1 - they cause linker issues
     set(obs /Ob2 /Ob1)
     foreach(ob ${obs})
-      foreach(variable ${variables})
-        if(${variable} MATCHES ${ob} )
-          string(REGEX REPLACE ${ob} "" ${variable} "${${variable}}")
-        endif()
+        foreach(variable ${variables})
+            if(${variable} MATCHES ${ob} )
+                string(REGEX REPLACE ${ob} "" ${variable} "${${variable}}")
+            endif()
       endforeach()
     endforeach()
-    msvc_runtime_set_static_ignores(${bDynamic} ${bDynamicRuntime})
-  endif()
-endmacro()
 
-msvc_runtime_configure(${EXIV2_ENABLE_SHARED} ${EXIV2_ENABLE_DYNAMIC_RUNTIME})
+    # don't link msvcrt for .exe which use shared libraries (use default libcmt)
+    if ( NOT ${EXIV2_ENABLE_SHARED} AND NOT ${EXIV2_ENABLE_DYNAMIC_RUNTIME})
+        set(CMAKE_EXE_LINKER_FLAGS_DEBUG          "/NODEFAULTLIB:MSVCRTD")
+        set(CMAKE_EXE_LINKER_FLAGS_RELEASE        "/NODEFAULTLIB:MSVCRT")
+        set(CMAKE_EXE_LINKER_FLAGS_MINSIZEREL     "/NODEFAULTLIB:MSVCRT")
+        set(CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO "/NODEFAULTLIB:MSVCRT")
+    endif()
+
+endif()
