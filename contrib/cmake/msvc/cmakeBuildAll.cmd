8dda618b4da12564891e56bf1bb45e1bb500ab80
Robin Mills <robin@clanmills.com>
2017-09-09 08:38:09 +0100

    Fixing 2008 typo.

--- contrib/cmake/msvc/cmakeBuildAll.cmd
+++ contrib/cmake/msvc/cmakeBuildAll.cmd
@@ -1,1 +1,1 @@
-for %%a in (64 32) do for %%v in (2017 2015 2005) do cmd /c "vcvars %%v %%a && cmakeBuild --build %* 2>&1" 
+for %%a in (64 32) do for %%v in (2008) do for %%s in (--static --dll) do for %%r in (--release --debug) do cmd /c "vcvars %%v %%a && cmakeBuild --build %%s %%r %* 2>&1" 
