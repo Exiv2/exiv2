e3ff1ba694cf9e5c2f296d653a37283a586048c3
Michał Walenciak <Kicer86@gmail.com>
2017-09-21 17:33:25 +0200

    fixing indent

--- src/bigtiffimage.cpp
+++ src/bigtiffimage.cpp
@@ -160,7 +160,7 @@ namespace Exiv2
 
                 void printStructure(std::ostream& os, PrintStructureOption option, int depth)
                 {
-                    printIFD(os, option, header_.dirOffset(), depth);
+                    printIFD(os, option, header_.dirOffset(), depth - 1);
                 }
 
             private:
