9acc1c56a70624e74e930b403acb4bda50b47bfe
Michał Walenciak <Kicer86@gmail.com>
2017-09-24 16:07:26 +0200

    adding ifd8 and long longs to is8ByteType()

--- src/image.cpp
+++ src/image.cpp
@@ -221,6 +221,8 @@ namespace Exiv2 {
     bool Image::is8ByteType(uint16_t type)
     {
         return isRationalType(type)
+             || isLongLongType(type)
+             || type == Exiv2::tiffIfd8
              || type == Exiv2::tiffDouble
             ;
     }
