c30ababe6c9b7422d05b1efe372ea9b0b022dd23
Dan Čermák <dan.cermak@cgc-instruments.com>
2017-10-07 23:07:26 +0200

    Added arithmetic operation overflow error
    (cherry picked from commit da67c16f3d8f8431ae5c732126499f74ccca6a81)

--- src/error.cpp
+++ src/error.cpp
@@ -112,6 +112,7 @@ namespace {
         { 56, N_("invalid type value detected in Image::printIFDStructure") },
         { 57, N_("invalid memory allocation request") },
         { 58, N_("corrupted image metadata") },
+        { 59, N_("Arithmetic operation overflow") },
     };
 
 }
