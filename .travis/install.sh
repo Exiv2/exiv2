f454c375a0337e7d240ea4ab5a1671f65d2d15e0
Luis Diaz Mas <piponazo@gmail.com>
2017-10-07 19:47:30 +0200

    Disable bash -ex to not fail with commands returning non-0 values

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-set -e
-set -x
+#set -e
+#set -x
 
 if [[ "$(uname -s)" == 'Linux' ]]; then
     sudo apt-get install cmake zlib1g-dev libssh-dev libcurl4-openssl-dev gettext libexpat1-dev
