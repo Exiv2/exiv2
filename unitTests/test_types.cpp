72cf9166feeb2dc00e1adc2dbadf93a274006fa5
Luis Díaz Más <piponazo@gmail.com>
2017-10-08 22:04:51 +0200

    Add simple tests for exifTime

--- unitTests/test_types.cpp
+++ unitTests/test_types.cpp
@@ -3,9 +3,24 @@
 
 using namespace Exiv2;
 
+// More info about tm : http://www.cplusplus.com/reference/ctime/tm/
+
 TEST(ExivTime, getsTimeFromValidString)
 {
     struct tm tmInstance;
     ASSERT_EQ(0, exifTime("2007:05:24 12:31:55", &tmInstance));
-    /// \todo add more checks here
+    ASSERT_EQ(107,  tmInstance.tm_year); // Years since 1900
+    ASSERT_EQ(4,    tmInstance.tm_mon);
+    ASSERT_EQ(24,   tmInstance.tm_mday);
+    ASSERT_EQ(12,   tmInstance.tm_hour);
+    ASSERT_EQ(31,   tmInstance.tm_min);
+    ASSERT_EQ(55,   tmInstance.tm_sec);
+}
+
+TEST(ExivTime, doesNotGetTimeWithBadFormedString)
+{
+    struct tm tmInstance;
+    ASSERT_EQ(1, exifTime("007:a5:24 aa:bb:cc", &tmInstance));
+}
+
 }
