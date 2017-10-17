bfa549042561ed1d8682b1d0b26e15585dbaca55
Dan Čermák <dan.cermak@cgc-instruments.com>
2017-10-18 00:21:39 +0200

    Backported travis config
    (cherry picked from commit 2123757384aefb1daae6a8abba9797a6bbad6fe5)

---
+++ .travis/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e # Enables cheking of return values from each command
+set -x # Prints every command
+
+if [[ "$(uname -s)" == 'Linux' ]]; then
+    sudo apt-get install cmake zlib1g-dev libssh-dev gettext expat libcurl4-openssl-dev
+    sudo pip install virtualenv
+else
+    brew update
+    brew install gettext libssh expat zlib curl
+    # By default it already has cmake 3.6.2
+fi
---
+++ .travis/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -x
+
+mkdir test/tmp/
+mkdir build && cd build
+cmake ${CMAKE_OPTIONS} -DCMAKE_INSTALL_PREFIX=install ..
+make -j2
+make tests
+make install
