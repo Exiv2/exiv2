ec55f74f6815350dd5b939895d9a76fd934dd0e9
Dan Čermák <dan.cermak@cgc-instruments.com>
2017-10-20 23:30:09 +0200

    Added reproducer for CVE-2017-14863 / #132 to the test suite

--- test/bugfixes-test.sh
+++ test/bugfixes-test.sh
@@ -779,6 +779,12 @@ source ./functions.source
     copyTestFile                      $filename
     runTest exiv2                     $filename
 
+    num=g132
+    printf "$num " >&3
+    filename=01-Null-exiv2-poc
+    echo '------>' Bug $filename '<-------' >&2
+    copyTestFile                      $filename
+    runTest exiv2                     $filename
 
 ) 3>&1 > $results 2>&1
 
