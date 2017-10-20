14f386d69262eca05a3dd00a4c38e44f040a15b8
Luis Díaz Más <piponazo@gmail.com>
2017-10-20 18:50:55 +0200

    Add gtestWrapper.h to skip some gtest warnings on windows

---
+++ unitTests/gtestWrapper.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef _MSC_VER
+        #pragma warning(push)
+        #pragma warning(disable : 4251)
+        #pragma warning(disable : 4275)
+#endif
+#include <gtest/gtest.h>
+#ifdef _MSC_VER
+        #pragma warning(pop)
+#endif
--- unitTests/test_types.cpp
+++ unitTests/test_types.cpp
@@ -1,5 +1,6 @@
-#include <gtest/gtest.h>
 #include <exiv2/types.hpp>
+
+#include "gtestWrapper.h"
 
 using namespace Exiv2;
 
