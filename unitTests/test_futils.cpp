171c7970f98d75941c1840847a3405b33cb1a4b6
Luis Díaz Más <piponazo@gmail.com>
2017-11-28 17:02:11 +0100

    Add characterization tests for exiv2::getEnv

--- unitTests/test_futils.cpp
+++ unitTests/test_futils.cpp
@@ -6,7 +6,7 @@
 #include <cstdio>
 #include <cerrno>
 
-#include <gtest/gtest.h>
+#include "gtestwrapper.h"
 
 using namespace Exiv2;
 
@@ -29,4 +29,27 @@ TEST(strError, doNotRecognizeUnknownError)
 {
     errno = 9999;
     ASSERT_STREQ("Unknown error 9999 (errno = 9999)", strError().c_str());
+}
+
+TEST(getEnv, getsDefaultValueWhenExpectedEnvVariableDoesNotExist)
+{
+    ASSERT_STREQ("/exiv2.php", getEnv(envHTTPPOST).c_str());
+    ASSERT_STREQ("40", getEnv(envTIMEOUT).c_str());
+}
+
+TEST(getEnv, getsProperValuesWhenExpectedEnvVariableExists)
+{
+    const char * expectedValue = "test";
+    ASSERT_EQ(0, setenv("EXIV2_HTTP_POST", expectedValue, 1));
+
+    ASSERT_STREQ(expectedValue, getEnv(envHTTPPOST).c_str());
+
+    ASSERT_EQ(0, unsetenv("EXIV2_HTTP_POST"));
+}
+
+TEST(getEnv, throwsWhenKeyDoesNotExist)
+{
+    // This is just a characterisation test that was written to see how the current implementation
+    // works.
+    ASSERT_THROW(getEnv(static_cast<EnVar>(3)), std::exception);
 }
