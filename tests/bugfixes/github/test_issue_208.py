c754ca6d1439d022bd4254cf007cea5caeb29b51
Dan Čermák <dan.cermak@cgc-instruments.com>
2018-02-01 23:35:06 +0100

    [testsuite] Corrected class name, issue had no CVE assigned

--- tests/bugfixes/github/test_issue_208.py
+++ tests/bugfixes/github/test_issue_208.py
@@ -3,7 +3,7 @@
 import system_tests
 
 
-class CVE_2017_14857(system_tests.Case):
+class AssertionPoC(system_tests.Case):
 
     filename = "{data_path}/2018-01-09-exiv2-crash-001.tiff"
     commands = ["{exiv2} " + filename]
