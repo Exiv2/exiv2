cabf756553d177998b3f2be3a4b532bd38300f50
Dan Čermák <dan.cermak@cgc-instruments.com>
2018-02-14 23:17:26 +0100

    Add reproducer for #170 to the test suite

---
+++ tests/bugfixes/github/test_issue_170.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import system_tests
+
+
+class decodeIHDRChunkOutOfBoundsRead(system_tests.Case):
+
+    url = "https://github.com/Exiv2/exiv2/issues/170"
+
+    filename = "{data_path}/issue_170_poc"
+
+    commands = ["{exiv2} " + filename]
+    stdout = [""]
+    stderr = ["""{exiv2_exception_msg} """ + filename + """:
+{kerFailedToReadImageData}
+"""]
+    retval = [1]
