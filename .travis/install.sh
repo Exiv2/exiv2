1f4e3075894276ea590dc61c698628178d22b459
Andreas Sturmlechner <andreas.sturmlechner@gmail.com>
2018-02-22 18:20:29 +0100

    Add libxml2-utils package for the linux build on travis

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,7 +3,7 @@ set -e # Enables cheking of return values from each command
 set -x # Prints every command
 
 if [[ "$(uname -s)" == 'Linux' ]]; then
-    sudo apt-get install cmake zlib1g-dev libssh-dev gettext expat libcurl4-openssl-dev
+    sudo apt-get install cmake zlib1g-dev libssh-dev gettext expat libcurl4-openssl-dev libxml2-utils
     sudo pip install virtualenv
 else
     brew update
