08aa27ae37b4777b5dc144a340f483233fc29e7e
Luis Diaz Mas <piponazo@gmail.com>
2018-04-01 08:27:23 +0200

    Add reproducers for #253 and #246

---
+++ tests/bugfixes/github/test_issue_253.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import system_tests
+
+class TestFirstPoC(system_tests.Case):
+    """
+    Regression test for the first bug described in:
+    https://github.com/Exiv2/exiv2/issues/253
+    """
+    url = "https://github.com/Exiv2/exiv2/issues/253"
+
+    filename = "{data_path}/3-stringformat-outofbound-read"
+    commands = ["{exiv2} " + filename]
+    stdout = [""]
+    stderr = ["""{exiv2_exception_message} """ + filename + """:
+{kerNotAJpeg}
+"""]
+    retval = [1]
