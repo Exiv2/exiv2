89e7b53c8178dfa9c3daabe2ee727934df2101b3
Steven Robbins <smr@debian.org>
2018-04-25 20:55:09 -0500

    Test XMP type bit sizes.

--- unitTests/test_xmp.cpp
+++ unitTests/test_xmp.cpp
@@ -4,14 +4,14 @@
 
 TEST(XmpTypes, bitsize)
 {
-    ASSERT_EQ(8, sizeof(XMP_Int8));
-    ASSERT_EQ(16, sizeof(XMP_Int16));
-    ASSERT_EQ(32, sizeof(XMP_Int32));
-    ASSERT_EQ(64, sizeof(XMP_Int64));
+    ASSERT_EQ(1, sizeof(XMP_Int8));
+    ASSERT_EQ(2, sizeof(XMP_Int16));
+    ASSERT_EQ(4, sizeof(XMP_Int32));
+    ASSERT_EQ(8, sizeof(XMP_Int64));
 
-    ASSERT_EQ(8, sizeof(XMP_Uns8));
-    ASSERT_EQ(16, sizeof(XMP_Uns16));
-    ASSERT_EQ(32, sizeof(XMP_Uns32));
-    ASSERT_EQ(64, sizeof(XMP_Uns64));
+    ASSERT_EQ(1, sizeof(XMP_Uns8));
+    ASSERT_EQ(2, sizeof(XMP_Uns16));
+    ASSERT_EQ(4, sizeof(XMP_Uns32));
+    ASSERT_EQ(8, sizeof(XMP_Uns64));
 }
 
