c3629117d87ff49251f8b3f57bda2b475b86376b
Luis Díaz Más <piponazo@gmail.com>
2018-05-22 17:06:46 +0200

    conan: Upgrade libcurl version
    Thanks to this upgrade the cmake code that handles the CURL dependency
    can be simplified. Now we can use the shared version of CURL in all the
    platforms. However we will use for the moment the static version on Mac,
    since we are having some issues with 'make tests'. Once those tests are
    migrated to python we can also use the shared version there.

--- conanfile.py
+++ conanfile.py
@@ -8,17 +8,14 @@ class Exiv2Conan(ConanFile):
     default_options = 'unitTests=True'
 
     def configure(self):
-        # Note : The linking in exiv2lib fails if we try to use the static version of libcurl.
-        # The libcurl CMake code is not mature enough and therefore the conan recipe for
-        # Windows also has some problems (since it uses CMake for configuring the project).
-        if os_info.is_windows:
+        if not os_info.is_macos:
             self.options['libcurl'].shared = True
         self.options['gtest'].shared = True
 
     def requirements(self):
         self.requires('Expat/2.2.5@pix4d/stable')
         self.requires('zlib/1.2.11@conan/stable')
-        self.requires('libcurl/7.56.1@bincrafters/stable') # from conan-bincrafters
+        self.requires('libcurl/7.60.0@bincrafters/stable')
 
         if self.options.unitTests:
             self.requires('gtest/1.8.0@bincrafters/stable')
