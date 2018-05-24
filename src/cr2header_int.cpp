dc255711434f10342f93fbbb6ff6b33826e81c0f
Luis Diaz Mas <piponazo@gmail.com>
2018-05-24 23:51:05 +0200

    Fix bug in Cr2Header::read

--- src/cr2header_int.cpp
+++ src/cr2header_int.cpp
@@ -17,7 +17,9 @@ namespace Exiv2 {
 
     bool Cr2Header::read(const byte* pData, uint32_t size)
     {
-        if (size < 16) return false;
+        if (!pData || size < 16) {
+            return false;
+        }
 
         if (pData[0] == 'I' && pData[0] == pData[1]) {
             setByteOrder(littleEndian);
