335607054c7e18e6c1553fc5d2a9dc838148b7e6
Luis Díaz Más <piponazo@gmail.com>
2018-05-25 15:08:37 +0200

    Use enforce instead of std::assert to fix warning about unused variable in realease mode

--- samples/tiff-test.cpp
+++ samples/tiff-test.cpp
@@ -3,6 +3,7 @@
 // First and very simple TIFF write test.
 
 #include <exiv2/exiv2.hpp>
+#include <enforce.hpp>
 
 #include <string>
 #include <iostream>
@@ -41,25 +42,25 @@ void mini1(const char* path)
 
     // Write nothing to a new structure, without a previous binary image
     wm = ExifParser::encode(blob, 0, 0, bigEndian, exifData);
-    assert(wm == wmIntrusive);
+    enforce(wm == wmIntrusive, Exiv2::kerErrorMessage, "encode returned an unexpected value");
     assert(blob.size() == 0);
     std::cout << "Test 1: Writing empty Exif data without original binary data: ok.\n";
 
     // Write nothing, this time with a previous binary image
     DataBuf buf = readFile(path);
     wm = ExifParser::encode(blob, buf.pData_, buf.size_, bigEndian, exifData);
-    assert(wm == wmIntrusive);
+    enforce(wm == wmIntrusive, Exiv2::kerErrorMessage, "encode returned an unexpected value");
     assert(blob.size() == 0);
     std::cout << "Test 2: Writing empty Exif data with original binary data: ok.\n";
 
     // Write something to a new structure, without a previous binary image
     exifData["Exif.Photo.DateTimeOriginal"] = "Yesterday at noon";
     wm = ExifParser::encode(blob, 0, 0, bigEndian, exifData);
-    assert(wm == wmIntrusive);
+    enforce(wm == wmIntrusive, Exiv2::kerErrorMessage, "encode returned an unexpected value");
     std::cout << "Test 3: Wrote non-empty Exif data without original binary data:\n";
     exifData.clear();
     ByteOrder bo = ExifParser::decode(exifData, &blob[0], (uint32_t) blob.size());
-    assert(bo == bigEndian);
+    enforce(bo == bigEndian, Exiv2::kerErrorMessage, "decode returned an unexpected value");
     print(exifData);
 }
 
--- src/value.cpp
+++ src/value.cpp
@@ -31,6 +31,7 @@
 #include "types.hpp"
 #include "error.hpp"
 #include "convert.hpp"
+#include "unused.h"
 
 // + standard includes
 #include <iostream>
@@ -495,7 +496,8 @@ namespace Exiv2 {
         std::string c = value_;
         if (charsetId() == unicode) {
             c = value_.substr(8);
-            std::string::size_type sz = c.size();
+            const size_t sz = c.size();
+            UNUSED(sz);
             if (byteOrder_ == littleEndian && byteOrder == bigEndian) {
                 convertStringCharset(c, "UCS-2LE", "UCS-2BE");
                 assert(c.size() == sz);
@@ -506,7 +508,8 @@ namespace Exiv2 {
             }
             c = value_.substr(0, 8) + c;
         }
-        if (c.size() == 0) return 0;
+        if (c.size() == 0)
+            return 0;
         assert(buf != 0);
         return static_cast<long>(c.copy(reinterpret_cast<char*>(buf), c.size()));
     }
