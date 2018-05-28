d7f4453dbdbb9aeae7d24d397479e0fbaa5c3464
Dan Čermák <dan.cermak@cgc-instruments.com>
2018-05-28 20:41:32 +0200

    [testsuite] Remove usage of $cat via hooks

--- tests/bugfixes/redmine/test_issue_751.py
+++ tests/bugfixes/redmine/test_issue_751.py
@@ -15,18 +15,20 @@ class AdobeXmpNamespace(metaclass=CaseMeta):
     commands = [
         """$exiv2 -v -M"reg imageapp orig/" -M "set Xmp.imageapp.uuid abcd" $filename""",
         "$exiv2 -f -eX $filename",
-        "$cat $xmpname",
         """$exiv2 -v -M"reg imageapp dest/" -M "set Xmp.imageapp.uuid abcd" $filename""",
         "$exiv2 -f -eX $filename",
-        "$cat $xmpname",
     ]
 
-    stdout = [
-        """File 1/1: $filename
-Reg imageapp="orig/"
-Set Xmp.imageapp.uuid "abcd" (XmpText)
-""",
-        "",
+    def post_command_hook(self, i, command):
+        def read_xmpfile():
+            with open(self.xmpname, "r", encoding='utf-8') as xmp:
+                return xmp.read(-1)
+
+        if i == 2 or i == 4:
+            self.assertMultiLineEqual(self.xmp_packets[i//2 - 1], read_xmpfile())
+
+
+    xmp_packets = [
         """<?xpacket begin="\ufeff" id="W5M0MpCehiHzreSzNTczkc9d"?>
 <x:xmpmeta xmlns:x="adobe:ns:meta/" x:xmptk="XMP Core 4.4.0-Exiv2">
  <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
@@ -36,11 +38,6 @@ Set Xmp.imageapp.uuid "abcd" (XmpText)
  </rdf:RDF>
 </x:xmpmeta>
 <?xpacket end="w"?>""",
-        """File 1/1: $filename
-Reg imageapp="dest/"
-Set Xmp.imageapp.uuid "abcd" (XmpText)
-""",
-        "",
         """<?xpacket begin="\ufeff" id="W5M0MpCehiHzreSzNTczkc9d"?>
 <x:xmpmeta xmlns:x="adobe:ns:meta/" x:xmptk="XMP Core 4.4.0-Exiv2">
  <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
@@ -50,16 +47,26 @@ Set Xmp.imageapp.uuid "abcd" (XmpText)
  </rdf:RDF>
 </x:xmpmeta>
 <?xpacket end="w"?>"""
+    ]
 
+    stdout = [
+        """File 1/1: $filename
+Reg imageapp="orig/"
+Set Xmp.imageapp.uuid "abcd" (XmpText)
+""",
+        "",
+        """File 1/1: $filename
+Reg imageapp="dest/"
+Set Xmp.imageapp.uuid "abcd" (XmpText)
+""",
+        "",
     ]
     stderr = [
-        "",
         "",
         "",
         """Warning: Updating namespace URI for imageapp from orig/ to dest/
 """,
         """Warning: Updating namespace URI for imageapp from dest/ to orig/
 """,
-        ""
     ]
-    retval = [0] * 6
+    retval = [0] * 4
