e67910a6691f5238e85d5a43477af7dd13f0574e
Dan Čermák <dan.cermak@cgc-instruments.com>
2018-07-29 01:11:21 +0200

    [tests] Change name of test for #365 and #366
    Issues got a CVE assigned

--- tests/bugfixes/github/test_CVE_2018_12265.py
+++ tests/bugfixes/github/test_CVE_2018_12265.py
@@ -7,6 +7,8 @@ class AdditionOverflowInLoaderExifJpeg(metaclass=system_tests.CaseMeta):
     """
     Regression test for bug #365:
     https://github.com/Exiv2/exiv2/issues/365
+    aka CVE 2018-12265:
+    https://cve.mitre.org/cgi-bin/cvename.cgi?name=2018-12265
     """
     filename = system_tests.path("$data_path/1-out-of-read-Poc")
     commands = ["$exiv2 -ep $filename"]
