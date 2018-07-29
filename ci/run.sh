59585fa3af379ee88ea7c30467f7f084f6bf0575
Luis Diaz Mas <piponazo@gmail.com>
2018-07-29 21:11:19 +0200

    [Travis] export TZ=UTC before running tests
    This is needed in some tests which are using exiv2json.
    It seems that during metadata conversion, the dates are converted from the
    original timezone (which seems to be saved in xmp) into the local time zone.

--- ci/run.sh
+++ ci/run.sh
@@ -13,6 +13,8 @@ else
     pyenv activate conan
 fi
 
+
+
 mkdir build && cd build
 conan install .. --build missing --profile release
 cmake ${CMAKE_OPTIONS} -DCMAKE_INSTALL_PREFIX=install \
@@ -21,6 +23,10 @@ cmake ${CMAKE_OPTIONS} -DCMAKE_INSTALL_PREFIX=install \
       -DCMAKE_EXE_LINKER_FLAGS="-fsanitize=address" \
       -DCMAKE_MODULE_LINKER_FLAGS="-fsanitize=address" ..
 make -j2 VERBOSE=1
+
+#On most systems, you can set the TZ environment variable to set the timezone for a process. It's a POSIX feature.
+export TZ=UTC
+
 make tests
 make install
 cd bin
