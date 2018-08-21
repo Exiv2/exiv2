a947ee35d365e7976741ea7196a557be7f044598
Dan Čermák <dan.cermak@cgc-instruments.com>
2018-08-21 23:33:08 +0200

    [Travis CI] Enable all sanitizers for builds

--- ci/run.sh
+++ ci/run.sh
@@ -14,14 +14,9 @@ else
 fi
 
 
-
 mkdir build && cd build
 conan install .. --build missing --profile release
-cmake ${CMAKE_OPTIONS} -DCMAKE_INSTALL_PREFIX=install \
-      -DCMAKE_CXX_FLAGS="-fsanitize=address" \
-      -DCMAKE_C_FLAGS="-fsanitize=address" \
-      -DCMAKE_EXE_LINKER_FLAGS="-fsanitize=address" \
-      -DCMAKE_MODULE_LINKER_FLAGS="-fsanitize=address" ..
+cmake ${CMAKE_OPTIONS} -DCMAKE_INSTALL_PREFIX=install ..
 make -j2 VERBOSE=1
 
 #On most systems, you can set the TZ environment variable to set the timezone for a process. It's a POSIX feature.
