597b17fff594d47ea02bfc82d9279f1740c0bed2
Luis Díaz Más <piponazo@gmail.com>
2018-09-02 19:05:01 +0200

    Simplify timegm.h and make it usable with MINGW

--- src/timegm.h
+++ src/timegm.h
@@ -3,22 +3,10 @@
   @brief   Declaration of timegm(). The implementation is in localtime.c
 */
 #pragma once
-
 #include <time.h>
 
-#ifdef  __cplusplus
-extern "C" {
-#endif
-
 // The UTC version of mktime
-/* rmills - timegm is replaced with _mkgmtime on VC 2005 and up */
-/*        - see localtime.c                                     */
-#if !defined(_MSC_VER) || (_MSC_VER < 1400)
-time_t timegm(struct tm * const tmp);
-#else
+/* timegm is replaced with _mkgmtime on Windows (msvc && mingw) */
+#if defined(__MINGW__) || defined(_MSC_VER)
 #define timegm _mkgmtime
-#endif
-
-#ifdef  __cplusplus
-}
 #endif
