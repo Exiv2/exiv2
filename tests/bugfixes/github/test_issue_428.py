cfc7a1c4dd0ed2b30a88466291c3deef7d5cd6c5
Luis Díaz Más <piponazo@gmail.com>
2018-09-10 21:10:41 +0200

    Add remaining pocs for the issue 428 and adapt the regression test

--- tests/bugfixes/github/test_issue_428.py
+++ tests/bugfixes/github/test_issue_428.py
@@ -2,6 +2,10 @@
 
 import system_tests
 
+def stderr_exception (fname):
+    return """$exiv2_exception_message """ + fname + """:
+$kerFailedToReadImageData
+"""
 
 class PngReadRawProfile(metaclass=system_tests.CaseMeta):
 
@@ -12,14 +16,20 @@ class PngReadRawProfile(metaclass=system_tests.CaseMeta):
         system_tests.path("$data_path/issue_428_poc2.png"),
         system_tests.path("$data_path/issue_428_poc3.png"),
         system_tests.path("$data_path/issue_428_poc4.png"),
+        system_tests.path("$data_path/issue_428_poc5.png"),
+        system_tests.path("$data_path/issue_428_poc8.png"),
+
+        system_tests.path("$data_path/issue_428_poc6.png"),
+        system_tests.path("$data_path/issue_428_poc7.png"),
     ]
 
     commands = ["$exiv2 " + fname for fname in filenames]
     stdout = [""] * len(filenames)
-    stderr = [
-        """$exiv2_exception_message """ + fname + """:
-$kerFailedToReadImageData
-"""
-        for fname in filenames
-    ]
+    stderr = [ stderr_exception(fname) for fname in filenames[0:6] ]
+    stderr.append("""Error: XMP Toolkit error 201: XML parsing failure
+Warning: Failed to decode XMP metadata.
+""" + stderr_exception(filenames[6]))
+    stderr.append("""Warning: Failed to decode Exif metadata.
+""" + stderr_exception(filenames[7]))
+
     retval = [1] * len(filenames)
