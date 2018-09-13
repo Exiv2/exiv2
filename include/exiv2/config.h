a0e7b7490f654f6bffbdfe956a935de91a546ae4
Luis Díaz Más <piponazo@gmail.com>
2018-09-13 18:28:14 +0200

    Add MSVC definitions back that are still needed

--- include/exiv2/config.h
+++ include/exiv2/config.h
@@ -6,6 +6,10 @@
 ///// Start of Visual Studio Support /////
 #ifdef  _MSC_VER
 
+#define _MSC_VER_2010 1600
+#define _MSC_VER_2008 1500
+#define _MSC_VER_2005 1400
+
 #if  _MSC_VER >= _MSC_VER_2005
 #define MSDEV_2005    1
 #else
