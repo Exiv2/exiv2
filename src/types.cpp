916c535d944edd8df062cf7c081baf85c434dad6
Dan Čermák <dan.cermak@cgc-instruments.com>
2018-09-17 09:35:00 +0200

    clang-format floatToRationalCast

--- src/types.cpp
+++ src/types.cpp
@@ -683,14 +683,20 @@ namespace Exiv2 {
         // Beware: primitive conversion algorithm
         int32_t den = 1000000;
         const long f_as_long = static_cast<long>(f);
-        if (Safe::abs(f_as_long) > 2147) den = 10000;
-        if (Safe::abs(f_as_long) > 214748) den = 100;
-        if (Safe::abs(f_as_long) > 21474836) den = 1;
+        if (Safe::abs(f_as_long) > 2147) {
+            den = 10000;
+        }
+        if (Safe::abs(f_as_long) > 214748) {
+            den = 100;
+        }
+        if (Safe::abs(f_as_long) > 21474836) {
+            den = 1;
+        }
         const float rnd = f >= 0 ? 0.5f : -0.5f;
         const int32_t nom = static_cast<int32_t>(f * den + rnd);
         const int32_t g = gcd(nom, den);
 
-        return Rational(nom/g, den/g);
+        return Rational(nom / g, den / g);
     }
 
 }                                       // namespace Exiv2
