2cfa65831b0bf6e86abeef157590b2084d40ca95
Luis Díaz Más <piponazo@gmail.com>
2018-10-13 11:31:56 +0200

    Add reproducer for #460

---
+++ tests/bugfixes/github/test_issue_460.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import system_tests
+
+
+class ThrowsWhenCRWImageIsMalformed(metaclass=system_tests.CaseMeta):
+    url = "https://github.com/Exiv2/exiv2/issues/460"
+
+    filename = system_tests.path("$data_path/issue_460")
+    commands = ["$exiv2 " + filename]
+    retval = [1]
+    stderr = ["""$exiv2_exception_message """ + filename + """:
+$kerCorruptedMetadata
+"""]
