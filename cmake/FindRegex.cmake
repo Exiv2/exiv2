87278e6be3b99b04ea107d5ffc4b0cfe95014d33
Robin Mills <robin@clanmills.com>
2018-10-15 17:50:05 +0100

    MSYS2/MinGW: Disable Regex

--- cmake/FindRegex.cmake
+++ cmake/FindRegex.cmake
@@ -11,38 +11,40 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
+if ( NOT MSVC AND NOT MINGW AND NOT MSYS )
+    find_path(Regex_INCLUDE_DIR
+              NAMES regex.h
+              DOC "libregex include directory"
+    )
 
-find_path(Regex_INCLUDE_DIR
-          NAMES regex.h
-          DOC "libregex include directory"
-)
+    mark_as_advanced(Regex_INCLUDE_DIR)
 
-mark_as_advanced(Regex_INCLUDE_DIR)
+    find_library(Regex_LIBRARY "regex"
+                 DOC "libregex libraries"
+    )
 
-find_library(Regex_LIBRARY "regex"
-             DOC "libregex libraries"
-)
+    mark_as_advanced(Regex_LIBRARY)
 
-mark_as_advanced(Regex_LIBRARY)
+    find_package_handle_standard_args(Regex
+                                      FOUND_VAR Regex_FOUND
+                                      REQUIRED_VARS Regex_INCLUDE_DIR
+                                      FAIL_MESSAGE "Failed to find libregex"
+    )
 
-find_package_handle_standard_args(Regex
-                                  FOUND_VAR Regex_FOUND
-                                  REQUIRED_VARS Regex_INCLUDE_DIR
-                                  FAIL_MESSAGE "Failed to find libregex"
-)
+    if(REGEX_FOUND)
 
-if(REGEX_FOUND)
+        set(REGEX_INCLUDE_DIRS ${Regex_INCLUDE_DIRS})
 
-    set(REGEX_INCLUDE_DIRS ${Regex_INCLUDE_DIRS})
+        if(Regex_LIBRARY)
 
-    if(Regex_LIBRARY)
+            set(REGEX_LIBRARIES ${Regex_LIBRARY})
 
-        set(REGEX_LIBRARIES ${Regex_LIBRARY})
+        else()
 
-    else()
+            unset(REGEX_LIBRARIES)
 
-        unset(REGEX_LIBRARIES)
+        endif()
 
     endif()
-
 endif()
+
