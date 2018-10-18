8baa316afce529ac62daa6d6bfbc4e295e57a2a7
Robin Mills <robin@clanmills.com>
2018-10-18 20:49:01 +0100

    Implemented options --status --clone and --publish

--- contrib/buildserver/build.sh
+++ contrib/buildserver/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 syntax() {
-    echo "usage: $0 { --help | -? | -h | platform | option | switch }+ "
-    echo "platform: all | cygwin | linux | macosx | mingw | mingw32 | msvc "
-    echo "switch:   --2015 | --2017 | --publish | --verbose | --static | --status"
+    echo "usage: $0 { --help | -? | -h | platform | option value | switch }+ "
+    echo "platform:  all | cygwin | linux | macosx | mingw | mingw32 | msvc"
+    echo "switch: --2015 | --2017 | --publish | --status | --clone"
     echo "option: --branch x | --server x | --user x"
 }
 
@@ -29,19 +29,25 @@ if [ "$status" == "1" ]; then
     ! ssh $1 ${command} <<EOF
 cd ${cd}/buildserver/build
 ls -alt *.tar.gz | sed -E -e 's/\+ / /g' # remove extended attribute marker
-# echo +++ scp *.tar.gz rmills@rmillsmm:/mmHD/Users/Shared/Jenkins/Home/userContent/builds/$tag
 EOF
 else
+    # remove the buildserver directory if we are to clone
+    if [ "$clone" == "1" ];then
+        ssh $1 ${command} <<EOF
+          rm -rf ${cd}/buildserver
+EOF
+    fi
     ! ssh $1 ${command} <<EOF
 PATH="/usr/local/bin/:/usr/bin:/mingw64/bin:$PATH"
 cd ${cd}
-mkdir -p buildserver
-rm   -rf buildserver
-git clone --branch $branch https://github.com/exiv2/exiv2 buildserver
+if [ ! -e buildserver ]; then
+    git clone --branch $branch https://github.com/exiv2/exiv2 buildserver
+fi
 cd       buildserver
 git pull --rebase
 mkdir -p build
 cd       build
+echo 'tag='$tag > tag # leave our finger prints for 'publish'
 cmake .. -G "Unix Makefiles"
 make
 make tests
@@ -66,22 +72,27 @@ if [ "$status" == "1" ]; then
     ! ssh $1 msys64 <<EOF
 cd ${cd}buildserver\\\\build
 ls -alt *.zip
-# echo scp exiv2-0.27.0.1-msvc.zip rmills@rmillsmm:/mmHD/Users/Shared/Jenkins/Home/userContent/builds/$tag-exiv2-0.27.0.1-msvc.zip
 EOF
 else
+    if [ $clone == 1 ]; then
+    ! ssh $1 "rmdir/s/q ${cd}buildserver" 2>/dev/null
+    fi
     ! ssh $1 <<EOF
 cd ${cd}
-IF EXIST buildserver rmdir/s/q buildserver
-git clone --branch ${branch} https://github.com/exiv2/exiv2 buildserver
+IF NOT EXIST buildserver git clone --branch ${branch} https://github.com/exiv2/exiv2 buildserver
 cd buildserver
 git pull --rebase
-mkdir    build
-cd       build
+if NOT EXIST build mkdir build
+cd           build
 conan install .. --profile ${profile} --build missing
 cmake         .. -G ${generator} -DCMAKE_BUILD_TYPE=${config}  -DCMAKE_INSTALL_PREFIX=..\\dist\\${profile}
 cmake --build .  --config ${config}   --target install
 cmake --build .  --config ${config}   --target package
 ls -alt *.zip
+EOF
+    # write tag into the build directory (in Unix format without \r)
+    ! ssh $1 msys64 <<EOF
+echo tag=$tag > ${cd}buildserver\\\\build\\\\tag
 EOF
 fi
 }
@@ -101,13 +112,12 @@ static=0
 edition=2017
 branch=RC1
 dryrun=0
+clone=0
 server=rmillsmm
 user=rmills
 status=0
 all=0
-date=$(date '+%Y-%m-%d+%H-%M-%S')
-tag=${date}
-
+tag=$(date '+%Y:%m:%d_%H:%M:%S')
 if [ "$#" == "0" ]; then help=1; fi
 
 ##
@@ -124,9 +134,8 @@ while [ "$#" != "0" ]; do
       mingw)     mingw=1       ;;
       mingw32)   mingw32=1     ;;
       msvc)      msvc=1        ;;
-      --verbose) verbose=1     ;;
-      --dryrun)  dryrun=1      ;;
       --publish) publish=1     ;;
+      --clone)   clone=1       ;;
       --static)  static=1      ;;
       --2017)    edition=2017  ;;
       --2015)    edition=2015  ;;
@@ -146,6 +155,38 @@ fi
 if [ "$all" == "1" ]; then
     cygwin=1; linux=1; macosx=1; mingw=1; mingw32=1;msvc=1;
 fi
+
+publishBundle()
+{
+	# $1 = server    (eg rmillsmm-w7)
+	# $2 = path      (eg /c/msys32/home/rmills/gnu/github/exiv2/buildserver/build)
+	# $3 = extension (eg tar.gz or zip)
+	# find the build tag left during the build
+	tag_saved=$tag
+	if [ -e tag ]; then rm -rf tag ; fi
+	scp -q "$user@$1:$2/tag" . 2>/dev/null  # silently collect build tag file
+	if [ -e tag ]; then
+		source tag
+	fi
+
+	file=$(ssh $user@$1 "ls -1 $2/*$3" 2>/dev/null)
+	if [ ! -z $file ]; then
+	    scp -p "$user@$1:$file" $dest/$(basename $file $3)-$tag$3
+	fi
+	tag=$tag_saved
+}
+
+if [ $publish == 1 ]; then
+    dest=/Users/rmills/Jenkins/builds/new
+    publishBundle $server-w7     /c/msys32/home/$user/gnu/github/exiv2/buildserver/build   '.tar.gz'
+    publishBundle $server-w7     /c/msys64/home/$user/gnu/github/exiv2/buildserver/build   '.tar.gz'
+    publishBundle $server-w7     /c/cygwin64/home/$user/gnu/github/exiv2/buildserver/build '.tar.gz'
+    publishBundle $server-w7     /c/users/$user/gnu/github/exiv2/buildserver/build         '.zip'
+    publishBundle $server-ubuntu /home/$user/gnu/github/exiv2/buildserver/build            '.tar.gz'
+    publishBundle $server        /Users/$user/gnu/github/exiv2/buildserver/build           '.tar.gz'
+    exit 0
+fi
+
 
 ##
 # perform builds
@@ -183,6 +224,7 @@ if [ $msvc == 1 ]; then
     command=''
     msvcBuild ${user}@${server}-w7
 fi
+
 
 # That's all Folks
 ##
