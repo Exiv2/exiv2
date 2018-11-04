18ead129827b983094b0da5f13aeb1e477028202
Luis Diaz Mas <piponazo@gmail.com>
2018-11-04 21:55:10 +0100

    Add regression test for #511

---
+++ tests/bugfixes/github/test_issue_511.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import system_tests
+
+
+class ThrowsWhenIFDsAreMalformed(metaclass=system_tests.CaseMeta):
+    url = "https://github.com/Exiv2/exiv2/issues/511"
+
+    filename = system_tests.path("$data_path/pocIssue511")
+    commands = ["$exiv2 -pR " + filename]
+    retval = [1]
+    stdout = ["""STRUCTURE OF TIFF FILE (II): """ + filename + """
+"""]
+    stderr = ["""$exiv2_exception_message """ + filename + """:
+$kerCorruptedMetadata
+"""]
