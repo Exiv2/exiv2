b6bc4262c45fbfca238e8ca3178a5cc0697fed7c
Luis Diaz Mas <piponazo@gmail.com>
2018-11-04 22:48:22 +0100

    Add regression test for #426

---
+++ tests/bugfixes/github/test_issue_426.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import system_tests
+
+
+class DetectsWrongLengthOfImageResourceInPSDFile(metaclass=system_tests.CaseMeta):
+    url = "https://github.com/Exiv2/exiv2/issues/426"
+
+    filename = system_tests.path("$data_path/h02.psd")
+    commands = ["$exiv2 " + filename]
+    retval = [1]
+    stderr = ["""$exiv2_exception_message """ + filename + """:
+$kerCorruptedMetadata
+"""]
