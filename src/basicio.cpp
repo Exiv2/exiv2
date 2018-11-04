59013f2e2fc5ee51bc71e85222f56a35f8daa570
Luis Díaz Más <piponazo@gmail.com>
2018-11-04 18:43:54 +0100

    Formatting code

--- src/basicio.cpp
+++ src/basicio.cpp
@@ -915,7 +915,9 @@ namespace Exiv2 {
         case BasicIo::end: fileSeek = SEEK_END; break;
         }
 
-        if (p_->switchMode(Impl::opSeek) != 0) return 1;
+        if (p_->switchMode(Impl::opSeek) != 0) {
+            return 1;
+        }
         return std::fseek(p_->fp_, offset, fileSeek);
     }
 #endif
@@ -997,7 +999,9 @@ namespace Exiv2 {
     long FileIo::read(byte* buf, long rcount)
     {
         assert(p_->fp_ != 0);
-        if (p_->switchMode(Impl::opRead) != 0) return 0;
+        if (p_->switchMode(Impl::opRead) != 0) {
+            return 0;
+        }
         return (long)std::fread(buf, 1, rcount, p_->fp_);
     }
 
