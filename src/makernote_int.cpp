e397f8f0532f4a9b80507ab39d97f818f7fb4fde
Luis Diaz Mas <piponazo@gmail.com>
2018-11-13 22:15:43 +0100

    Fix #533 - Apply patch proposed by Dominique Nerriec

--- src/makernote_int.cpp
+++ src/makernote_int.cpp
@@ -989,7 +989,7 @@ namespace Exiv2 {
             // Require at least the header and an IFD with 1 entry
             if (size < PentaxDngMnHeader::sizeOfSignature() + 18)
                 return 0;
-            return newPentaxDngMn2(tag, group, pentaxDngId);
+            return newPentaxDngMn2(tag, group, (tag == 0xc634 ? pentaxDngId:pentaxId));
         } else if (size > 4 && std::string(reinterpret_cast<const char*>(pData), 4) == std::string("AOC\0", 4)) {
             // Require at least the header and an IFD with 1 entry
             if (size < PentaxMnHeader::sizeOfSignature() + 18)
