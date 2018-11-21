9e0bbd02052a9550989b002bcaadd72f7dc837dd
Luis Díaz Más <piponazo@gmail.com>
2018-11-21 17:15:03 +0100

    Conan: updating to expat 2.2.6

--- conanfile.py
+++ conanfile.py
@@ -35,7 +35,7 @@ class Exiv2Conan(ConanFile):
         if self.options.xmp:
             self.requires('XmpSdk/2016.7@piponazo/stable') # from conan-piponazo
         else:
-            self.requires('Expat/2.2.5@pix4d/stable')
+            self.requires('Expat/2.2.6@pix4d/stable')
 
     def imports(self):
         self.copy('*.dll', dst='conanDlls', src='bin')
