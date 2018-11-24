045af8ea3661f3c375a82ef6ff14fae7784c794e
Robin Mills <robin@clanmills.com>
2018-11-24 19:24:37 +0000

    add logs to the build bundle

--- cmake/packaging.cmake
+++ cmake/packaging.cmake
@@ -61,6 +61,8 @@ set( DOCS
 foreach(doc ${DOCS})
     install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/${doc} DESTINATION .)
 endforeach()
+install(FILES ${PROJECT_SOURCE_DIR}/build/logs/build.txt DESTINATION "logs")
+install(FILES ${PROJECT_SOURCE_DIR}/build/logs/test.txt  DESTINATION "logs")
 
 # Copy releasenotes.txt and appropriate ReadMe.txt (eg releasenotes/${PACKDIR}/ReadMe.txt)
 install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/releasenotes/${PACKDIR}/ReadMe.txt DESTINATION .)
