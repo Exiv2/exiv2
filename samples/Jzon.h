5de0db60d0203c5439ce6d56676d06af8b374df9
Luis Diaz Mas <piponazo@gmail.com>
2018-12-19 08:17:37 +0100

    cppcheck: this was failing on VS 2008

--- samples/Jzon.h
+++ samples/Jzon.h
@@ -47,7 +47,7 @@ namespace Jzon
     template<typename T1, typename T2>
     struct Pair
     {
-        Pair(const T1& first, const T2& second) : first(first), second(second)
+        Pair(T1 first, T2 second) : first(first), second(second)
         {}
 
         Pair &operator=(const Pair &rhs)
