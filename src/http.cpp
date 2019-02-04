b3636007ebddbe488f2b1e7456c679f3e1b58999
tbeu <tc@tbeu.de>
2019-02-04 19:40:27 +0100

    Fix resource leaks

--- src/http.cpp
+++ src/http.cpp
@@ -272,9 +272,6 @@ int Exiv2::http(Exiv2::Dictionary& request, Exiv2::Dictionary& response, std::st
     if (sockfd < 0)
         return error(errors, "unable to create socket\n", nullptr, nullptr, 0);
 
-    // connect the socket to the server
-    int server = -1;
-
     // fill in the address
     struct sockaddr_in serv_addr;
     int serv_len = sizeof(serv_addr);
@@ -288,19 +285,24 @@ int Exiv2::http(Exiv2::Dictionary& request, Exiv2::Dictionary& response, std::st
     // http://publib.boulder.ibm.com/infocenter/iseries/v5r3/index.jsp?topic=/rzab6/rzab6uafinet.htm
     if (serv_addr.sin_addr.s_addr == (unsigned long)INADDR_NONE) {
         struct hostent* host = gethostbyname(servername_p);
-        if (!host)
+        if (!host) {
+            closesocket(sockfd);
             return error(errors, "no such host", servername_p);
+        }
         memcpy(&serv_addr.sin_addr, host->h_addr, sizeof(serv_addr.sin_addr));
     }
 
     makeNonBlocking(sockfd);
 
     ////////////////////////////////////
-    // and connect
-    server = connect(sockfd, (const struct sockaddr*)&serv_addr, serv_len);
-    if (server == SOCKET_ERROR && WSAGetLastError() != WSAEWOULDBLOCK)
+    // connect the socket to the server
+    auto server = connect(sockfd, (const struct sockaddr*)&serv_addr, serv_len);
+    if (server == SOCKET_ERROR && WSAGetLastError() != WSAEWOULDBLOCK) {
+        auto errorCode = WSAGetLastError();
+        closesocket(sockfd);
         return error(errors, "error - unable to connect to server = %s port = %s wsa_error = %d", servername_p, port_p,
-                     WSAGetLastError());
+                     errorCode);
+    }
 
     char buffer[32 * 1024 + 1];
     size_t buff_l = sizeof buffer - 1;
@@ -318,9 +320,13 @@ int Exiv2::http(Exiv2::Dictionary& request, Exiv2::Dictionary& response, std::st
         sleep_ -= snooze;
     }
 
-    if (sleep_ < 0)
+    if (sleep_ < 0) {
+        auto errorCode = WSAGetLastError();
+        closesocket(server);
+        closesocket(sockfd);
         return error(errors, "error - timeout connecting to server = %s port = %s wsa_error = %d", servername, port,
-                     WSAGetLastError());
+                     errorCode);
+    }
 
     int end = 0;             // write position in buffer
     bool bSearching = true;  // looking for headers in the response
@@ -398,8 +404,11 @@ int Exiv2::http(Exiv2::Dictionary& request, Exiv2::Dictionary& response, std::st
             //  we finished OK without finding headers, flush the buffer
             flushBuffer(buffer, 0, end, file);
         } else {
+            auto errorCode = WSAGetLastError();
+            closesocket(server);
+            closesocket(sockfd);
             return error(errors, "error - no response from server = %s port = %s wsa_error = %d", servername, port,
-                         WSAGetLastError());
+                         errorCode);
         }
     }
 
