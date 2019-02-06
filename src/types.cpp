7ea519e93a1636d6682e8c3fb6635ec53a5997d6
Dan Čermák <dan.cermak@cgc-instruments.com>
2019-02-06 23:05:18 +0100

    [localization] Add a mutex guarding the global exvGettextInitialized

--- src/types.cpp
+++ src/types.cpp
@@ -49,6 +49,7 @@
 #include <cstring>
 #include <cmath>
 #include <math.h>
+#include <mutex>
 
 // *****************************************************************************
 namespace {
@@ -722,21 +723,30 @@ namespace Exiv2 {
 }                                       // namespace Exiv2
 
 #ifdef EXV_ENABLE_NLS
+
+namespace
+{
+    bool exvGettextInitialized = false;
+    std::mutex exvGettextInitializedMutex;
+}  // namespace
+
 // Declaration is in i18n.h
 const char* _exvGettext(const char* str)
 {
-    static bool exvGettextInitialized = false;
+    // hold the mutex only as long as necessary
+    {
+        std::lock_guard<std::mutex> lock(exvGettextInitializedMutex);
 
-    if (!exvGettextInitialized) {
-        //bindtextdomain(EXV_PACKAGE_NAME, EXV_LOCALEDIR);
-        const std::string localeDir = Exiv2::getProcessPath() + EXV_LOCALEDIR;
-        bindtextdomain(EXV_PACKAGE_NAME, localeDir.c_str());
-# ifdef EXV_HAVE_BIND_TEXTDOMAIN_CODESET
-        bind_textdomain_codeset (EXV_PACKAGE_NAME, "UTF-8");
-# endif
-        exvGettextInitialized = true;
+        if (!exvGettextInitialized) {
+            // bindtextdomain(EXV_PACKAGE_NAME, EXV_LOCALEDIR);
+            const std::string localeDir = Exiv2::getProcessPath() + EXV_LOCALEDIR;
+            bindtextdomain(EXV_PACKAGE_NAME, localeDir.c_str());
+#ifdef EXV_HAVE_BIND_TEXTDOMAIN_CODESET
+            bind_textdomain_codeset(EXV_PACKAGE_NAME, "UTF-8");
+#endif
+            exvGettextInitialized = true;
+        }
     }
-
     return dgettext(EXV_PACKAGE_NAME, str);
 }
 #endif // EXV_ENABLE_NLS
