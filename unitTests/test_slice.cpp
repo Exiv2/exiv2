adcdb4f7d3da65c9ec39ab393266b350dfeabb61
Dan Čermák <dan.cermak@cgc-instruments.com>
2019-02-26 00:42:40 +0100

    [unit_tests] Refactor slice test internals to use function overloading
    The function get_test_data was relying on template specializations, which are
    not required here: we can simply overload the function.

--- unitTests/test_slice.cpp
+++ unitTests/test_slice.cpp
@@ -11,44 +11,24 @@ template <typename T>
 class slice;
 
 /*!
- * This namespace contains the helper-function get_test_data. It is intented
- * to be used for test with the slice fixture: it returns the appropriate
+ * This namespace contains the helper-function getTestData. It is intented
+ * to be used for tests with the slice fixture: it returns the appropriate
  * data to the constructor of slice. For (const) T==std::vector it returns the
- * fixtures meber vec_, for (const) T==int* it returns vec_.data()
+ * fixtures member vec_, for (const) T==int* it returns vec_.data()
  *
- * Due to C++98's limitations, this requires a separate traits class, that
- * specifies the return type *and* a specialization of get_test_data for each
- * case (maybe some can be reduced with SFINAE, but that ain't improving
- * readability either).
- *
- * Unfortunately, C++11 will probably only make the return_type_traits go away,
- * but not the template specializations of get_test_data (for that we need
- * C++17, so see you in 2025).
+ * As we don't have C++17 and can't use a single function with a big `constexpr
+ * if`, we instead overload getTestData for the four types that we test.
  */
-namespace cpp_98_boilerplate
+namespace cpp_pre_17_boilerplate
 {
-    template <typename T>
-    struct return_type_traits
-    {
-        typedef T type;
-    };
+    int* getTestData(slice<int*>& st);
 
-    template <typename U>
-    struct return_type_traits<std::vector<U> >
-    {
-        typedef typename std::vector<U>& type;
-    };
+    const int* getTestData(slice<const int*>& st);
 
-    template <typename U>
-    struct return_type_traits<const std::vector<U> >
-    {
-        typedef const typename std::vector<U>& type;
-    };
+    std::vector<int>& getTestData(slice<std::vector<int> >& st);
 
-    template <typename T>
-    typename return_type_traits<T>::type get_test_data(slice<T>& st);
-
-}  // namespace cpp_98_boilerplate
+    const std::vector<int>& getTestData(slice<const std::vector<int> >& st);
+}  // namespace cpp_pre_17_boilerplate
 
 /*!
  * Fixture for slice testing. Has one public vector of ints with size vec_size
@@ -77,41 +57,37 @@ public:
 
     Slice<T> getTestSlice(size_t begin = 1, size_t end = vec_size - 1)
     {
-        return Slice<T>(cpp_98_boilerplate::get_test_data<T>(*this), begin, end);
+        return Slice<T>(cpp_pre_17_boilerplate::getTestData(*this), begin, end);
     }
 
     // TODO: once we have C++11: use initializer list
     std::vector<int> vec_;
 };
 
-// specializations of get_test_data are provided here, since they must have the
-// full definition of slice available
-namespace cpp_98_boilerplate
+// implementation of the overloads of getTestData are provided here, since
+// they must have the full definition of slice available
+namespace cpp_pre_17_boilerplate
 {
-    template <>
-    int* get_test_data<int*>(slice<int*>& st)
+    int* getTestData(slice<int*>& st)
     {
         return st.vec_.data();
     }
 
-    template <>
-    const int* get_test_data<const int*>(slice<const int*>& st)
+    const int* getTestData(slice<const int*>& st)
     {
         return st.vec_.data();
     }
 
-    template <>
-    std::vector<int>& get_test_data<std::vector<int> >(slice<std::vector<int> >& st)
+    std::vector<int>& getTestData(slice<std::vector<int> >& st)
     {
         return st.vec_;
     }
 
-    template <>
-    const std::vector<int>& get_test_data<const std::vector<int> >(slice<const std::vector<int> >& st)
+    const std::vector<int>& getTestData(slice<const std::vector<int> >& st)
     {
         return st.vec_;
     }
-}  // namespace cpp_98_boilerplate
+}  // namespace cpp_pre_17_boilerplate
 
 /*!
  * Fixture to run test for mutable slices.
