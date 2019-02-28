73536d5861afd924776cbdff1276f6be35b32d1d
Luis Diaz Mas <piponazo@gmail.com>
2019-02-28 19:37:00 +0100

    Generate doxygen.hpp page

--- cmake/generateDoc.cmake
+++ cmake/generateDoc.cmake
@@ -15,9 +15,12 @@ macro(generate_documentation DOX_CONFIG_FILE)
     set(INCDIR  "${PROJECT_SOURCE_DIR}/include/exiv2")
     set(SRCDIR  "${PROJECT_SOURCE_DIR}/src")
     set(ROOTDIR "${PROJECT_SOURCE_DIR}")
+    set(BINDIR  "${PROJECT_BINARY_DIR}")
     #set(TESTSDIR "${PROJECT_SOURCE_DIR}/tests")
 
     configure_file(${DOX_CONFIG_FILE} ${CMAKE_CURRENT_BINARY_DIR}/doxy.config @ONLY) #OUT-OF-PLACE LOCATION
+    configure_file(${PROJECT_SOURCE_DIR}/src/doxygen.hpp.in ${PROJECT_BINARY_DIR}/doxygen.hpp @ONLY)
+
     set(DOXY_CONFIG "${CMAKE_CURRENT_BINARY_DIR}/doxy.config")
     add_custom_target(doc ${DOXYGEN_EXECUTABLE} ${DOXY_CONFIG})
 
