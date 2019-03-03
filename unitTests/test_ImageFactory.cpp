72c5915c6f02cc49795d717341bf915c9f137eb8
Luis Díaz Más <piponazo@gmail.com>
2019-03-03 13:07:58 +0100

    Split test identifying which Images can be created and which not

--- unitTests/test_ImageFactory.cpp
+++ unitTests/test_ImageFactory.cpp
@@ -18,40 +18,44 @@
 #include <tiffimage.hpp>
 #include <webpimage.hpp>
 
+#include <error.hpp> // Need to include this header for the Exiv2::Error exception
+
 #include <gtest/gtest.h>
 
 using namespace Exiv2;
 
-TEST(TheImageFactory, createsInstancesOfAllSupportedTypes)
+TEST(TheImageFactory, createsInstancesForSupportedTypes)
 {
+    // Note that the constructor of these Image classes take an 'create' argument
     EXPECT_NO_THROW(ImageFactory::create(ImageType::jp2));
     EXPECT_NO_THROW(ImageFactory::create(ImageType::jpeg));
     EXPECT_NO_THROW(ImageFactory::create(ImageType::exv));
     EXPECT_NO_THROW(ImageFactory::create(ImageType::pgf));
     EXPECT_NO_THROW(ImageFactory::create(ImageType::png));
+}
 
+TEST(TheImageFactory, cannotCreateInstancesForSomeTypes)
+{
+    // Note that the constructor of these Image classes does not take an 'create' argument
 
-    // The next expectations fail ... O_O ...
-
-    EXPECT_NO_THROW(ImageFactory::create(ImageType::bmp));
-    EXPECT_NO_THROW(ImageFactory::create(ImageType::cr2));
-    EXPECT_NO_THROW(ImageFactory::create(ImageType::crw));
-    EXPECT_NO_THROW(ImageFactory::create(ImageType::gif));
-    EXPECT_NO_THROW(ImageFactory::create(ImageType::mrw));
-    EXPECT_NO_THROW(ImageFactory::create(ImageType::orf));
-    EXPECT_NO_THROW(ImageFactory::create(ImageType::psd));
-    EXPECT_NO_THROW(ImageFactory::create(ImageType::raf));
-    EXPECT_NO_THROW(ImageFactory::create(ImageType::rw2));
-    EXPECT_NO_THROW(ImageFactory::create(ImageType::tga));
+    EXPECT_THROW(ImageFactory::create(ImageType::bmp), Error);
+    EXPECT_THROW(ImageFactory::create(ImageType::cr2), Error);
+    EXPECT_THROW(ImageFactory::create(ImageType::crw), Error);
+    EXPECT_THROW(ImageFactory::create(ImageType::gif), Error);
+    EXPECT_THROW(ImageFactory::create(ImageType::mrw), Error);
+    EXPECT_THROW(ImageFactory::create(ImageType::orf), Error);
+    EXPECT_THROW(ImageFactory::create(ImageType::psd), Error);
+    EXPECT_THROW(ImageFactory::create(ImageType::raf), Error);
+    EXPECT_THROW(ImageFactory::create(ImageType::rw2), Error);
+//    EXPECT_THROW(ImageFactory::create(ImageType::tga), Error); // This one crashes badly
+    EXPECT_THROW(ImageFactory::create(ImageType::webp), Error);
 
     // TIFF
-    EXPECT_NO_THROW(ImageFactory::create(ImageType::tiff));
-    EXPECT_NO_THROW(ImageFactory::create(ImageType::dng));
-    EXPECT_NO_THROW(ImageFactory::create(ImageType::nef));
-    EXPECT_NO_THROW(ImageFactory::create(ImageType::pef));
-    EXPECT_NO_THROW(ImageFactory::create(ImageType::arw));
-    EXPECT_NO_THROW(ImageFactory::create(ImageType::sr2));
-    EXPECT_NO_THROW(ImageFactory::create(ImageType::srw));
-
-    EXPECT_NO_THROW(ImageFactory::create(ImageType::webp));
+    EXPECT_THROW(ImageFactory::create(ImageType::tiff), Error);
+    EXPECT_THROW(ImageFactory::create(ImageType::dng), Error);
+    EXPECT_THROW(ImageFactory::create(ImageType::nef), Error);
+    EXPECT_THROW(ImageFactory::create(ImageType::pef), Error);
+    EXPECT_THROW(ImageFactory::create(ImageType::arw), Error);
+    EXPECT_THROW(ImageFactory::create(ImageType::sr2), Error);
+    EXPECT_THROW(ImageFactory::create(ImageType::srw), Error);
 }
