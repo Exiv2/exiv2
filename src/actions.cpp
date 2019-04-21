41614952c08d7fe2c62b767e47236d95b8eb86ba
Luis Diaz Mas <piponazo@gmail.com>
2019-04-21 17:54:35 +0200

    Cosmetics
    - Remove useless assert
    - Do not pre-declare ExifKeys
    - Rename variable to make code more readable
    - Add some separation between related blocks of code

--- src/actions.cpp
+++ src/actions.cpp
@@ -792,35 +792,36 @@ namespace Action
                 ts.read(path);
 
             Exiv2::Image::UniquePtr image = Exiv2::ImageFactory::open(path);
-            assert(image.get() != 0);
             image->readMetadata();
             Exiv2::ExifData& exifData = image->exifData();
             if (exifData.empty()) {
                 std::cerr << path << ": " << _("No Exif data found in the file\n");
                 return -3;
             }
-            Exiv2::ExifKey key("Exif.Photo.DateTimeOriginal");
-            auto md = exifData.findKey(key);
+
+            auto md = exifData.findKey(Exiv2::ExifKey("Exif.Photo.DateTimeOriginal"));
             if (md == exifData.end()) {
-                key = Exiv2::ExifKey("Exif.Image.DateTime");
-                md = exifData.findKey(key);
+                md = exifData.findKey(Exiv2::ExifKey("Exif.Image.DateTime"));
             }
             if (md == exifData.end()) {
                 std::cerr << _("Neither tag") << " `Exif.Photo.DateTimeOriginal' " << _("nor")
                           << " `Exif.Image.DateTime' " << _("found in the file") << " " << path << "\n";
                 return 1;
             }
-            std::string v = md->toString();
-            if (v.length() == 0 || v[0] == ' ') {
+
+            std::string dateTime = md->toString();
+            if (dateTime.length() == 0 || dateTime[0] == ' ') {
                 std::cerr << _("Image file creation timestamp not set in the file") << " " << path << "\n";
                 return 1;
             }
+
             struct tm tm;
-            if (str2Tm(v, &tm) != 0) {
-                std::cerr << _("Failed to parse timestamp") << " `" << v << "' " << _("in the file") << " " << path
-                          << "\n";
+            if (str2Tm(dateTime, &tm) != 0) {
+                std::cerr << _("Failed to parse timestamp") << " `" << dateTime << "' " << _("in the file") << " "
+                          << path << "\n";
                 return 1;
             }
+
             if (Params::instance().timestamp_ || Params::instance().timestampOnly_) {
                 ts.read(&tm);
             }
@@ -828,7 +829,7 @@ namespace Action
             std::string newPath = path;
             if (Params::instance().timestampOnly_) {
                 if (Params::instance().verbose_) {
-                    std::cout << _("Updating timestamp to") << " " << v << std::endl;
+                    std::cout << _("Updating timestamp to") << " " << dateTime << std::endl;
                 }
             } else {
                 rc = renameFile(newPath, &tm);
