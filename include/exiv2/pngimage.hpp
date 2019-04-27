c2b2738ca9825b5b02075d86c27ff391861ed35c
Luis Díaz Más <piponazo@gmail.com>
2019-04-28 00:03:37 +0200

    Update doxygen style

--- include/exiv2/pngimage.hpp
+++ include/exiv2/pngimage.hpp
@@ -44,29 +44,20 @@ namespace Exiv2
 // *****************************************************************************
 // class definitions
 
-    /*!
-      @brief Class to access PNG images. Exif and IPTC metadata are supported
-          directly.
-     */
+    /// @brief Class to access PNG images. Exif and IPTC metadata are supported directly.
     class EXIV2API PngImage : public Image {
     public:
         //! @name Creators
         //@{
-        /*!
-          @brief Constructor that can either open an existing PNG image or create
-              a new image from scratch. If a new image is to be created, any
-              existing data is overwritten. Since the constructor can not return
-              a result, callers should check the good() method after object
-              construction to determine success or failure.
-          @param io An auto-pointer that owns a BasicIo instance used for
-              reading and writing image metadata. \b Important: The constructor
-              takes ownership of the passed in BasicIo instance through the
-              auto-pointer. Callers should not continue to use the BasicIo
-              instance after it is passed to this method.  Use the Image::io()
-              method to get a temporary reference.
-          @param create Specifies if an existing image should be read (false)
-              or if a new file should be created (true).
-         */
+        /// @brief Constructor that can either open an existing PNG image or create a new image from scratch.
+        ///
+        /// If a new image is to be created, any existing data is overwritten. Since the constructor can not return a
+        /// result, callers should check the good() method after object construction to determine success or failure.
+        /// @param io An auto-pointer that owns a BasicIo instance used for reading and writing image metadata.
+        /// \b Important: The constructor takes ownership of the passed in BasicIo instance through the auto-pointer.
+        /// Callers should not continue to use the BasicIo instance after it is passed to this method.  Use the
+        /// Image::io() method to get a temporary reference.
+        /// @param create Indicates if a new file will be created, or if we are reading an existing file.
         PngImage(BasicIo::UniquePtr io, bool create);
         //@}
 
@@ -74,13 +65,6 @@ namespace Exiv2
         //@{
         void readMetadata() override;
         void writeMetadata() override;
-
-        /*!
-          @brief Print out the structure of image file.
-          @throw Error if reading of the file fails or the image data is
-                not valid (does not look like data of the specific image type).
-          @warning This function is not thread safe and intended for exiv2 -pS for debugging.
-         */
         void printStructure(std::ostream& out, PrintStructureOption option,int depth) override;
         //@}
 
@@ -95,33 +79,22 @@ namespace Exiv2
         PngImage(const PngImage&& rhs) = delete;
 
     private:
-        /*!
-          @brief Provides the main implementation of writeMetadata() by
-                writing all buffered metadata to the provided BasicIo.
-          @throw Error on input-output errors or when the image data is not valid.
-          @param oIo BasicIo instance to write to (a temporary location).
-
-         */
+        /// @brief Provides the main implementation of writeMetadata() by writing all buffered metadata to the
+        /// provided BasicIo.
+        /// @throw Error on input-output errors or when the image data is not valid.
+        /// @param oIo BasicIo instance to write to (a temporary location).
         void doWriteMetadata(BasicIo& oIo);
         //@}
 
         std::string profileName_;
-
-    }; // class PngImage
-
-// *****************************************************************************
-// template, inline and free functions
+    };
 
     // These could be static private functions on Image subclasses but then
     // ImageFactory needs to be made a friend.
-    /*!
-      @brief Create a new PngImage instance and return an auto-pointer to it.
-             Caller owns the returned object and the auto-pointer ensures that
-             it will be deleted.
-     */
+    /// @brief Create a new PngImage instance and return an auto-pointer to it.
     EXIV2API Image::UniquePtr newPngInstance(BasicIo::UniquePtr io, bool create);
 
-    //! Check if the file iIo is a PNG image.
+    /// @brief Check if the file iIo is a PNG image.
     EXIV2API bool isPngType(BasicIo& iIo, bool advance);
 
 }                                       // namespace Exiv2
