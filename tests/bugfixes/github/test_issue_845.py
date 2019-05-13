d3e69f6d2c60bd06bf1c0564b919989ecfc89ec1
Kevin Backhouse <kev@semmle.com>
2019-05-13 14:57:09 +0100

    Add bounds check on allocation size.

---
+++ tests/bugfixes/github/test_issue_845.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from system_tests import CaseMeta, path
+
+
+class LargeAllocationInPngChunk(metaclass=CaseMeta):
+    """
+    Regression test for the bug described in:
+    https://github.com/Exiv2/exiv2/issues/845
+
+    An unchecked allocation size causes a std::bad_alloc to
+    be thrown.
+    """
+    url = "https://github.com/Exiv2/exiv2/issues/845"
+
+    filename = path("$data_path/issue_845_poc.png")
+    commands = ["$exiv2 $filename"]
+    stdout = [""]
+    stderr = [
+        """$exiv2_exception_message $filename:
+$kerCorruptedMetadata
+"""]
+    retval = [1]
