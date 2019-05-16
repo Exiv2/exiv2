b87dbd886753dd12fbec25e2f85986d157d0ee55
Kevin Backhouse <kev@semmle.com>
2019-05-16 15:22:46 +0100

    Throw an exception if the data location is invalid.

--- src/crwimage.cpp
+++ src/crwimage.cpp
@@ -734,12 +734,11 @@ namespace Exiv2 {
 
     DataLocId CiffComponent::dataLocation(uint16_t tag)
     {
-        DataLocId di = invalidDataLocId;
         switch (tag & 0xc000) {
-        case 0x0000: di = valueData; break;
-        case 0x4000: di = directoryData; break;
+        case 0x0000: return valueData;
+        case 0x4000: return directoryData;
+        default: throw Error(33);
         }
-        return di;
     } // CiffComponent::dataLocation
 
     /*!
