bcaad3e583af2f9a5bb05a48c03d2aebae1ba8c5
Luis Díaz Más <piponazo@gmail.com>
2019-06-30 22:39:10 +0200

    Upgrade doxygen style

--- src/utils.hpp
+++ src/utils.hpp
@@ -26,61 +26,40 @@
  */
 #pragma once
 
-// *********************************************************************
-// included header files
-
-// + standard includes
 #include <string>
 
 // *********************************************************************
 // namespace extensions
-/*!
-  @brief Contains utility classes and functions. Most of these are
-         wrappers for common C functions that do not require pointers
-         and memory considerations.
-*/
-namespace Util {
+/// @brief Contains utility classes and functions.
+/// 
+/// Most of these are wrappers for common C functions that do not require pointers and memory considerations.
+namespace Util
+{
+    // *********************************************************************
+    // free functions
 
-// *********************************************************************
-// free functions
-
-    /*!
-      @brief Get the directory component from the \em path string.
-             See %dirname(3).
-
-      This function can handle Windows paths to some extent: c:\\bar should
-      be fine, \\\\bigsrv\\foo also, but \\\\bigsrv alone doesn't work.
-     */
+    /// @brief Get the directory component from the \em path string. See %dirname(3).
+    ///
+    /// This function can handle Windows paths to some extent: c:\\bar should be fine, \\\\bigsrv\\foo also,
+    /// but \\\\bigsrv alone doesn't work.
     std::string dirname(const std::string& path);
 
-    /*!
-      @brief Get the filename component from the \em path string.
-             See %basename(3). If the \em delsuffix parameter is true,
-             the suffix will be removed.
+    /// @brief Get the filename component from the \em path string. See %basename(3).
+    ///
+    /// If the \em delsuffix parameter is true, the suffix will be removed. This function can handle Windows paths to
+    /// some extent: c:\\bar should be fine, \\\\bigsrv\\foo also, but \\\\bigsrv alone doesn't work.
+    std::string basename(const std::string& path, bool delsuffix = false);
 
-      This function can handle Windows paths to some extent: c:\\bar should
-      be fine, \\\\bigsrv\\foo also, but \\\\bigsrv alone doesn't work.
-     */
-    std::string basename(const std::string& path, bool delsuffix =false);
-
-    /*!
-      @brief Get the suffix from the path string. Normally, the suffix
-             is the substring of the basename of path from the last '.'
-             to the end of the string.
-     */
+    /// @brief Get the suffix from the path string.
+    ///
+    /// Normally, the suffix is the substring of the basename of path from the last '.' to the end of the string.
     std::string suffix(const std::string& path);
 
-    /*!
-      @brief Convert a C string to a long value, which is returned in n.
-             Returns true if the conversion is successful, else false.
-             n is not modified if the conversion is unsuccessful. See strtol(2).
-     */
+    /// @brief Convert a C string to a long value, which is returned in n. See strtol(2).
+    ///
+    /// Returns true if the conversion is successful, else false. n is not modified if the conversion is unsuccessful.
     bool strtol(const char* nptr, long& n);
 
-    /*!
-      @brief Replaces all occurrences of \em searchText in the \em text string
-             by \em replaceText.
-     */
+    /// @brief Replaces all occurrences of \em searchText in the \em text string by \em replaceText.
     void replace(std::string& text, const std::string& searchText, const std::string& replaceText);
-
-}                                       // namespace Util
+}  // namespace Util
