be875ce50f6a205179f35a89d79288832647d94f
Kevin Backhouse <kev@semmle.com>
2019-07-06 16:09:18 +0100

    Add a second test for better code coverage.

--- tests/bugfixes/github/test_issue_943.py
+++ tests/bugfixes/github/test_issue_943.py
@@ -3,19 +3,23 @@
 from system_tests import CaseMeta, path
 
 
-class OutOfBoundsReadInIptcParserDecode(metaclass=CaseMeta):
+class MrmImageLargeAllocation(metaclass=CaseMeta):
     """
     Regression test for the bug described in:
     https://github.com/Exiv2/exiv2/pull/943
     """
     url = "https://github.com/Exiv2/exiv2/pull/943"
 
-    filename = path("$data_path/issue_943_poc.mrm")
-    commands = ["$exiv2 $filename"]
-    stdout = [""]
+    filename1 = path("$data_path/issue_943_poc1.mrm")
+    filename2 = path("$data_path/issue_943_poc2.mrm")
+    commands = ["$exiv2 $filename1", "$exiv2 $filename2"]
+    stdout = ["",""]
     stderr = [
-        """Exiv2 exception in print action for file $filename:
+"""Exiv2 exception in print action for file $filename1:
+Failed to read image data
+""",
+"""Exiv2 exception in print action for file $filename2:
 Failed to read image data
 """
 ]
-    retval = [1]
+    retval = [1,1]
