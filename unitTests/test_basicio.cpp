8060a8f8e519ed99f7de62f9ddf61d882fda20f5
Kevin Backhouse <kev@semmle.com>
2019-07-11 09:45:04 +0100

    Split test into 4 parts. (CVE-2019-13504)

--- unitTests/test_basicio.cpp
+++ unitTests/test_basicio.cpp
@@ -7,7 +7,7 @@
 
 using namespace Exiv2;
 
-TEST(MemIo, seek_out_of_bounds)
+TEST(MemIo, seek_out_of_bounds_00)
 {
     byte buf[1024];
     memset(buf, 0, sizeof(buf));
@@ -24,10 +24,17 @@ TEST(MemIo, seek_out_of_bounds)
 
     // The seek was invalid, so the offset didn't change and this read still works.
     ASSERT_EQ(io.read(tmp, sizeof(tmp)), sizeof(tmp));
+}
 
-    // Reset the file.
-    ASSERT_EQ(io.seek(0, BasicIo::beg), 0);
+TEST(MemIo, seek_out_of_bounds_01)
+{
+    byte buf[1024];
+    memset(buf, 0, sizeof(buf));
+
+    MemIo io(buf, sizeof(buf));
     ASSERT_FALSE(io.eof());
+
+    byte tmp[16];
 
     // Seek to the end of the file.
     ASSERT_EQ(io.seek(0, BasicIo::end), 0);
@@ -37,20 +44,34 @@ TEST(MemIo, seek_out_of_bounds)
     ASSERT_EQ(io.seek(0x10000000, BasicIo::end), 1);
     ASSERT_TRUE(io.eof());
     ASSERT_EQ(io.read(tmp, sizeof(tmp)), 0);
+}
 
-    // Reset the file.
-    ASSERT_EQ(io.seek(100, BasicIo::beg), 0);
+TEST(MemIo, seek_out_of_bounds_02)
+{
+    byte buf[1024];
+    memset(buf, 0, sizeof(buf));
+
+    MemIo io(buf, sizeof(buf));
     ASSERT_FALSE(io.eof());
+
+    byte tmp[16];
 
     // Try to seek past the end of the file.
     ASSERT_EQ(io.seek(0x10000000, BasicIo::cur), 1);
     ASSERT_TRUE(io.eof());
     // The seek was invalid, so the offset didn't change and this read still works.
     ASSERT_EQ(io.read(tmp, sizeof(tmp)), sizeof(tmp));
+}
 
-    // Reset the file.
-    ASSERT_EQ(io.seek(100, BasicIo::beg), 0);
+TEST(MemIo, seek_out_of_bounds_03)
+{
+    byte buf[1024];
+    memset(buf, 0, sizeof(buf));
+
+    MemIo io(buf, sizeof(buf));
     ASSERT_FALSE(io.eof());
+
+    byte tmp[16];
 
     // Try to seek past the beginning of the file.
     ASSERT_EQ(io.seek(-0x10000000, BasicIo::cur), 1);
