3a0d42f7c7c24deb3012feba90383a2a6c00f765
Luis Díaz Más <piponazo@gmail.com>
2019-07-15 20:04:11 +0200

    Add regression test for #960
    (cherry picked from commit a048325e18d5328fc00b80cd2f0bf70a8f35ad9b)

---
+++ tests/bugfixes/github/test_issue_960.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from system_tests import CaseMeta, path
+
+
+class WebPImageGetHeaderOffset(metaclass=CaseMeta):
+    """
+    Regression test for the bug described in:
+    https://github.com/Exiv2/exiv2/pull/960
+    """
+    url = "https://github.com/Exiv2/exiv2/pull/960"
+
+    filename1 = path("$data_path/issue_960.poc.webp")
+    commands = ["$exiv2 $filename1"]
+    stdout = [""]
+    stderr = [
+"""Warning: Failed to decode Exif metadata.
+Exiv2 exception in print action for file $filename1:
+$kerCorruptedMetadata
+"""
+]
+    retval = [1]
