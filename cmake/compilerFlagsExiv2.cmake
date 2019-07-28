fbf95bbaaecd00e35e8a88a4a003e19f419c9084
Luis Díaz Más <piponazo@gmail.com>
2019-07-28 17:06:06 +0200

    cmake: use add_compile_option & add new security flags
    - Replacement of CMAKE_CXX_FLAGS by add_compile_option where it was
      possible
    - Addition of new compiler flags which are interesting in terms of
      software security
    - Do not mix coverage flags with -O0 and -g

--- cmake/compilerFlagsExiv2.cmake
+++ cmake/compilerFlagsExiv2.cmake
@@ -1,6 +1,6 @@
 # These flags only applies to exiv2lib, and the applications, but not to the xmp code
 
-if (MINGW OR UNIX) # MINGW, Linux, APPLE, CYGWIN
+if (COMPILER_IS_GCC OR COMPILER_IS_CLANG) # MINGW, Linux, APPLE, CYGWIN
     if ( EXIV2_TEAM_WARNINGS_AS_ERRORS )
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror")
     endif ()
@@ -8,7 +8,7 @@ endif()
 
 if (MSVC)
     if ( EXIV2_TEAM_WARNINGS_AS_ERRORS )
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /WX")
+        add_compile_options(/WX)
         set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /WX")
         set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} /WX")
         set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} /WX")
