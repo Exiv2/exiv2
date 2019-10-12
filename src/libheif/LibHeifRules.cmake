9308c288bf7ce458326cbf9747043c2c73533e5f
Gilles Caulier <caulier.gilles@gmail.com>
2019-10-12 16:12:17 +0200

    polish

--- src/libheif/LibHeifRules.cmake
+++ src/libheif/LibHeifRules.cmake
@@ -4,13 +4,8 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-include(CheckFunctionExists)
 include(CheckIncludeFile)
 
-CHECK_FUNCTION_EXISTS(posix_memalign HAVE_POSIX_MEMALIGN)
-CHECK_INCLUDE_FILE(malloc.h HAVE_MALLOC_H)
-CHECK_INCLUDE_FILE(stdint.h HAVE_STDINT_H)
-CHECK_INCLUDE_FILE(stdbool.h HAVE_STDBOOL_H)
 CHECK_INCLUDE_FILE(inttypes.h HAVE_INTTYPES_H)
 CHECK_INCLUDE_FILE(stddef.h HAVE_STDDEF_H)
 CHECK_INCLUDE_FILE(strings.h HAVE_STRINGS_H)
@@ -30,22 +25,6 @@ endif()
 
 if(HAVE_UNISTD_H)
     add_definitions(-DHAVE_UNISTD_H)
-endif()
-
-if(HAVE_MALLOC_H)
-    add_definitions(-DHAVE_MALLOC_H)
-endif()
-
-if(HAVE_STDINT_H)
-    add_definitions(-DHAVE_STDINT_H)
-endif()
-
-if(HAVE_STDBOOL_H)
-    add_definitions(-DHAVE_STDBOOL_H)
-endif()
-
-if(HAVE_POSIX_MEMALIGN)
-    add_definitions(-DHAVE_POSIX_MEMALIGN)
 endif()
 
 set(NUMERIC_VERSION 0x01050100)
