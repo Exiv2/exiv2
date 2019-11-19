56ea0881e24294e6d6020a208512e7f38e0d625f
Luis Diaz Mas <piponazo@gmail.com>
2019-11-19 15:40:57 +0100

    More tests for FileIo

--- unitTests/test_FileIo.cpp
+++ unitTests/test_FileIo.cpp
@@ -2,6 +2,8 @@
 
 #include <gtest/gtest.h>
 
+#include <array>
+
 using namespace Exiv2;
 
 namespace
@@ -34,4 +36,48 @@ TEST_F(AClosedFileIo, sizeReturnsImageSize)
 TEST_F(AClosedFileIo, eofReturnsFalse)
 {
     ASSERT_FALSE(file.eof());
+}
+
+TEST_F(AClosedFileIo, pathReturnsInputPath)
+{
+    ASSERT_EQ(jpegPath, file.path());
+}
+
+TEST_F(AClosedFileIo, canBeClosed)
+{
+    ASSERT_EQ(0, file.close());
+}
+
+TEST_F(AClosedFileIo, writeWithRawDataFails)
+{
+    const std::array<byte, 4> buff{1, 2, 3, 4};
+    ASSERT_EQ(0, file.write(buff.data(), buff.size()));
+}
+
+TEST_F(AClosedFileIo, writeWithOtherBasicIoClosedFails)
+{
+    FileIo file2{jpegPath};
+    ASSERT_EQ(0, file.write(file2));
+}
+
+TEST_F(AClosedFileIo, putbFails)
+{
+    ASSERT_EQ(EOF, file.putb(5));
+}
+
+TEST_F(AClosedFileIo, readFails)
+{
+    auto buff = file.read(5);
+    ASSERT_EQ(0, buff.size_);
+}
+
+TEST_F(AClosedFileIo, readIntoArrayFails)
+{
+    std::array<byte, 4> buff;
+    ASSERT_EQ(0, file.read(buff.data(), buff.size()));
+}
+
+TEST_F(AClosedFileIo, getbFails)
+{
+    ASSERT_EQ(EOF, file.getb());
 }
