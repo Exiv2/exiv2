0e9beff86e22e0c2d84095a6c29ded865a2eb126
Luis Díaz Más <piponazo@gmail.com>
2019-11-20 19:36:44 +0100

    Reshape existing tests for MemIo

--- unitTests/test_MemIo.cpp
+++ unitTests/test_MemIo.cpp
@@ -1,77 +1,60 @@
-#include <exiv2/basicio.hpp>
+#include <exiv2/basicio.hpp> // SUT
+
 #include <gtest/gtest.h>
+#include <array>
 
 using namespace Exiv2;
 
-TEST(MemIo, seek_out_of_bounds_00)
+// Regression tests for bug reported in https://github.com/Exiv2/exiv2/pull/944
+struct MemIoOutOfBounds : public testing::Test
 {
-    byte buf[1024];
-    memset(buf, 0, sizeof(buf));
+    void SetUp() override
+    {
+        ASSERT_FALSE(io.eof());
+    }
 
-    MemIo io(buf, sizeof(buf));
-    ASSERT_FALSE(io.eof());
+    std::array<byte, 1024> buf;
+    std::array<byte, 16> tmp;
+    MemIo io{buf.data(), buf.size()};
+};
 
-    // Regression test for bug reported in https://github.com/Exiv2/exiv2/pull/945
-    // The problem is that MemIo::seek() does not check that the new offset is
-    // in bounds.
-    byte tmp[16];
+TEST_F(MemIoOutOfBounds, seek_out_of_bounds_00)
+{
+    // The problem is that MemIo::seek() does not check that the new offset is in bounds.
     ASSERT_EQ(io.seek(0x10000000, BasicIo::beg), 1);
     ASSERT_TRUE(io.eof());
 
     // The seek was invalid, so the offset didn't change and this read still works.
-    ASSERT_EQ(io.read(tmp, sizeof(tmp)), sizeof(tmp));
+    ASSERT_EQ(io.read(tmp.data(), tmp.size()), tmp.size());
 }
 
-TEST(MemIo, seek_out_of_bounds_01)
+TEST_F(MemIoOutOfBounds, seek_out_of_bounds_01)
 {
-    byte buf[1024];
-    memset(buf, 0, sizeof(buf));
-
-    MemIo io(buf, sizeof(buf));
-    ASSERT_FALSE(io.eof());
-
-    byte tmp[16];
-
     // Seek to the end of the file.
     ASSERT_EQ(io.seek(0, BasicIo::end), 0);
-    ASSERT_EQ(io.read(tmp, sizeof(tmp)), 0);
+    ASSERT_FALSE(io.eof());
+    ASSERT_EQ(io.read(tmp.data(), tmp.size()), 0);
 
     // Try to seek past the end of the file.
     ASSERT_EQ(io.seek(0x10000000, BasicIo::end), 1);
     ASSERT_TRUE(io.eof());
-    ASSERT_EQ(io.read(tmp, sizeof(tmp)), 0);
+    ASSERT_EQ(io.read(tmp.data(), tmp.size()), 0);
 }
 
-TEST(MemIo, seek_out_of_bounds_02)
+TEST_F(MemIoOutOfBounds, seek_out_of_bounds_02)
 {
-    byte buf[1024];
-    memset(buf, 0, sizeof(buf));
-
-    MemIo io(buf, sizeof(buf));
-    ASSERT_FALSE(io.eof());
-
-    byte tmp[16];
-
     // Try to seek past the end of the file.
     ASSERT_EQ(io.seek(0x10000000, BasicIo::cur), 1);
     ASSERT_TRUE(io.eof());
     // The seek was invalid, so the offset didn't change and this read still works.
-    ASSERT_EQ(io.read(tmp, sizeof(tmp)), sizeof(tmp));
+    ASSERT_EQ(io.read(tmp.data(), tmp.size()), tmp.size());
 }
 
-TEST(MemIo, seek_out_of_bounds_03)
+TEST_F(MemIoOutOfBounds, seek_out_of_bounds_03)
 {
-    byte buf[1024];
-    memset(buf, 0, sizeof(buf));
-
-    MemIo io(buf, sizeof(buf));
-    ASSERT_FALSE(io.eof());
-
-    byte tmp[16];
-
     // Try to seek past the beginning of the file.
     ASSERT_EQ(io.seek(-0x10000000, BasicIo::cur), 1);
     ASSERT_FALSE(io.eof());
     // The seek was invalid, so the offset didn't change and this read still works.
-    ASSERT_EQ(io.read(tmp, sizeof(tmp)), sizeof(tmp));
+    ASSERT_EQ(io.read(tmp.data(), tmp.size()), tmp.size());
 }
