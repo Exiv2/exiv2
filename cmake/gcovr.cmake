d60e6f4e3ab526d066cf33ff09b7442ef639a029
emye <emye@umich.edu>
2019-12-08 19:06:19 -0500

    Compiles and runs

---
+++ cmake/gcovr.cmake
@@ -0,0 +1,16 @@
+if(USE_GCOV)
+ 
+    SET(GCOVR gcovr)
+ 
+    SET(GCC_COVERAGE_COMPILE_FLAGS "-fprofile-arcs -ftest-coverage")
+    SET(GCC_COVERAGE_LINK_FLAGS "-lgcov")
+    SET( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${GCC_COVERAGE_COMPILE_FLAGS}" )
+    SET( CMAKE_EXE_LINKER_FLAGS "${CMAKE_LINKER_FLAGS} ${GCC_COVERAGE_LINK_FLAGS}" )               
+ 
+    add_custom_command(OUTPUT _run_gcovr_parser
+    POST_BUILD
+    COMMAND ${GCOVR} -r ${CMAKE_SOURCE_DIR}/src --object-dir=${CMAKE_BINARY_DIR} --html --html-details -o output.html
+    #COMMAND ${GCOVR} -r ${CMAKE_SOURCE_DIR}/src --object-dir=${CMAKE_BINARY_DIR} --branches --exclude-unreachable-branches -e test_*
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+    add_custom_target (coverage DEPENDS _run_gcovr_parser)
+endif(USE_GCOV)
