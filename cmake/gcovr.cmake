969e6dbc7cb7dc8e767ef20f255f5e2dd2139e2b
emye <emye@umich.edu>
2019-12-10 19:18:53 -0500

    CI test fixes

--- cmake/gcovr.cmake
+++ cmake/gcovr.cmake
@@ -1,15 +1,15 @@
 if(BUILD_WITH_COVERAGE)
 
     find_program (GCOVR gcovr)
-    if(NOT GCOVR)
-        message(FATAL_ERROR "gcovr not found")
+
+    if(GCOVR)
+        file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/coverage_output )
+        add_custom_command(OUTPUT _run_gcovr_parser
+            POST_BUILD
+            COMMAND ${GCOVR} --root ${CMAKE_SOURCE_DIR} --object-dir=${CMAKE_BINARY_DIR} --html --html-details -o coverage_output/coverage.html
+            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+        )
+        add_custom_target (coverage DEPENDS _run_gcovr_parser)
     endif()
              
-    file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/coverage_output )
-    add_custom_command(OUTPUT _run_gcovr_parser
-        POST_BUILD
-        COMMAND ${GCOVR} --root ${CMAKE_SOURCE_DIR} --object-dir=${CMAKE_BINARY_DIR} --html --html-details -o coverage_output/coverage.html
-        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-    )
-    add_custom_target (coverage DEPENDS _run_gcovr_parser)
 endif(BUILD_WITH_COVERAGE)
