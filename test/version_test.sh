bf7796b1d20c42a9239f16e983118298689cebd4
clanmills <robin@clanmills.com>
2020-04-19 17:44:58 +0100

    Revert "enhanced_test4_0.27"
    This reverts commit 19ee0f6ef8e5ab5f7f620a3a3b582b7d355b48f4.

--- test/version_test.sh
+++ test/version_test.sh
@@ -4,13 +4,8 @@
 source ./functions.source
 
 (   cd "$testdir"
-    # Curiously the pipe into grep causes FreeBSD to core dump!
-    if [ $(uname) != "FreeBSD" ]; then
-								  runTest exiv2 --verbose --version | grep -v ^xmlns
-		echo xmlns entry count: $(runTest exiv2 --verbose --version | grep    ^xmlns | wc -l)
-    else
-        runTest exiv2 --verbose --version     
-    fi
+                              runTest exiv2 --verbose --version | grep -v -e ^xmlns
+    echo xmlns entry count: $(runTest exiv2 --verbose --version | grep    -e ^xmlns | wc -l)
 )
 
 # That's all Folks!
