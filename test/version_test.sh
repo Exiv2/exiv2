19ee0f6ef8e5ab5f7f620a3a3b582b7d355b48f4
clanmills <robin@clanmills.com>
2020-04-19 17:35:10 +0100

    enhanced_test4_0.27 a) new_tests => python_tests b) FreeBSD grep/pipe issue in version_test c) report summary of bash_tests

--- test/version_test.sh
+++ test/version_test.sh
@@ -4,8 +4,13 @@
 source ./functions.source
 
 (   cd "$testdir"
-                              runTest exiv2 --verbose --version | grep -v -e ^xmlns
-    echo xmlns entry count: $(runTest exiv2 --verbose --version | grep    -e ^xmlns | wc -l)
+    # Curiously the pipe into grep causes FreeBSD to core dump!
+    if [ $(uname) != "FreeBSD" ]; then
+								  runTest exiv2 --verbose --version | grep -v ^xmlns
+		echo xmlns entry count: $(runTest exiv2 --verbose --version | grep    ^xmlns | wc -l)
+    else
+        runTest exiv2 --verbose --version     
+    fi
 )
 
 # That's all Folks!
