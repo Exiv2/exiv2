0d4d17bbe511176b8e57880ba78bc898345b4f16
clanmills <robin@clanmills.com>
2020-05-09 19:05:22 +0100

    Ensure $ make unit_test shows neither "Unsupported date format" nor "Unsupport time format".

--- test/unit_test.sh
+++ test/unit_test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-# Test driver for exiv2.exe --verbose --version
+# driver for $ make unit_test
 
-( source ./functions.source ; runTest unit_tests --gtest_color=no | grep -v "Warning: Unsupported date format")
+( source ./functions.source ; runTest unit_tests --gtest_color=no 2>&1 | grep -v "Warning: Unsupported .... format" )
 
 # That's all Folks!
 ##
