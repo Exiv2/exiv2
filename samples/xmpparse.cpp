e15c483e298d00e183d4cc85a0092c47f81f0145
clanmills <robin@clanmills.com>
2020-05-13 10:46:15 +0100

    Use toString() instead of value() to write to std::cout.  https://github.com/Exiv2/exiv2/issues/901#issuecomment-627872855

--- samples/xmpparse.cpp
+++ samples/xmpparse.cpp
@@ -41,7 +41,7 @@ try {
                   << std::dec << std::setw(3)
                   << std::setfill(' ') << std::right
                   << md->count() << "  "
-                  << std::dec << md->value()
+                  << std::dec << md->toString()
                   << std::endl;
     }
     Exiv2::XmpParser::terminate();
