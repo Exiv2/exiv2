83cab13ae12f68e01c52205b5136c5e20ae723b2
Robin Mills <robin@clanmills.com>
2020-05-16 08:50:42 +0100

    Fixing file test (no remote).  Replace bash variable url with exiv2_url. Simplification of iotest.sh

--- test/iotest.sh
+++ test/iotest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Test driver for image file i/o
+# Test driver for file i/o
 
 source ./functions.source
 
@@ -9,7 +9,8 @@ source ./functions.source
     test_files="table.jpg smiley2.jpg ext.dat"
     echo
     printf "file io tests"
-    for i in $test_files; do ioTest $i; done
+    copyTestFiles $test_files
+    for i in $test_files; do runTest ioTest $i s1 s2; done
 
     printf "\n---------------------------------------------------------\n"
     if [ $errors -eq 0 ]; then
@@ -19,33 +20,35 @@ source ./functions.source
     fi
 )
 
+sniff() {
+    # Format spec for stat
+    F='-c%s'
+    if [ $PLATFORM  == 'Darwin' -o $PLATFORM == 'NetBSD' -o $PLATFORM == 'FreeBSD' ]; then
+        F='-f%z'
+    fi
+    echo $(stat $F s0 s1 s2 ../data/table.jpg) $(checkSum s0) $(checkSum s1) $(checkSum s2) $(checkSum ../data/table.jpg)
+}
+
 # Test http I/O
 if [ "$EXIV2_PORT" != "None" -a "$EXIV2_HTTP" != "None" ]; then
     startHttpServer
     if [ ! -z $exiv2_httpServer ]; then
         (   cd "${testdir}" 
             >&2 printf "*** HTTP tests begin\n"
-
+            
             cd "$testdir"
             test_files="table.jpg Reagan.tiff exiv2-bug922a.jpg"
             for i in $test_files; do
-                runTest iotest s0 s1 s2                    $url/data/$i
-                for t in s0 s1 s2 $url/data/$i; do
+                runTest iotest s0 s1 s2 $exiv2_url/data/$i
+                for t in s0 s1 s2       $exiv2_url/data/$i; do
                     runTest exiv2 -g City -g DateTime $t
                 done
             done
-            # Format spec for stat
-            F='-c%s'
-            if [ $PLATFORM  == 'Darwin' -o $PLATFORM == 'NetBSD' -o $PLATFORM == 'FreeBSD' ]; then
-            F='-f%z'
-            fi
             
-            runTest iotest s0 s1 s2 $url/data/table.jpg 1
-            echo $(stat $F s0 s1 s2 ../data/table.jpg) $(checkSum s0) $(checkSum s1) $(checkSum s2) $(checkSum ../data/table.jpg)
-            runTest iotest s0 s1 s2 $url/data/table.jpg 10
-            echo $(stat $F s0 s1 s2 ../data/table.jpg) $(checkSum s0) $(checkSum s1) $(checkSum s2) $(checkSum ../data/table.jpg)
-            runTest iotest s0 s1 s2 $url/data/table.jpg 1000
-            echo $(stat $F s0 s1 s2 ../data/table.jpg) $(checkSum s0) $(checkSum s1) $(checkSum s2) $(checkSum ../data/table.jpg)
+            runTest iotest s0 s1 s2 $exiv2_url/data/table.jpg 1    ; sniff
+            runTest iotest s0 s1 s2 $exiv2_url/data/table.jpg 10   ; sniff
+            runTest iotest s0 s1 s2 $exiv2_url/data/table.jpg 1000 ; sniff
+            
             >&2 printf "*** HTTP tests end\n"
         )  | tr -d '\r' | sed 's/[ \t]+$//' > $results
         reportTest
