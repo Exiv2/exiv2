de28105895cbf1711c1a6c4f78affa2253a5adfd
clanmills <robin@clanmills.com>
2020-05-20 09:27:11 +0100

    Woops.  Use curl once in /tmp

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -58,8 +58,8 @@ case "$distro_id" in
     'opensuse'|'opensuse-tumbleweed')
         zypper --non-interactive refresh
         zypper --non-interactive install gcc-c++ clang cmake make ccache libexpat-devel zlib-devel libssh-devel curl libcurl-devel git which dos2unix libxml2-tools
-        curl -LO https://github.com/google/googletest/archive/release-1.8.0.tar.gz
         pushd /tmp
+          curl -LO https://github.com/google/googletest/archive/release-1.8.0.tar.gz
           tar xzf   release-1.8.0.tar.gz
           mkdir -p  googletest-release-1.8.0/build
           pushd     googletest-release-1.8.0/build
