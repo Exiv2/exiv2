51801bdd09dcb7672a29abac71b803c83806d665
clanmills <robin@clanmills.com>
2020-05-20 08:42:02 +0100

    1.8.0-lp151.2.3

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -57,7 +57,7 @@ case "$distro_id" in
 
     'opensuse'|'opensuse-tumbleweed')
         zypper --non-interactive refresh
-        zypper --non-interactive install gcc-c++ clang cmake make ccache libexpat-devel zlib-devel libssh-devel libcurl-devel gtest=1.8.0 which dos2unix libxml2-tools
+        zypper --non-interactive install gcc-c++ clang cmake make ccache libexpat-devel zlib-devel libssh-devel libcurl-devel gtest=1.8.0-lp151.2.3 which dos2unix libxml2-tools
         ;;
     *)
         echo "Sorry, no predefined dependencies for your distribution $distro_id exist yet"
