1ea63ccb345a4498b41cb8842622ba7ecc9fd484
Gilles Caulier <caulier.gilles@gmail.com>
2020-06-07 10:37:13 +0200

    fix broken compilation under ARM. tested with my Raspberry PIV and Raspbian 64bits.

--- cmake/compilerFlags.cmake
+++ cmake/compilerFlags.cmake
@@ -26,7 +26,11 @@ if ( MINGW OR UNIX OR MSYS ) # MINGW, Linux, APPLE, CYGWIN
         # This fails under Fedora - MinGW - Gcc 8.3
         if (NOT MINGW)
             if (COMPILER_IS_GCC AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 8.0)
-                add_compile_options(-fstack-clash-protection -fcf-protection)
+                if (NOT ${CMAKE_SYSTEM_PROCESSOR} MATCHES "arm")
+                    add_compile_options(-fstack-clash-protection -fcf-protection)
+                else()
+                    add_compile_options(-fstack-clash-protection)
+                endif()
             endif()
 
             if (COMPILER_IS_GCC OR (COMPILER_IS_CLANG AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 3.7 ))
