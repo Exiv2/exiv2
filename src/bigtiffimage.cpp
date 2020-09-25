bda488ae427557929858c41469a0a5c31b81041d
Rosen Penev <rosenp@gmail.com>
2020-09-24 19:47:24 -0700

    remove empty if statement
    Just return from it.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/bigtiffimage.cpp
+++ src/bigtiffimage.cpp
@@ -435,8 +435,9 @@ namespace Exiv2
                     uint64_t result = 0;
 
                     if (data.size_ == 1)
-                        {}
-                    else if (data.size_ == 2)
+                        return result;
+
+                    if (data.size_ == 2)
                         result = byteSwap2(data, 0, doSwap_);
                     else if (data.size_ == 4)
                         result = byteSwap4(data, 0, doSwap_);
