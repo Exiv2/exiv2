9eb058bd43e3db834b0fdb5a4b682bafb8907d3b
Luis Díaz Más <piponazo@gmail.com>
2020-10-17 17:21:23 +0200

    Modify strncpy0 to avoid warning

--- src/ini.cpp
+++ src/ini.cpp
@@ -79,7 +79,7 @@ static char* find_chars_or_comment(const char* s, const char* chars)
 /* Version of strncpy that ensures dest (size bytes) is null-terminated. */
 static char* strncpy0(char* dest, const char* src, size_t size)
 {
-    strncpy(dest, src, size);
+    memcpy(dest, src, size);
     dest[size - 1] = '\0';
     return dest;
 }
