dd6922e3da03e4c09a74716089e058b8f6993ce7
Robin Mills <robin@clanmills.com>
2021-02-10 20:13:55 +0000

    fix_1464_sony2010e test script

--- tests/bugfixes/github/test_issue_1464.py
+++ tests/bugfixes/github/test_issue_1464.py
@@ -3,18 +3,16 @@
 from system_tests import CaseMeta, CopyTmpFiles, path
 @CopyTmpFiles("$data_path/test_issue_1464.exv")
 
-class test_issue_1180Test(metaclass=CaseMeta):
+class test_issue_1464Test(metaclass=CaseMeta):
 
     filename  = path("$tmp_path/test_issue_1464.exv")
-    commands  = [  "$exiv2 -K Exif.Sony2010e.WB_RGBLevels       $filename"
+    commands  = [  "$exiv2 -K Exif.Sony2010e.WB_RGBLevels            $filename"
+                ,  "$exiv2 -M'set Exif.Sony2010e.WB_RGBLevels 9 1 1' $filename"
+                ,  "$exiv2 -K Exif.Sony2010e.WB_RGBLevels            $filename"
                 ]
-    stdout   =  ["Exif.Image.DateTime                          Ascii      50  2020-04-24 17:44:15 \n"
+    stdout   =  ["Exif.Sony2010e.WB_RGBLevels                  Short       3  598 256 442\n"
                 ,""
-                ,"Exif.Image.DateTime                          Ascii      50  2020-04-24 17:44:15 \n"
-                ,""
-                ,"Exif.Image.DateTime                          Ascii      20  2020:04:24 15:44:15\n"
-                ,""
-                ,"Exif.Image.DateTime                          Ascii      20  2020:04:24 15:44:15\n"
+                ,"Exif.Sony2010e.WB_RGBLevels                  Short       3  9 1 1\n"
                 ]
     stderr = [""]*len(commands)
     retval = [ 0]*len(commands)
