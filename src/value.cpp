26ba26e241e10bcac91fac8ce3f0b5a4dd7a45cd
Miloš Komarčević <miloskomarcevic@aim.com>
2021-03-10 21:44:12 +0100

    Terminate empty ASCII strings as well

--- src/value.cpp
+++ src/value.cpp
@@ -410,7 +410,7 @@ namespace Exiv2 {
     int AsciiValue::read(const std::string& buf)
     {
         value_ = buf;
-        if (value_.size() > 0 && value_[value_.size()-1] != '\0') value_ += '\0';
+        if (value_.size() == 0 || value_[value_.size()-1] != '\0') value_ += '\0';
         return 0;
     }
 
