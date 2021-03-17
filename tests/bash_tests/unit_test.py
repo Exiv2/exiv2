da52bb3b0b548bf16fd280a35b9933ebe2701c6e
Robin Mills <robin@clanmills.com>
2021-03-17 10:10:32 +0000

    Better logic and error message.

--- tests/bash_tests/unit_test.py
+++ tests/bash_tests/unit_test.py
@@ -4,8 +4,11 @@ import system_tests
 import os
 import unittest
 
-bSkip=not os.path.isfile(system_tests.unit_tests) and not os.path.isfile(system_tests.unit_tests + '.exe')
+bSkip=not (os.path.isfile(system_tests.unit_tests) or os.path.isfile(system_tests.unit_tests + '.exe'))
 if bSkip:
-    raise unittest.SkipTest('*** %s does not exist ***' % system_tests.unit_tests)
+    msg='*** %s does not exist ***' % system_tests.unit_tests
+    print(msg)
+    raise unittest.SkipTest(msg)
 
 system_tests.BT.runTest(system_tests.unit_tests,raw=True)
+print('')
