d71b258b2432fc139b4f2268d22cea42eb6a46f6
Miloš Komarčević <miloskomarcevic@aim.com>
2021-03-19 09:16:35 +0100

    Update variable references to avoid expansion warnings

--- cmake/exiv2_uninstall.cmake
+++ cmake/exiv2_uninstall.cmake
@@ -1,14 +1,14 @@
-IF(NOT EXISTS "@CMAKE_BINARY_DIR@/install_manifest.txt")
-  MESSAGE(FATAL_ERROR "Cannot find install manifest: \"@CMAKE_BINARY_DIR@/install_manifest.txt\"")
-ENDIF(NOT EXISTS "@CMAKE_BINARY_DIR@/install_manifest.txt")
+IF(NOT EXISTS "${CMAKE_BINARY_DIR}/install_manifest.txt")
+  MESSAGE(FATAL_ERROR "Cannot find install manifest: ${CMAKE_BINARY_DIR}/install_manifest.txt")
+ENDIF(NOT EXISTS "${CMAKE_BINARY_DIR}/install_manifest.txt")
 
-FILE(READ "@CMAKE_BINARY_DIR@/install_manifest.txt" files)
+FILE(READ "${CMAKE_BINARY_DIR}/install_manifest.txt" files)
 STRING(REGEX REPLACE "\n" ";" files "${files}")
 FOREACH(file ${files})
-  MESSAGE(STATUS "Uninstalling \"${file}\"")
+  MESSAGE(STATUS "Uninstalling: ${file}")
   IF(EXISTS "${file}")
     EXEC_PROGRAM(
-      "@CMAKE_COMMAND@" ARGS "-E remove \"${file}\""
+      "${CMAKE_COMMAND}" ARGS "-E remove \"${file}\""
       OUTPUT_VARIABLE rm_out
       RETURN_VALUE rm_retval
       )
