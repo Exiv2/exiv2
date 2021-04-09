48587f4c6f00a2fce9c41669531e2a9478db8658
Kevin Backhouse <kevinbackhouse@github.com>
2021-04-09 13:06:57 +0100

    Regression test for https://github.com/Exiv2/exiv2/issues/1530
    (cherry picked from commit c92ac88cb0ebe72a5a17654fe6cecf411ab1e572)
    
    # Conflicts:
    #	tests/bugfixes/github/test_issue_1530.py

--- tests/bugfixes/github/test_issue_1530.py
+++ tests/bugfixes/github/test_issue_1530.py
@@ -3,10 +3,17 @@
 from system_tests import CaseMeta, path
 
 
+<<<<<<< HEAD
 class CrwEncode0x1810IntegerOverflow(metaclass=CaseMeta):
     """
     Regression test for the bug described in:
     https://github.com/Exiv2/exiv2/issues/1530
+=======
+class MrmImageLargeAllocation(metaclass=CaseMeta):
+    """
+    Regression test for the bug described in:
+    https://github.com/Exiv2/exiv2/pull/943
+>>>>>>> c92ac88cb... Regression test for https://github.com/Exiv2/exiv2/issues/1530
     """
     url = "https://github.com/Exiv2/exiv2/issues/1530"
 
@@ -15,6 +22,10 @@ class CrwEncode0x1810IntegerOverflow(metaclass=CaseMeta):
     commands = ["$exiv2 in $filename1 $filename2"]
     stdout = [""]
     stderr = [
+<<<<<<< HEAD
 """$filename1: Could not write metadata to file: $kerCorruptedMetadata
+=======
+"""$filename1: Could not write metadata to file: corrupted image metadata
+>>>>>>> c92ac88cb... Regression test for https://github.com/Exiv2/exiv2/issues/1530
 """]
     retval = [1]
