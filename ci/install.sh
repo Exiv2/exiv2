4bca4fc06b3b6104c0f04097807de3dcdccfc1be
Robin Mills <robin@clanmills.com>
2021-04-11 14:03:33 +0100

    Give up.  Restore original script and comment off/exclude 18.04

--- ci/install.sh
+++ ci/install.sh
@@ -4,29 +4,38 @@ set -x # Prints every command
 
 # This file is only used from Travis CI, where the only Linux distro used is Ubuntu
 
-pip=pip3
+python --version
+python3 --version
+
 if [[ "$(uname -s)" == 'Linux' ]]; then
-    sudo apt  update     --yes
-    sudo apt  install    --yes cmake
-    sudo apt  install    --yes zlib1g-dev libssh-dev python3-pip libxml2-utils
-    if [ -n "$WITH_VALGRIND" ]; then
-        sudo apt install --yes valgrind
+    sudo apt-get update
+
+    sudo apt-get install cmake
+    if [[ "$(lsb_release -cs)" == 'focal' ]]; then
+        # In Ubuntu 20.04 python-pip does not exist. Furthermore we need to have the alias python for python3
+        sudo apt-get install zlib1g-dev libssh-dev python3-pip python-is-python3 libxml2-utils
+    else
+        sudo apt-get install zlib1g-dev libssh-dev python-pip libxml2-utils
     fi
-    sudo apt  autoremove --yes
+
     if [ -n "$WITH_VALGRIND" ]; then
         sudo apt-get install valgrind
     fi
-    pip=pip
+    sudo pip install virtualenv
+    virtualenv conan
+    source conan/bin/activate
+    pip install conan==1.30.2
+    pip install codecov
+    pip install lxml
+else
+    sudo pip3 install virtualenv
+    virtualenv conan
+    source conan/bin/activate
+    pip3 install conan==1.30.2
+    pip3 install codecov
+    pip3 install lxml
 fi
 
-sudo $pip install virtualenv
-virtualenv conan
-source conan/bin/activate
-$pip3 install conan==1.30.2
-$pip3 install codecov
-$pip3 install lxml
-
-python3 --version
 conan --version
 conan config set storage.path=~/conanData
 conan profile new default --detect
@@ -34,3 +43,4 @@ conan profile new default --detect
 if [[ "$(uname -s)" == 'Linux' ]]; then
     conan profile update settings.compiler.libcxx=libstdc++11 default
 fi
+
