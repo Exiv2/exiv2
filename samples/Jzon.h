428e3f3bf0456fd17c118e934befbbce9d18758c
Rosen Penev <rosenp@gmail.com>
2021-04-26 18:37:04 -0700

    clang-tidy: use = delete
    Found with modernize-use-equals-delete
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- samples/Jzon.h
+++ samples/Jzon.h
@@ -393,6 +393,8 @@ namespace Jzon
 		// Return result from last call to Write()
 		const std::string &GetResult() const;
 
+		// Disable assignment operator
+		Writer &operator=(const Writer&) = delete;
 	private:
 		void writeNode(const Node &node, unsigned int level);
 		void writeObject(const Object &node, unsigned int level);
@@ -404,9 +406,6 @@ namespace Jzon
 		class FormatInterpreter *fi;
 
 		const Node &root;
-
-		// Disable assignment operator
-		Writer &operator=(const Writer&);
 	};
 
 	class JzonAPI Parser
@@ -421,6 +420,8 @@ namespace Jzon
 
 		const std::string &GetError() const;
 
+		// Disable assignment operator
+		Parser &operator=(const Parser&) = delete;
 	private:
 		enum Token
 		{
@@ -455,9 +456,6 @@ namespace Jzon
 		Node &root;
 
 		std::string error;
-
-		// Disable assignment operator
-		Parser &operator=(const Parser&);
 	};
 }
 
