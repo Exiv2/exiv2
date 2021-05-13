3416b631c554ec75d6f48496606cda73d2500cee
Miloš Komarčević <4973094+kmilos@users.noreply.github.com>
2021-05-13 16:45:41 +0200

    Add Sony ARW compression to dict

--- src/tags_int.cpp
+++ src/tags_int.cpp
@@ -251,6 +251,7 @@ namespace Exiv2 {
         {     9, N_("JBIG B&W")                 },
         {    10, N_("JBIG Color")               },
         { 32766, N_("Next 2-bits RLE")          },
+        { 32767, N_("Sony ARW Compressed")      },
         { 32769, N_("Epson ERF Compressed")     },
         { 32770, N_("Samsung SRW Compressed")   },
         { 32771, N_("CCITT RLE 1-word")         },
