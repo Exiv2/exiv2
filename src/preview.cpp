d6aa4542e2e6ee11845a5274327bd706d819535e
Rosen Penev <rosenp@gmail.com>
2021-05-19 23:23:59 -0700

    remove initializers
    these were moved outside the constructor body.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/preview.cpp
+++ src/preview.cpp
@@ -1033,8 +1033,6 @@ namespace Exiv2 {
     PreviewImage::PreviewImage(PreviewProperties properties, DataBuf data)
         : properties_(std::move(properties)), pData_(data.pData_), size_(data.size_)
     {
-        pData_ = data.pData_;
-        size_ = data.size_;
         std::pair<byte*, long> ret = data.release();
         UNUSED(ret);
     }
