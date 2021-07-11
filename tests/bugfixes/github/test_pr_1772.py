5d2d6037c7a89b4242b0e2221a694b77ab7b8f8b
postscript-dev <43813-postscript-dev@users.noreply.gitlab.gnome.org>
2021-07-11 16:50:24 +0100

    Add python test for SonyMisc1

---
+++ tests/bugfixes/github/test_pr_1772.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from system_tests import CaseMeta, path
+
+class SonyMisc1Test(metaclass=CaseMeta):
+
+    filename = path("$data_path/exiv2-pr906.exv")
+    commands = ["$exiv2 -pa --grep SonyMisc1 $filename"]
+
+    stdout = ["""Exif.SonyMisc1.CameraTemperature             SByte       1  26 °C
+"""
+    ]
+    stderr = [""]
+    retval = [0]
