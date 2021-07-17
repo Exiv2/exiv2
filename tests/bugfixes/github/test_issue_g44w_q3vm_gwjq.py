f3be36dfb6aac28a12cf179f4d615789bd72ad55
Kevin Backhouse <kevinbackhouse@github.com>
2021-07-17 22:06:06 +0100

    Skip unittest because -pR is disabled in release builds so the test fails.

--- tests/bugfixes/github/test_issue_g44w_q3vm_gwjq.py
+++ tests/bugfixes/github/test_issue_g44w_q3vm_gwjq.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from system_tests import CaseMeta, CopyTmpFiles, path, check_no_ASAN_UBSAN_errors
+import unittest
 
+@unittest.skip("Skipping test using option -pR (only for Debug mode)")
 class ImagePrintIFDStructureZeroCountAssert(metaclass=CaseMeta):
     """
     Regression test for the bug described in:
@@ -10,7 +12,7 @@ class ImagePrintIFDStructureZeroCountAssert(metaclass=CaseMeta):
     url = "https://github.com/Exiv2/exiv2/security/advisories/GHSA-g44w-q3vm-gwjq"
 
     filename = path("$data_path/issue_ghsa_g44w_q3vm_gwjq_poc.jpg")
-    commands = ["$exiv2 -p R $filename"]
+    commands = ["$exiv2 -pR $filename"]
     stderr = ["""invalid type in tiff structure0
 Exiv2 exception in print action for file $filename:
 $kerInvalidTypeValue
