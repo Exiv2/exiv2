32a26add37189dca6b8f85ffcdfe98899dcebee6
Kevin Backhouse <kevinbackhouse@github.com>
2021-07-30 17:35:14 +0100

    Check size before allocation to avoid out-of-memory errors.

--- tests/bugfixes/github/test_issue_ghsa_7569_phvm_vwc2.py
+++ tests/bugfixes/github/test_issue_ghsa_7569_phvm_vwc2.py
@@ -12,13 +12,10 @@ class Jp2ImageDoWriteMetadataOutOfBoundsRead(metaclass=CaseMeta):
 
     filename1 = path("$data_path/issue_ghsa_7569_phvm_vwc2_poc.jp2")
     filename2 = path("$data_path/issue_ghsa_7569_phvm_vwc2_poc.exv")
-    commands = ["$exiv2 in $filename1"]
+    commands = ["$exiv2 -q in $filename1"]
     stdout = [""]
     stderr = [
-"""Warning: Directory Thumbnail, entry 0x1000 has unknown Exif (TIFF) type 28928; setting type size 1.
-Error: Directory Thumbnail: IFD entry 1 lies outside of the data buffer.
-Warning: Directory Thumbnail, entry 0x1000 has unknown Exif (TIFF) type 28928; setting type size 1.
-Error: Offset of directory Thumbnail, entry 0x1000 is out of bounds: Offset = 0x2020506a; truncating the entry
-$filename1: Could not write metadata to file: $kerCorruptedMetadata
+"""Exiv2 exception in insert action for file $filename1:
+$kerCorruptedMetadata
 """]
     retval = [1]
