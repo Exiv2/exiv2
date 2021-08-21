411f1af067ca8e07efd6d66105dab6a6c08d5144
Kevin Backhouse <kevinbackhouse@github.com>
2021-08-21 12:42:24 +0100

    Credit to OSS-Fuzz: https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=37363 Do some basic XML validation before running the xmpsdk library to avoid bugs in xmpsdk.

--- tests/bugfixes/github/test_issue_1819.py
+++ tests/bugfixes/github/test_issue_1819.py
@@ -15,27 +15,7 @@ class EmptyStringXmpTextValueRead(metaclass=CaseMeta):
 File size       : 1088 Bytes
 MIME type       : application/rdf+xml
 Image size      : 0 x 0
-Thumbnail       : None
-Camera make     : 
-Camera model    : 
-Image timestamp : 
-File number     : 
-Exposure time   : 
-Aperture        : 
-Exposure bias   : 
-Flash           : 
-Flash bias      : 
-Focal length    : 
-Subject distance: 
-ISO speed       : 
-Exposure mode   : 
-Metering mode   : 
-Macro mode      : 
-Image quality   : 
-White balance   : 
-Copyright       : 
-Exif comment    : 
-
 """]
-    stderr = [""]
-    retval = [0]
+    stderr = ["""$filename: No Exif data found in the file
+"""]
+    retval = [253]
