ddd1c47f8ba645b945d566695e40b425efa8560e
Kevin Backhouse <kevinbackhouse@github.com>
2021-08-27 17:42:20 +0100

    Add second test file for better code coverage.

--- tests/bugfixes/github/test_issue_1881.py
+++ tests/bugfixes/github/test_issue_1881.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from system_tests import CaseMeta, CopyTmpFiles, path
-@CopyTmpFiles("$data_path/issue_1881_poc.jpg")
+@CopyTmpFiles("$data_path/issue_1881_poc.jpg", "$data_path/issue_1881_coverage.jpg")
 
 class SonyPreviewImageLargeAllocation(metaclass=CaseMeta):
     """
@@ -10,11 +10,13 @@ class SonyPreviewImageLargeAllocation(metaclass=CaseMeta):
     """
     url = "https://github.com/Exiv2/exiv2/issues/1881"
 
-    filename = path("$tmp_path/issue_1881_poc.jpg")
-    commands = ["$exiv2 -q -d I rm $filename"]
-    stdout = [""]
+    filename1 = path("$tmp_path/issue_1881_poc.jpg")
+    filename2 = path("$tmp_path/issue_1881_coverage.jpg")
+    commands = ["$exiv2 -q -d I rm $filename1", "$exiv2 -q -d I rm $filename2"]
+    stdout = ["",""]
     stderr = [
-"""Exiv2 exception in erase action for file $filename:
+"""Exiv2 exception in erase action for file $filename1:
 $kerCorruptedMetadata
-"""]
-    retval = [1]
+""",
+""]
+    retval = [1,0]
