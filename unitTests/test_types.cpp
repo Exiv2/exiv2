aec6733df98bd37ee0885b61965cba587560ae26
Kevin Backhouse <kevinbackhouse@github.com>
2021-08-29 22:20:25 +0100

    Add unit test for methods like DataBuf::read_uint32 and DataBuf::write_uint32.

--- unitTests/test_types.cpp
+++ unitTests/test_types.cpp
@@ -58,6 +58,39 @@ TEST(DataBuf, allocatesDataWithNonEmptyConstructor)
     ASSERT_EQ(5,    instance.size());
 }
 
+// Test methods like DataBuf::read_uint32 and DataBuf::write_uint32.
+TEST(DataBuf, read_write_endianess)
+{
+    DataBuf buf(4 + 1 + 2 + 4 + 8);
+    buf.clear();
+
+    // Big endian.
+    buf.write_uint8(4, 0x01);
+    buf.write_uint16(4 + 1, 0x0203, bigEndian);
+    buf.write_uint32(4 + 1 + 2, 0x04050607, bigEndian);
+    buf.write_uint64(4 + 1 + 2 + 4, 0x08090a0b0c0d0e0fULL, bigEndian);
+    static const uint8_t expected_le[4 + 1 + 2 + 4 + 8] =
+        {0, 0, 0, 0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf};
+    ASSERT_EQ(0, buf.cmpBytes(0, expected_le, buf.size()));
+    ASSERT_EQ(buf.read_uint8(4), 0x01);
+    ASSERT_EQ(buf.read_uint16(4 + 1, bigEndian), 0x0203);
+    ASSERT_EQ(buf.read_uint32(4 + 1 + 2, bigEndian), 0x04050607);
+    ASSERT_EQ(buf.read_uint64(4 + 1 + 2 + 4, bigEndian), 0x08090a0b0c0d0e0fULL);
+
+    // Little endian.
+    buf.write_uint8(4, 0x01);
+    buf.write_uint16(4 + 1, 0x0203, littleEndian);
+    buf.write_uint32(4 + 1 + 2, 0x04050607, littleEndian);
+    buf.write_uint64(4 + 1 + 2 + 4, 0x08090a0b0c0d0e0fULL, littleEndian);
+    static const uint8_t expected_be[4 + 1 + 2 + 4 + 8] =
+        {0, 0, 0, 0, 0x1, 0x3, 0x2, 0x7, 0x6, 0x5, 0x4, 0xf, 0xe, 0xd, 0xc, 0xb, 0xa, 0x9, 0x8};
+    ASSERT_EQ(0, buf.cmpBytes(0, expected_be, buf.size()));
+    ASSERT_EQ(buf.read_uint8(4), 0x01);
+    ASSERT_EQ(buf.read_uint16(4 + 1, littleEndian), 0x0203);
+    ASSERT_EQ(buf.read_uint32(4 + 1 + 2, littleEndian), 0x04050607);
+    ASSERT_EQ(buf.read_uint64(4 + 1 + 2 + 4, littleEndian), 0x08090a0b0c0d0e0fULL);
+}
+
 TEST(Rational, floatToRationalCast)
 {
     static const float floats[] = {0.5F, 0.015F, 0.0000625F};
