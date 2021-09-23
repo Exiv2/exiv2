45fe046a9c6b175705bfcf24994793afaa30c6e1
Dmitry Fedorov <dima@dimin.net>
2021-09-23 09:16:11 -0700

    Update src/tiffimage_int.cpp
    Co-authored-by: Miloš Komarčević <4973094+kmilos@users.noreply.github.com>

--- src/tiffimage_int.cpp
+++ src/tiffimage_int.cpp
@@ -2033,7 +2033,7 @@ namespace Exiv2 {
             return false;
         }
         uint16_t t = getUShort(pData + 2, byteOrder_);
-        if (t != 444 && tag_ != t) return false; // dima: 444 is for the JPEG-XR
+        if (t != 444 && tag_ != t) return false; // 444 is for the JPEG-XR
         offset_ = getULong(pData + 4, byteOrder_);
 
         return true;
