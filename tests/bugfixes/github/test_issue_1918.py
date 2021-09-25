eb85d4c26794d1f50e5d6945440c2b75ee4561fa
Kevin Backhouse <kevinbackhouse@github.com>
2021-09-25 18:09:41 +0100

    Add third test to improve code coverage.

--- tests/bugfixes/github/test_issue_1918.py
+++ tests/bugfixes/github/test_issue_1918.py
@@ -11,8 +11,9 @@ class PrintXmpDateOutOfBoundsIndex19(metaclass=CaseMeta):
 
     filename1 = path("$data_path/issue_1918_poc1.jpg")
     filename2 = path("$data_path/issue_1918_poc2.jpg")
-    commands = ["$exiv2 -px $filename1", "$exiv2 -px $filename2"]
-    stderr = ["", ""]
-    retval = [0, 0]
+    filename3 = path("$data_path/issue_1918_poc3.jpg")
+    commands = ["$exiv2 -px $filename1", "$exiv2 -px $filename2", "$exiv2 -px $filename3"]
+    stderr = ["", "", ""]
+    retval = [0, 0, 0]
 
     compare_stdout = check_no_ASAN_UBSAN_errors
