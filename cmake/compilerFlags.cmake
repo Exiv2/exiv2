4480ffbc8b6f809aad43d6b8f6a3c591fab4052a
Kevin Backhouse <kevinbackhouse@github.com>
2021-10-14 10:40:21 +0100

    Revert "Fix for https://github.com/Exiv2/exiv2/issues/1856"
    This reverts commit e6673465823a053eb1bf9dcb19ee93b56a704c79.
    
    (cherry picked from commit d2bdf6868f0d491c61b56ede4c45e82b5f4de990)

--- cmake/compilerFlags.cmake
+++ cmake/compilerFlags.cmake
@@ -23,12 +23,10 @@ if ( MINGW OR UNIX OR MSYS ) # MINGW, Linux, APPLE, CYGWIN
 
 
     if (COMPILER_IS_GCC OR COMPILER_IS_CLANG)
-        # This fails under Fedora - MinGW - Gcc 8.3 and macOS/M1
+        # This fails under Fedora - MinGW - Gcc 8.3
         if (NOT (MINGW OR CYGWIN OR CMAKE_HOST_SOLARIS))
-            # macOS M1 will set ARCHITECTURE == arm64
-            EXECUTE_PROCESS( COMMAND uname -m COMMAND tr -d '\n' OUTPUT_VARIABLE ARCHITECTURE )
-            if ( NOT ${ARCHITECTURE} STREQUAL arm64 )
-                check_cxx_compiler_flag(-fstack-clash-protection HAS_FSTACK_CLASH_PROTECTION)
+            if (NOT APPLE) # Don't know why this isn't working correctly on Apple with M1 processor
+            check_cxx_compiler_flag(-fstack-clash-protection HAS_FSTACK_CLASH_PROTECTION)
             endif()
             check_cxx_compiler_flag(-fcf-protection HAS_FCF_PROTECTION)
             check_cxx_compiler_flag(-fstack-protector-strong HAS_FSTACK_PROTECTOR_STRONG)
