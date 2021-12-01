2e2dd887e30c1ef6897a68be392840decf4891cb
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2021-12-01 10:17:25 +0000

    Use \" instead of ' for exiv2 command string
    The original failed on some Windows test runs.

--- tests/bugfixes/github/test_pr_1999.py
+++ tests/bugfixes/github/test_pr_1999.py
@@ -10,16 +10,16 @@ class TestXmpDateTimeSetting(metaclass=CaseMeta):
 
     infile = path("$data_path/issue_1998.xmp")
     commands = [
-        "$exiv2 -M'set Xmp.xmp.CreateDate XmpText 2021-02-03T12:00:00+01:00' $infile",
+        "$exiv2 -M\"set Xmp.xmp.CreateDate XmpText 2021-02-03T12:00:00+01:00\" $infile",
         "$exiv2 -K Xmp.xmp.CreateDate $infile",
-        "$exiv2 -M'set Xmp.xmp.CreateDate XmpText 2021-02-03T12:34:56+02:00' $infile",
+        "$exiv2 -M\"set Xmp.xmp.CreateDate XmpText 2021-02-03T12:34:56+02:00\" $infile",
         "$exiv2 -K Xmp.xmp.CreateDate $infile",
         ]
     stdout = [
-        """""",
+        "",
         """Xmp.xmp.CreateDate                           XmpText    25  2021-02-03T12:00:00+01:00
 """,
-        """""",
+        "",
         """Xmp.xmp.CreateDate                           XmpText    25  2021-02-03T12:34:56+02:00
 """,
               ]
