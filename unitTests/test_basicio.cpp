098679e515c8ee3a7403f392daddcd8b41aba93a
Luis Díaz Más <piponazo@gmail.com>
2022-01-05 18:13:35 +0100

    Refactor & add tests for MemIO

--- unitTests/test_basicio.cpp
+++ unitTests/test_basicio.cpp
@@ -21,80 +21,148 @@
 #include <exiv2/basicio.hpp>
 #include <gtest/gtest.h>
 
+#include <array>
+
 using namespace Exiv2;
 
-TEST(MemIo, seek_out_of_bounds_00)
+TEST(MemIo, isNotAtEofInitially)
 {
-    byte buf[1024];
-    memset(buf, 0, sizeof(buf));
+    std::array<byte, 64> buf;
+    buf.fill(0);
 
-    MemIo io(buf, sizeof(buf));
+    MemIo io(buf.data(), buf.size());
     ASSERT_FALSE(io.eof());
-
-    // Regression test for bug reported in https://github.com/Exiv2/exiv2/pull/945
-    // The problem is that MemIo::seek() does not check that the new offset is
-    // in bounds.
-    byte tmp[16];
-    ASSERT_EQ(io.seek(0x10000000, BasicIo::beg), 1);
-    ASSERT_TRUE(io.eof());
-
-    // The seek was invalid, so the offset didn't change and this read still works.
-    const long sizeTmp = static_cast<long>(sizeof(sizeTmp));
-    ASSERT_EQ(io.read(tmp, sizeTmp), sizeTmp);
 }
 
-TEST(MemIo, seek_out_of_bounds_01)
+TEST(MemIo, seekBeyondBufferSizeReturns1AndSetsEofToTrue)
 {
-    byte buf[1024];
-    memset(buf, 0, sizeof(buf));
+    std::array<byte, 64> buf;
+    buf.fill(0);
 
-    MemIo io(buf, sizeof(buf));
-    ASSERT_FALSE(io.eof());
-
-    byte tmp[16];
-
-    // Seek to the end of the file.
-    ASSERT_EQ(io.seek(0, BasicIo::end), 0);
-    ASSERT_EQ(io.read(tmp, sizeof(tmp)), 0);
-
-    // Try to seek past the end of the file.
-    ASSERT_EQ(io.seek(0x10000000, BasicIo::end), 1);
+    MemIo io(buf.data(), buf.size());
+    ASSERT_EQ(1, io.seek(65, BasicIo::beg));
     ASSERT_TRUE(io.eof());
-    ASSERT_EQ(io.read(tmp, sizeof(tmp)), 0);
 }
 
-TEST(MemIo, seek_out_of_bounds_02)
+TEST(MemIo, seekBefore0Returns1ButItDoesNotSetEofToTrue)
 {
-    byte buf[1024];
-    memset(buf, 0, sizeof(buf));
+    std::array<byte, 64> buf;
+    buf.fill(0);
 
-    MemIo io(buf, sizeof(buf));
+    MemIo io(buf.data(), buf.size());
+    ASSERT_EQ(1, io.seek(-1, BasicIo::beg));
     ASSERT_FALSE(io.eof());
-
-    byte tmp[16];
-
-    // Try to seek past the end of the file.
-    ASSERT_EQ(io.seek(0x10000000, BasicIo::cur), 1);
-    ASSERT_TRUE(io.eof());
-    // The seek was invalid, so the offset didn't change and this read still works.
-    const long sizeTmp = static_cast<long>(sizeof(sizeTmp));
-    ASSERT_EQ(io.read(tmp, sizeTmp), sizeTmp);
 }
 
-TEST(MemIo, seek_out_of_bounds_03)
+TEST(MemIo, seekBeyondBoundsDoesNotMoveThePosition)
 {
-    byte buf[1024];
-    memset(buf, 0, sizeof(buf));
+    std::array<byte, 64> buf;
+    buf.fill(0);
 
-    MemIo io(buf, sizeof(buf));
+    MemIo io(buf.data(), buf.size());
+    ASSERT_EQ(0, io.tell());
+    ASSERT_EQ(1, io.seek(65, BasicIo::beg));
+    ASSERT_EQ(0, io.tell());
+}
+
+TEST(MemIo, seekInsideBoundsMoveThePosition)
+{
+    std::array<byte, 64> buf;
+    buf.fill(0);
+
+    MemIo io(buf.data(), buf.size());
+    ASSERT_EQ(0, io.tell());
+    ASSERT_EQ(0, io.seek(32, BasicIo::beg));
+    ASSERT_EQ(32, io.tell());
+}
+
+TEST(MemIo, seekInsideBoundsUsingBeg_resetsThePosition)
+{
+    std::array<byte, 64> buf;
+    buf.fill(0);
+
+    MemIo io(buf.data(), buf.size());
+    std::vector<std::int64_t> positions {0, 8, 16, 32, 64};
+    for(auto pos: positions) {
+      ASSERT_EQ(0, io.seek(pos, BasicIo::beg));
+      ASSERT_EQ(pos, io.tell());
+    }
+}
+
+TEST(MemIo, seekInsideBoundsUsingCur_shiftThePosition)
+{
+    std::array<byte, 64> buf;
+    buf.fill(0);
+
+    MemIo io(buf.data(), buf.size());
+    std::vector<std::int64_t> shifts {4, 4, 8, 16, 32};
+    std::vector<std::int64_t> positions {4, 8, 16, 32, 64};
+    for (size_t i = 0; i < shifts.size(); ++i) {
+      ASSERT_EQ(0, io.seek(shifts[i], BasicIo::cur));
+      ASSERT_EQ(positions[i], io.tell());
+    }
+}
+
+TEST(MemIo, seekToEndPosition_doesNotTriggerEof)
+{
+    std::array<byte, 64> buf;
+    buf.fill(0);
+
+    MemIo io(buf.data(), buf.size());
+    ASSERT_EQ(0, io.tell());
+    ASSERT_EQ(0, io.seek(0, BasicIo::end));
+    ASSERT_EQ(64, io.tell());
     ASSERT_FALSE(io.eof());
+}
 
-    byte tmp[16];
+TEST(MemIo, seekToEndPositionAndReadTriggersEof)
+{
+    std::array<byte, 64> buf;
+    buf.fill(0);
 
-    // Try to seek past the beginning of the file.
-    ASSERT_EQ(io.seek(-0x10000000, BasicIo::cur), 1);
-    ASSERT_FALSE(io.eof());
-    // The seek was invalid, so the offset didn't change and this read still works.
-    const long sizeTmp = static_cast<long>(sizeof(sizeTmp));
-    ASSERT_EQ(io.read(tmp, sizeTmp), sizeTmp);
+    MemIo io(buf.data(), buf.size());
+    ASSERT_EQ(0, io.seek(0, BasicIo::end));
+    ASSERT_EQ(64, io.tell());
+
+    std::array<byte, 64> buf2;
+    buf2.fill(0);
+    ASSERT_EQ(0, io.read(buf2.data(), 1)); // Note that we cannot even read 1 byte being at the end
+    ASSERT_TRUE(io.eof());
+}
+
+TEST(MemIo, readEmptyIoReturns0)
+{
+    std::array<byte, 10> buf;
+    MemIo io;
+    ASSERT_EQ(0, io.read(buf.data(), buf.size()));
+}
+
+TEST(MemIo, readLessBytesThanAvailableReturnsRequestedBytes)
+{
+    std::array<byte, 10> buf1, buf2;
+    buf1.fill(1);
+    buf2.fill(0);
+
+    MemIo io(buf1.data(), buf1.size());
+    ASSERT_EQ(5, io.read(buf2.data(), 5));
+}
+
+TEST(MemIo, readSameBytesThanAvailableReturnsRequetedBytes)
+{
+    std::array<byte, 10> buf1, buf2;
+    buf1.fill(1);
+    buf2.fill(0);
+
+    MemIo io(buf1.data(), buf1.size());
+    ASSERT_EQ(10, io.read(buf2.data(), 10));
+}
+
+TEST(MemIo, readMoreBytesThanAvailableReturnsAvailableBytes)
+{
+    std::array<byte, 10> buf1, buf2;
+    buf1.fill(1);
+    buf2.fill(0);
+
+    MemIo io(buf1.data(), buf1.size());
+    ASSERT_EQ(10, io.read(buf2.data(), 15));
 }
