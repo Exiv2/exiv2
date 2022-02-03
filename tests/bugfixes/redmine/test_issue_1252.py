d9c08c6d076c7c0b582548104005d9aee8b83bf6
Alex Esseling <alexander.esseling@gmail.com>
2022-02-03 22:29:29 +0100

    fix of test_issue_1151.py

--- tests/bugfixes/redmine/test_issue_1252.py
+++ tests/bugfixes/redmine/test_issue_1252.py
@@ -22,8 +22,7 @@ Exif.Canon.LensModel                         Ascii      74  120-300mm
 Exif.Photo.LensSpecification                 Rational    4  120/1 300/1 0/1 0/1
 Exif.Photo.LensModel                         Ascii      10  120-300mm
 Exif.Photo.LensSerialNumber                  Ascii      11  0000000000
-""",
-              """Exif.CanonCs.LensType                        Short       1  Sigma 150-500mm f/5-6.3 APO DG OS HSM
+Exif.CanonCs.LensType                        Short       1  Sigma 150-500mm f/5-6.3 APO DG OS HSM
 Exif.CanonCs.Lens                            Short       3  150.0 - 500.0 mm
 Exif.Canon.LensModel                         Ascii      74  150-500mm
 Exif.Photo.LensSpecification                 Rational    4  150/1 500/1 0/1 0/1
