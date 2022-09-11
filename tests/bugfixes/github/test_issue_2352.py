8016f670918954483beb32a162441c0230127956
Kevin Backhouse <kevinbackhouse@github.com>
2022-09-11 15:10:04 +0100

    Regression test for https://github.com/Exiv2/exiv2/issues/2352

---
+++ tests/bugfixes/github/test_issue_2352.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from system_tests import CaseMeta, check_no_ASAN_UBSAN_errors
+
+class issue_2352_floatToRationalCast_integer_overflow(metaclass=CaseMeta):
+    url      = "https://github.com/Exiv2/exiv2/issues/2352"
+    filename = "$data_path/issue_2352_poc.jpg"
+    commands = ["$exiv2 -q $filename"]
+    retval   = [0]
+    stderr   = [""]
+    stdout   = ["""File name       : $filename
+File size       : 20 Bytes
+MIME type       : image/tiff
+Image size      : 0 x 0
+Thumbnail       : None
+Camera make     : 
+Camera model    : 
+Image timestamp : 
+File number     : 
+Exposure time   : 
+Aperture        : 
+Exposure bias   : 
+Flash           : 
+Flash bias      : 
+Focal length    : 
+Subject distance: -214748.50 m
+ISO speed       : 
+Exposure mode   : 
+Metering mode   : 
+Macro mode      : 
+Image quality   : 
+White balance   : 
+Copyright       : 
+Exif comment    : 
+
+"""]
+
