a770a751e633ef658b4848d5091f3f39299ccbf3
norbertwg <mail@quickimagecomment.de>
2022-12-10 17:47:57 +0100

    clang format

--- src/easyaccess.cpp
+++ src/easyaccess.cpp
@@ -361,21 +361,12 @@ ExifData::const_iterator maxApertureValue(const ExifData& ed) {
 }
 
 ExifData::const_iterator subjectDistance(const ExifData& ed) {
-  static const char* keys[] = {"Exif.Photo.SubjectDistance",
-                               "Exif.Image.SubjectDistance",
-                               "Exif.CanonSi.SubjectDistance",
-                               "Exif.CanonFi.FocusDistanceUpper",
-                               "Exif.CanonFi.FocusDistanceLower",
-                               "Exif.MinoltaCsNew.FocusDistance",
-                               "Exif.Nikon1.FocusDistance",
-                               "Exif.Nikon3.FocusDistance",
-                               "Exif.NikonLd2.FocusDistance",
-                               "Exif.NikonLd3.FocusDistance",
-                               "Exif.NikonLd4.FocusDistance",
-                               "Exif.Olympus.FocusDistance",
-                               "Exif.OlympusFi.FocusDistance",
-                               "Exif.Casio.ObjectDistance",
-                               "Exif.Casio2.ObjectDistance"};
+  static const char* keys[] = {
+      "Exif.Photo.SubjectDistance",      "Exif.Image.SubjectDistance",      "Exif.CanonSi.SubjectDistance",
+      "Exif.CanonFi.FocusDistanceUpper", "Exif.CanonFi.FocusDistanceLower", "Exif.MinoltaCsNew.FocusDistance",
+      "Exif.Nikon1.FocusDistance",       "Exif.Nikon3.FocusDistance",       "Exif.NikonLd2.FocusDistance",
+      "Exif.NikonLd3.FocusDistance",     "Exif.NikonLd4.FocusDistance",     "Exif.Olympus.FocusDistance",
+      "Exif.OlympusFi.FocusDistance",    "Exif.Casio.ObjectDistance",       "Exif.Casio2.ObjectDistance"};
   return findMetadatum(ed, keys, std::size(keys));
 }
 
