7883e3d00a8b59bce1f461881eb6b6224301df87
Rosen Penev <rosenp@gmail.com>
2023-02-10 19:05:38 -0800

    clang-tidy: replace memcpy with copy_n
    Found with bugprone-not-null-terminated-result
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/jpgimage.cpp
+++ src/jpgimage.cpp
@@ -1001,7 +1001,7 @@ int ExvImage::writeHeader(BasicIo& outIo) const {
   byte tmpBuf[7];
   tmpBuf[0] = 0xff;
   tmpBuf[1] = 0x01;
-  std::memcpy(tmpBuf + 2, exiv2Id_, 5);
+  std::copy_n(exiv2Id_, 5, tmpBuf + 2);
   if (outIo.write(tmpBuf, 7) != 7)
     return 4;
   if (outIo.error())
