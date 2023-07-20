4dd9cd5869161b5c22896670809720ced03f4c81
kamiccolo <kamicc@gmail.com>
2023-07-20 17:52:05 +0300

    Switch to PROJECT_SOURCE_DIR for gcovr as well
    (cherry picked from commit 5631e8423729bb11f3cc5d7a45c70cc556ac8ba7)

--- cmake/gcovr.cmake
+++ cmake/gcovr.cmake
@@ -11,7 +11,7 @@ if(BUILD_WITH_COVERAGE)
         file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/coverage_output )
         add_custom_command(OUTPUT _run_gcovr_parser
             POST_BUILD
-            COMMAND ${GCOVR} --root ${CMAKE_SOURCE_DIR} --object-dir=${CMAKE_BINARY_DIR} --html --html-details -o coverage_output/coverage.html
+            COMMAND ${GCOVR} --root ${PROJECT_SOURCE_DIR} --object-dir=${CMAKE_BINARY_DIR} --html --html-details -o coverage_output/coverage.html
               --exclude-directories xmpsdk --exclude-directories unitTests --exclude-directories samples
               --exclude '.*xmpsdk.*' --exclude '.*unitTests.*' --exclude '.*samples.*'
               --exclude-unreachable-branches --exclude-throw-branches
