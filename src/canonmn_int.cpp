9832e682d742595cd35688c5f1fced17c11abed9
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2023-12-14 09:42:09 +0000

    Another lint checker molifying tweak

--- src/canonmn_int.cpp
+++ src/canonmn_int.cpp
@@ -1079,8 +1079,8 @@ const TagInfo* CanonMakerNote::tagListLiOp() {
 // Canon LensInfo Tag
 constexpr TagInfo CanonMakerNote::tagInfoLe_[] = {
     {0x0000, "LensSerialNumber", N_("Lens Serial Number"),
-    N_("Lens Serial Number. Convert each byte to hexadecimal to get two "
-       "digits of the lens serial number."),
+     N_("Lens Serial Number. Convert each byte to hexadecimal to get two "
+        "digits of the lens serial number."),
      IfdId::canonLeId, SectionId::makerTags, unsignedByte, -1, printLe0x0000},
     {0xffff, "(UnkownCanonLensInfoTag)", "(UnkownCanonLensInfoTag)", N_("UnkownCanonLensInfoTag"), IfdId::canonLeId,
      SectionId::makerTags, undefined, 1, printValue}  // important to add end of tag
