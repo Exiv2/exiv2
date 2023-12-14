127765186bd6f2c2b9c2f3a2a5ea36225859967a
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2023-12-14 09:57:12 +0000

    Update tests with Exif.CanonLe.LensSerialNumber
    All of them have all-zero serial numbers, so they aren't testing this
    PR directly.
    
    Signed-off-by: Jim Easterbrook <jim@jim-easterbrook.me.uk>

--- tests/bugfixes/github/test_issue_45.py
+++ tests/bugfixes/github/test_issue_45.py
@@ -13,7 +13,7 @@ class Sigma24_105mmRecognition(metaclass=system_tests.CaseMeta):
 Exif.CanonCs.Lens                            Short       3  24.0 - 105.0 mm
 Exif.CanonCf.LensAFStopButton                Short       1  0
 Exif.Canon.LensModel                         Ascii      74  24-105mm F4 DG OS HSM | Art 013
-Exif.CanonLe.LensSerialNumber                SLong       1  0
+Exif.CanonLe.LensSerialNumber                Byte        5  0000000000
 """]
     stderr = [""]
     retval = [0]
