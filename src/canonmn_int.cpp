9e81e91022acfcd892bcc6d6e67d1e4a3691f67a
Miloš Komarčević <miloskomarcevic@aim.com>
2024-01-03 12:40:33 +0100

    Add Canon DigitalLensOptimizer and DualPixelRaw tags
    (cherry picked from commit 724b7f85f41f390cb6d93ba3be40ccbd7f89e281)
    
    # Conflicts:
    #	src/canonmn_int.cpp

--- src/canonmn_int.cpp
+++ src/canonmn_int.cpp
@@ -1005,7 +1005,13 @@ constexpr TagDetails canonLongExposureNoiseReduction[] = {{0, N_("Off")}, {1, N_
 
 // Canon HighISONoiseReduction, tag 0x0005
 constexpr TagDetails canonHighISONoiseReduction[] = {
-    {0, N_("Standard")}, {1, N_("Low")}, {2, N_("Strong")}, {3, N_("off")}};
+    {0, N_("Standard")}, {1, N_("Low")}, {2, N_("Strong")}, {3, N_("Off")}};
+
+// Canon DigitalLensOptimizer, tag 0x000a
+constexpr TagDetails canonDigitalLensOptimizer[] = {{0, N_("Off")}, {1, N_("Standard")}, {2, N_("High")}};
+
+// Canon DualPixelRaw, tag 0x000b
+constexpr TagDetails canonDualPixelRaw[] = {{0, N_("Off")}, {1, N_("On")}};
 
 // Canon LightingOpt Tag
 constexpr TagInfo CanonMakerNote::tagInfoLiOp_[] = {
@@ -1019,6 +1025,10 @@ constexpr TagInfo CanonMakerNote::tagInfoLiOp_[] = {
      IfdId::canonLiOpId, SectionId::makerTags, signedLong, -1, EXV_PRINT_TAG(canonLongExposureNoiseReduction)},
     {0x0005, "HighISONoiseReduction", N_("High ISO Noise Reduction"), N_("High ISO Noise Reduction"),
      IfdId::canonLiOpId, SectionId::makerTags, signedLong, -1, EXV_PRINT_TAG(canonHighISONoiseReduction)},
+    {0x000a, "DigitalLensOptimizer", N_("Digital Lens Optimizer"), N_("Digital Lens Optimizer"), IfdId::canonLiOpId,
+     SectionId::makerTags, signedLong, -1, EXV_PRINT_TAG(canonDigitalLensOptimizer)},
+    {0x000b, "DualPixelRaw", N_("Dual Pixel Raw"), N_("Dual Pixel Raw"), IfdId::canonLiOpId, SectionId::makerTags,
+     signedLong, -1, EXV_PRINT_TAG(canonDualPixelRaw)},
     {0xffff, "(UnknownLightingOptimizationTag)", "(UnknownLightingOptimizationTag)",
      N_("UnknownLightingOptimizationTag Selection Tag"), IfdId::canonLiOpId, SectionId::makerTags, signedLong, 1,
      printValue}  // important to add end of tag
