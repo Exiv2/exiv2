9dd7e3cefe7a58a7a74bf1dbd19bb073d086d668
Miloš Komarčević <miloskomarcevic@aim.com>
2024-05-28 14:41:28 +0200

    Remove the need to enable BMFF via API

--- samples/metacopy.cpp
+++ samples/metacopy.cpp
@@ -11,9 +11,6 @@ int main(int argc, char* const argv[]) {
   try {
     Exiv2::XmpParser::initialize();
     ::atexit(Exiv2::XmpParser::terminate);
-#ifdef EXV_ENABLE_BMFF
-    Exiv2::enableBMFF();
-#endif
 
     // Handle command line arguments
     Params params;
