06bfdc87bcacfb055af6bbdb1c0eeef6bfcf36e1
Sergey Hovakimyan <sergey.hovakimyan@gmail.com>
2024-07-10 12:21:49 +0400

    adds a workaround to avoid calling DeleteNamespace since XMP-Toolkit-sdk does not like it anymore

--- src/xmp.cpp
+++ src/xmp.cpp
@@ -634,8 +634,9 @@ void XmpParser::terminate() {
 void XmpParser::registerNs(const std::string& ns, const std::string& prefix) {
   try {
     initialize();
-    AutoLock autoLock(xmpLockFct_, pLockData_);
-    SXMPMeta::DeleteNamespace(ns.c_str());
+    // apparently we can't delete namespaces anymore
+    // see WXMPMeta.cpp
+    // SXMPMeta::DeleteNamespace(ns.c_str());
 #ifdef EXV_ADOBE_XMPSDK
     SXMPMeta::RegisterNamespace(ns.c_str(), prefix.c_str(), nullptr);
 #else
