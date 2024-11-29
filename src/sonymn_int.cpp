b0c7e0f4bd50c0b10f2be90c935b25da0b96db62
Miloš Komarčević <miloskomarcevic@aim.com>
2024-11-29 13:36:17 +0100

    Add Sony models
    (cherry picked from commit 5632b1d6fc1a3fdd3747521830232ea31b886b24)
    
    # Conflicts:
    #	src/sonymn_int.cpp

--- src/sonymn_int.cpp
+++ src/sonymn_int.cpp
@@ -437,7 +437,9 @@ constexpr TagDetails sonyModelId[] = {{0, N_("Multiple camera models")},
                                       {394, "ILCE-6700"},
                                       {395, "ZV-1M2"},
                                       {396, "ILCE-7CR"},
-                                      {397, "ILCE-7CM2"}};
+                                      {397, "ILCE-7CM2"},
+                                      {398, "ILX-LR1"},
+                                      {400, "ILCE-1M2"}};
 
 //! Lookup table to translate Sony creative style (main group) values to readable labels
 constexpr StringTagDetails sonyCreativeStyleStd[] = {{"AdobeRGB", N_("Adobe RGB")},
