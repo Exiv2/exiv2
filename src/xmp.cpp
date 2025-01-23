95caaa201811190df45792cfff2c5300a415b907
Rosen Penev <rosenp@gmail.com>
2025-01-23 14:27:18 -0800

    use erase_if
    Cast isspace to unsigned char for proper C API.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/xmp.cpp
+++ src/xmp.cpp
@@ -577,7 +577,7 @@ static XMP_Status nsDumper(void* refCon, XMP_StringPtr buffer, XMP_StringLen buf
   std::string out(buffer, bufferSize);
 
   // remove blanks: http://stackoverflow.com/questions/83439/remove-spaces-from-stdstring-in-c
-  out.erase(std::remove_if(out.begin(), out.end(), isspace), out.end());
+  std::erase_if(out, [](unsigned char c) { return std::isspace(c); });
 
   bool bURI = Internal::contains(out, "http://");
   bool bNS = Internal::contains(out, ':') && !bURI;
