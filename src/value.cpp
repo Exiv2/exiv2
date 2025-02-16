906a2a7df76bcf1449eb7efe8944e8559d7c158e
Rosen Penev <rosenp@gmail.com>
2025-02-15 16:31:01 -0800

    avoid strncmp
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/value.cpp
+++ src/value.cpp
@@ -389,15 +389,15 @@ CommentValue::CharsetId CommentValue::charsetId() const {
 
 const char* CommentValue::detectCharset(std::string& c) const {
   // Interpret a BOM if there is one
-  if (0 == strncmp(c.data(), "\xef\xbb\xbf", 3)) {
+  if (c.front() == '\xef' && c[1] == '\xbb' && c[2] == '\xbf') {
     c = c.substr(3);
     return "UTF-8";
   }
-  if (0 == strncmp(c.data(), "\xff\xfe", 2)) {
+  if (c.front() == '\xff' && c[1] == '\xfe') {
     c = c.substr(2);
     return "UCS-2LE";
   }
-  if (0 == strncmp(c.data(), "\xfe\xff", 2)) {
+  if (c.front() == '\xfe' && c[1] == '\xff') {
     c = c.substr(2);
     return "UCS-2BE";
   }
