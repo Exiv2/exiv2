ceb8d9763c73813f1c7b1dd0889820dca9e5af6b
Miloš Komarčević <miloskomarcevic@aim.com>
2025-04-23 20:46:13 +0200

    Indicate no translation for Nikon lenses
    (cherry picked from commit ceee3dc3d7ced7710cc3b5afb7bcd01d82f005c2)

--- src/nikonmn_int.cpp
+++ src/nikonmn_int.cpp
@@ -3133,7 +3133,7 @@ std::ostream& Nikon3MakerNote::printLensId(std::ostream& os, const Value& value,
      */
     if (auto pf = Exiv2::find(fmountlens, vid))
       return os << pf->manuf << " " << pf->lensname;
-    return os << value;
+    return os << "(" << value << ")";
   }
 
   byte raw[] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
@@ -3148,14 +3148,14 @@ std::ostream& Nikon3MakerNote::printLensId(std::ostream& os, const Value& value,
     ExifKey key(pre + std::string(tags[i]));
     auto md = metadata->findKey(key);
     if (md == metadata->end() || md->typeId() != unsignedByte || md->count() == 0) {
-      return os << value;
+      return os << "(" << value << ")";
     }
     raw[i] = static_cast<byte>(md->toInt64());
   }
 
   auto md = metadata->findKey(ExifKey("Exif.Nikon3.LensType"));
   if (md == metadata->end() || md->typeId() != unsignedByte || md->count() == 0) {
-    return os << value;
+    return os << "(" << value << ")";
   }
   raw[7] = static_cast<byte>(md->toInt64());
 
@@ -3180,9 +3180,9 @@ std::ostream& Nikon3MakerNote::printLensId(std::ostream& os, const Value& value,
     }
   }
   // Lens not found in database
-  return os << value;
+  return os << "(" << value << ")";
 #else
-  return os << value;
+  return os << "(" << value << ")";
 #endif  // EXV_HAVE_LENSDATA
 }
 
@@ -3893,7 +3893,7 @@ std::ostream& Nikon3MakerNote::printLensId4ZMount(std::ostream& os, const Value&
   for (auto&& [l, manuf, lensname] : zmountlens)
     if (l == lid)
       return os << manuf << " " << lensname;
-  return os << lid;
+  return os << "(" << value << ")";
 }
 
 std::ostream& Nikon3MakerNote::printApertureLd4(std::ostream& os, const Value& value, const ExifData*) {
