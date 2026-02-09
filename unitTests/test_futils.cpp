9407d21b83ca5dce6fd7a18df1dfeed82c0d98b9
Kevin Backhouse <kevinbackhouse@github.com>
2026-02-09 11:24:36 +0000

    Fix test names for URL parsing and decoding

--- unitTests/test_futils.cpp
+++ unitTests/test_futils.cpp
@@ -107,7 +107,7 @@ TEST(base64decode, decodesValidString) {
   ASSERT_STREQ(expected.c_str(), result.data());
 }
 
-TEST(AUri, parsesAndDecoreUrl1) {
+TEST(AUri, parsesAndDecodeUrl1) {
   const std::string url("http://www.geekhideout.com/urlcode.shtml");
   Uri uri = Uri::Parse(url);
 
@@ -122,7 +122,7 @@ TEST(AUri, parsesAndDecoreUrl1) {
   Uri::Decode(uri);
 }
 
-TEST(AUri, parsesAndDecoreUrl2) {
+TEST(AUri, parsesAndDecodeUrl2) {
   const std::string url("http://username:password@example.com:8000/path/to/page?name=ferret&color=purple");
   Uri uri = Uri::Parse(url);
 
@@ -137,7 +137,7 @@ TEST(AUri, parsesAndDecoreUrl2) {
   Uri::Decode(uri);
 }
 
-TEST(AUri, parsesAndDecoreUrl3) {
+TEST(AUri, parsesAndDecodeUrl3) {
   const std::string url("http://host?query@");  // bogus uri
   Uri uri = Uri::Parse(url);
 
