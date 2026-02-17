52953b91cc51d00e988e855d8f4ab3f00a7a44b7
Miloš Komarčević <miloskomarcevic@aim.com>
2026-02-17 22:22:50 +0100

    Bump conan deps

--- conanfile.py
+++ conanfile.py
@@ -22,25 +22,25 @@ class Exiv2Conan(ConanFile):
     def requirements(self):
         self.requires('zlib/1.3.1')
 
-        self.requires('brotli/1.1.0')
+        self.requires('brotli/1.2.0')
 
-        self.requires('inih/58')
+        self.requires('inih/62')
 
-        self.requires('fmt/10.2.1')
+        self.requires('fmt/12.1.0')
 
         if self.options.webready:
-            self.requires('libcurl/8.10.1')
+            self.requires('libcurl/8.18.0')
 
         if os_info.is_windows and self.options.iconv:
             self.requires('libiconv/1.18')
 
         if self.options.unitTests:
-            self.requires('gtest/1.15.0')
+            self.requires('gtest/1.17.0')
 
         if self.options.xmp:
             self.requires('XmpSdk/2016.7@piponazo/stable') # from conan-piponazo
         else:
-            self.requires('expat/2.6.3')
+            self.requires('expat/2.7.4')
 
     def imports(self):
         self.copy('*.dll', dst='bin', src='bin')
