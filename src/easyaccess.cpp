ddabfb97a43ae5c1b9f33a4536a60a4d2f936e32
oxygenkun <oxygenkun.1@gmail.com>
2026-04-03 18:10:57 +0800

    feat(easyaccess): add SonyMisc3c.CameraOrientation for orientation

--- src/easyaccess.cpp
+++ src/easyaccess.cpp
@@ -65,6 +65,7 @@ ExifData::const_iterator orientation(const ExifData& ed) {
       "Exif.MinoltaCs5D.Rotation",    "Exif.MinoltaCs5D.Rotation2", "Exif.MinoltaCs7D.Rotation",
       "Exif.Sony1MltCsA100.Rotation", "Exif.Sony1Cs.Rotation",      "Exif.Sony2Cs.Rotation",
       "Exif.Sony1Cs2.Rotation",       "Exif.Sony2Cs2.Rotation",     "Exif.Sony1MltCsA100.Rotation",
+      "Exif.SonyMisc3c.CameraOrientation",
   };
   return findMetadatum(ed, keys, std::size(keys));
 }
