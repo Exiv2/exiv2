e7b699a82f047655fad078a3170307c04538ad58
Kevin Backhouse <kevinbackhouse@github.com>
2026-07-12 22:37:32 +0100

    Loop a few times until the HTTP server is started.

--- tests/bash_tests/utils.py
+++ tests/bash_tests/utils.py
@@ -363,16 +363,19 @@ class HttpServer:
         shared_port = multiprocessing.Value('i', -1)
         self.proc = multiprocessing.Process(target=self._start, name=str(self), args=(shared_port,))
         self.proc.start()
-        time.sleep(2)
-        self.port = shared_port.value
-        log.info(f"HTTP server is running on port {self.port}")
-        try:
-            with request.urlopen(f'http://127.0.0.1:{self.port}', timeout=3) as f:
-                if f.status != 200:
-                    raise RuntimeError()
-        except Exception as e:
-            raise RuntimeError(f'Failed to run the HTTP server: {e}') from e
-        log.info('The HTTP server started')
+        for i in range(0,5):
+            time.sleep(2)
+            self.port = shared_port.value
+            log.info(f"HTTP server is running on port {self.port}")
+            try:
+                with request.urlopen(f'http://127.0.0.1:{self.port}', timeout=3) as f:
+                    if f.status != 200:
+                        raise RuntimeError()
+                log.info('The HTTP server started')
+                return
+            except:
+                continue
+        raise RuntimeError('Failed to run the HTTP server')
 
     def stop(self):
         self.proc.terminate()
