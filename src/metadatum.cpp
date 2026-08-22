2c1f184fbd508cbecbfa83a6d564b54dc4ffd5d7
Kevin Backhouse <kevinbackhouse@github.com>
2026-08-22 12:13:49 +0100

    WIP

--- src/metadatum.cpp
+++ src/metadatum.cpp
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "metadatum.hpp"
+#include "enforce.hpp"
+#include <limits>
 
 namespace Exiv2 {
 Key::UniquePtr Key::clone() const {
@@ -14,7 +16,10 @@ std::string Metadatum::print(const ExifData* pMetadata) const {
 }
 
 uint32_t Metadatum::toUint32(size_t n) const {
-  return static_cast<uint32_t>(toInt64(n));
+  auto i = toInt64(n);
+  Internal::enforce(std::numeric_limits<uint32_t>::min() <= i, ErrorCode::kerCorruptedMetadata);
+  Internal::enforce(i <= std::numeric_limits<uint32_t>::max(), ErrorCode::kerCorruptedMetadata);
+  return static_cast<uint32_t>(i);
 }
 
 bool cmpMetadataByTag(const Metadatum& lhs, const Metadatum& rhs) {
