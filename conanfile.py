6215aceea529f0a52d9b7c822a3218419b71e41d
Miloš Komarčević <4973094+kmilos@users.noreply.github.com>
2026-09-11 17:26:11 +0200

    Bump conan recipes

--- conanfile.py
+++ conanfile.py
@@ -19,24 +19,24 @@ class Exiv2Conan(ConanFile):
         self.options['gtest'].shared = False
 
     def requirements(self):
-        self.requires('zlib/1.3.1')
+        self.requires('zlib/1.3.2')
 
-        self.requires('brotli/1.1.0')
+        self.requires('brotli/1.2.0')
 
-        self.requires('inih/58')
+        self.requires('inih/62')
 
-        self.requires('fmt/11.0.2')
+        self.requires('fmt/12.2.0')
 
         if self.options.webready:
-            self.requires('libcurl/8.10.1')
+            self.requires('libcurl/8.21.0')
 
         if self.settings.os == "Windows" and self.options.iconv:
-            self.requires('libiconv/1.17')
+            self.requires('libiconv/1.18')
 
         if self.options.unitTests:
-            self.requires('gtest/1.15.0')
+            self.requires('gtest/1.18.0')
 
         if self.options.xmp:
             self.requires('XmpSdk/2016.7@piponazo/stable') # from conan-piponazo
         else:
-            self.requires('expat/2.6.3')
+            self.requires('expat/2.8.3')
