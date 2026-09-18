4d5c4b602c57ba80703206e142419755a1feb508
Miloš Komarčević <4973094+kmilos@users.noreply.github.com>
2026-09-18 18:08:43 +0200

    CMake: remove Iconv finding verbosity

--- cmake/findDependencies.cmake
+++ cmake/findDependencies.cmake
@@ -48,10 +48,6 @@ if (EXIV2_ENABLE_NLS)
 endif( )
 
 find_package(Iconv)
-if(ICONV_FOUND AND NOT Iconv_IS_BUILT_IN)
-    message ( "-- Iconv_INCLUDE_DIRS : " ${Iconv_INCLUDE_DIRS} )
-    message ( "-- Iconv_LIBRARIES : " ${Iconv_LIBRARIES} )
-endif()
 
 if( EXIV2_ENABLE_INIH )
     find_package(inih CONFIG QUIET)
